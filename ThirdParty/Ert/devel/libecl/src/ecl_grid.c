df40d89fe984ae5b8ce12b4ee4f921f28267650c
Jacob Støren <jacob.storen@ceetron.com>
2013-02-01 09:26:43 +0100

    Fixing the crash on retreiving the fractureActive index on none dual porosity again. p4#: 20326

--- ThirdParty/Ert/devel/libecl/src/ecl_grid.c
+++ ThirdParty/Ert/devel/libecl/src/ecl_grid.c
@@ -3005,6 +3005,7 @@ int ecl_grid_get_active_fracture_index3(const ecl_grid_type * ecl_grid , int i ,
 */
 
 int ecl_grid_get_active_fracture_index1(const ecl_grid_type * ecl_grid , int global_index) {
+  if (!ecl_grid->fracture_index_map) return -1;
   return ecl_grid->fracture_index_map[global_index];
 }
 
