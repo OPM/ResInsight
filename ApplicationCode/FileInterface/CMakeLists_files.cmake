4101c3cd8543bb0c0202435a60bc18227116327b
Magne Sjaastad <magne.sjaastad@ceetron.com>
2013-03-22 10:10:34 +0100

    Removed obsolete file p4#: 21048

--- ApplicationCode/FileInterface/CMakeLists_files.cmake
+++ ApplicationCode/FileInterface/CMakeLists_files.cmake
@@ -15,7 +15,6 @@ ${CEE_CURRENT_LIST_DIR}RifReaderEclipseInput.h
 ${CEE_CURRENT_LIST_DIR}RifReaderEclipseOutput.h
 ${CEE_CURRENT_LIST_DIR}RifReaderInterface.h
 ${CEE_CURRENT_LIST_DIR}RifReaderMockModel.h
-${CEE_CURRENT_LIST_DIR}RifReaderStatisticalCalculation.h
 )
 
 list(APPEND CODE_SOURCE_FILES
@@ -27,7 +26,6 @@ ${CEE_CURRENT_LIST_DIR}RifEclipseUnifiedRestartFileAccess.cpp
 ${CEE_CURRENT_LIST_DIR}RifReaderEclipseInput.cpp
 ${CEE_CURRENT_LIST_DIR}RifReaderEclipseOutput.cpp
 ${CEE_CURRENT_LIST_DIR}RifReaderMockModel.cpp
-${CEE_CURRENT_LIST_DIR}RifReaderStatisticalCalculation.cpp
 )
 
 
