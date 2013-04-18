f9985af7e96e9d1aa65783be67c0c514d9bb8c00
Magne Sjaastad <magne.sjaastad@ceetron.com>
2013-04-18 07:33:45 +0200

    Added source groups in CMake to improve file organization in generated projects p4#: 21330

--- ApplicationCode/FileInterface/CMakeLists_files.cmake
+++ ApplicationCode/FileInterface/CMakeLists_files.cmake
@@ -4,8 +4,7 @@ if (${CMAKE_VERSION} VERSION_GREATER "2.8.2")
     set(CEE_CURRENT_LIST_DIR  ${CMAKE_CURRENT_LIST_DIR}/)
 endif()
 
-
-list(APPEND CODE_HEADER_FILES
+set (SOURCE_GROUP_HEADER_FILES
 ${CEE_CURRENT_LIST_DIR}RifEclipseInputFileTools.h
 ${CEE_CURRENT_LIST_DIR}RifEclipseOutputFileTools.h
 ${CEE_CURRENT_LIST_DIR}RifEclipseRestartDataAccess.h
@@ -17,7 +16,7 @@ ${CEE_CURRENT_LIST_DIR}RifReaderInterface.h
 ${CEE_CURRENT_LIST_DIR}RifReaderMockModel.h
 )
 
-list(APPEND CODE_SOURCE_FILES
+set (SOURCE_GROUP_SOURCE_FILES
 ${CEE_CURRENT_LIST_DIR}RifEclipseInputFileTools.cpp
 ${CEE_CURRENT_LIST_DIR}RifEclipseOutputFileTools.cpp
 ${CEE_CURRENT_LIST_DIR}RifEclipseRestartDataAccess.cpp
@@ -28,4 +27,12 @@ ${CEE_CURRENT_LIST_DIR}RifReaderEclipseOutput.cpp
 ${CEE_CURRENT_LIST_DIR}RifReaderMockModel.cpp
 )
 
+list(APPEND CODE_HEADER_FILES
+${SOURCE_GROUP_HEADER_FILES}
+)
 
+list(APPEND CODE_SOURCE_FILES
+${SOURCE_GROUP_SOURCE_FILES}
+)
+
+source_group( "FileInterface" FILES ${SOURCE_GROUP_HEADER_FILES} ${SOURCE_GROUP_SOURCE_FILES} )
