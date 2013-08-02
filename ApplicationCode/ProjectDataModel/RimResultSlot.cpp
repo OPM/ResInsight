50777b3e3f54fb0894deaa223a0e0a19d5e6b5cc
Magne Sjaastad <magne.sjaastad@ceetron.com>
2013-08-02 13:04:25 +0200

    Fixed issue related to missing legend config Must hide legend config collection. Revisit when a more robust tree building strategy is in place. p4#: 22092

--- ApplicationCode/ProjectDataModel/RimResultSlot.cpp
+++ ApplicationCode/ProjectDataModel/RimResultSlot.cpp
@@ -66,11 +66,14 @@ RimResultSlot::~RimResultSlot()
 //--------------------------------------------------------------------------------------------------
 void RimResultSlot::fieldChangedByUi(const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue)
 {
-    if (changedField == &m_resultVariable)
+    RimResultDefinition::fieldChangedByUi(changedField, oldValue, newValue);
+
+    // Update of legend config must happen after RimResultDefinition::fieldChangedByUi(), as this function modifies this->resultVariable()
+    if (changedField == &m_resultVariableUiField)
     {
         if (oldValue != newValue)
         {
-           changeLegendConfig(this->resultVariable());
+            changeLegendConfig(this->resultVariable());
         }
 
         if (newValue != RimDefines::undefinedResultName())
@@ -78,8 +81,6 @@ void RimResultSlot::fieldChangedByUi(const caf::PdmFieldHandle* changedField, co
             if (m_reservoirView) m_reservoirView->animationMode = true;
         }
     }
-
-    RimResultDefinition::fieldChangedByUi(changedField, oldValue, newValue);
 
     if (m_reservoirView) m_reservoirView->createDisplayModelAndRedraw();
 }
