0bdacfba1729329cb1ec5cf6429dc643efefb15e
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2014-04-03 08:16:15 +0200

    Moved socket max byte count to riSettings
    Added header files to cmake

--- OctavePlugin/riSocketTools.h
+++ OctavePlugin/riSocketTools.h
@@ -7,7 +7,7 @@ bool readBlockData(QTcpSocket& socket, char* data, quint64 bytesToRead, QStringL
     quint64 bytesRead = 0;
     int blockCount = 0;
 
-    quint64 maxBlockSize = 100000;
+    quint64 maxBlockSize = riOctavePlugin::socketMaxByteCount;
 
     while (bytesRead < bytesToRead)
     {
@@ -18,7 +18,7 @@ bool readBlockData(QTcpSocket& socket, char* data, quint64 bytesToRead, QStringL
             qint64 actuallyBytesRead = socket.read(data + bytesRead, byteCountToRead);
             if (actuallyBytesRead < 0)
             {
-                errorMessages.push_back("Error detected when writing data, error string from socket");
+                errorMessages.push_back("Error detected when reading data, error string from socket :");
                 errorMessages.push_back(socket.errorString());
 
                 return false;
@@ -40,6 +40,40 @@ bool readBlockData(QTcpSocket& socket, char* data, quint64 bytesToRead, QStringL
                 return false;
             }
         }
+    }
+    
+    octave_stdout << "Bytes read " << bytesToRead << std::endl;
+
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+bool writeBlockData(QTcpSocket& socket, const char* data, quint64 bytesToWrite, QStringList& errorMessages)
+{
+    quint64 bytesWritten = 0;
+    int blockCount = 0;
+
+    quint64 maxBlockSize = riOctavePlugin::socketMaxByteCount;
+
+    while (bytesWritten < bytesToWrite)
+    {
+        quint64 byteCountToWrite = qMin(bytesToWrite - bytesWritten, maxBlockSize);
+
+        qint64 actuallyBytesWritten = socket.write(data + bytesWritten, byteCountToWrite);
+        if (actuallyBytesWritten < 0)
+        {
+            errorMessages.push_back("Error detected when writing data, error string from socket");
+            errorMessages.push_back(socket.errorString());
+
+            return false;
+        }
+
+        bytesWritten += actuallyBytesWritten;
+
+        blockCount++;
     }
 
     return true;
