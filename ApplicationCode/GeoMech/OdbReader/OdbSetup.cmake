936d6553cfbf57db5d746737fce334f3829c0920
Jacob Støren <jacob.storen@ceetronSolutions.com>
2015-04-28 10:45:00 +0200

    Improved CMakeSetup for OdbTest app
    Cleaned away the hardcoded paths. Made options instead.

--- ApplicationCode/GeoMech/OdbReader/OdbSetup.cmake
+++ ApplicationCode/GeoMech/OdbReader/OdbSetup.cmake
@@ -1,9 +1,10 @@
 
 # Copy Odb Dlls
+set(RESINSIGHT_ODB_API_DIR "" CACHE PATH "Path tho the ODB api from Simulia")
 
 if (MSVC)
 	# Find all the dlls
-	file (GLOB  RI_ALL_ODB_DLLS  ${RI_ODB_API_DIR}/lib/*.dll)
+	file (GLOB  RI_ALL_ODB_DLLS  ${RESINSIGHT_ODB_API_DIR}/lib/*.dll)
 	
 	# Strip off the path
 	foreach (aDLL  ${RI_ALL_ODB_DLLS})
@@ -12,9 +13,15 @@ if (MSVC)
 	endforeach(aDLL)
 
 	# Copy to target directory
+	#foreach (aDLL ${RI_ODB_DLLS})
+    #    add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+    #                       COMMAND ${CMAKE_COMMAND} -E copy_if_different  "${RI_ODB_API_DIR}/lib/${aDLL}"  "$<TARGET_FILE_DIR:${PROJECT_NAME}>")
+    #endforeach()
 	foreach (aDLL ${RI_ODB_DLLS})
         add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
-                           COMMAND ${CMAKE_COMMAND} -E copy_if_different  "${RI_ODB_API_DIR}/lib/${aDLL}"  "$<TARGET_FILE_DIR:${PROJECT_NAME}>")
+                           COMMAND ${CMAKE_COMMAND} -E copy_if_different  
+						   "${RESINSIGHT_ODB_API_DIR}/lib/${aDLL}"  
+						   "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIGURATION>")
     endforeach()
 
 endif(MSVC)
