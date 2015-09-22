OPM/ResInsight

commit 06e8bc2ec5a4b33f2f8117041673191f6ace738b

Moved building of time step names for UI to case

ApplicationCode/UserInterface/RiuMainWindow.cpp
@@ -799,12 +799,11 @@ void RiuMainWindow::refreshAnimationActions()
     m_animationToolBar->connectAnimationControl(animationControl);
 
     QStringList timeStepStrings;
+
     int currentTimeStepIndex = 0;
 
-    RiaApplication* app = RiaApplication::instance();
-
     bool enableAnimControls = false;
-    RimView * activeView = app->activeReservoirView();
+    RimView * activeView = RiaApplication::instance()->activeReservoirView();
     if (activeView && 
         activeView->viewer() &&
         activeView->viewer()->frameCount())
@@ -818,27 +817,7 @@ void RiuMainWindow::refreshAnimationActions()
             {
                 if (activeRiv->isTimeStepDependentDataVisible())
                 {
-                    std::vector<QDateTime> timeStepDates = activeRiv->currentGridCellResults()->cellResults()->timeStepDates(0);
-                    bool showHoursAndMinutes = false;
-                    for (size_t i = 0; i < timeStepDates.size(); i++)
-                    {
-                        if (timeStepDates[i].time().hour() != 0.0 || timeStepDates[i].time().minute() != 0.0)
-                        {
-                            showHoursAndMinutes = true;
-                        }
-                    }
-
-                    QString formatString = "dd.MMM yyyy";
-                    if (showHoursAndMinutes)
-                    {
-                        formatString += " - hh:mm";
-                    }
-
-                    for (size_t i = 0; i < timeStepDates.size(); i++)
-                    {
-                        timeStepStrings += timeStepDates[i].toString(formatString);
-                    }
-                    currentTimeStepIndex = RiaApplication::instance()->activeReservoirView()->currentTimeStep();
+                    timeStepStrings = activeRiv->eclipseCase()->timeStepStrings();
                 }
                 else
                 {
@@ -853,15 +832,12 @@ void RiuMainWindow::refreshAnimationActions()
             {
                 if (activeGmv->isTimeStepDependentDataVisible())
                 {
-                    std::vector<std::string> stepNames = activeGmv->geoMechCase()->geoMechData()->femPartResults()->stepNames();
-                    for (size_t i = 0; i < stepNames.size(); i++)
-                    {
-                        timeStepStrings += QString::fromStdString(stepNames[i]);
-                    }
-                    currentTimeStepIndex = RiaApplication::instance()->activeReservoirView()->currentTimeStep();
+                    activeGmv->geoMechCase()->timeStepStrings();
                 }
             }
         }
+
+        currentTimeStepIndex = activeView->currentTimeStep();
 
         // Animation control is only relevant for more than one time step
 
@@ -870,7 +846,7 @@ void RiuMainWindow::refreshAnimationActions()
             enableAnimControls = false;
         }
 
-        m_animationToolBar->setFrameRate(app->activeReservoirView()->maximumFrameRate());
+        m_animationToolBar->setFrameRate(activeView->maximumFrameRate());
     }
 
     m_animationToolBar->setTimeStepStrings(timeStepStrings);

ApplicationCode/ProjectDataModel/RimCase.h
@@ -40,6 +40,9 @@ public:
     virtual void                                updateFilePathsFromProjectPath(const QString& projectPath, const QString& oldProjectPath) = 0;
 
     virtual caf::PdmFieldHandle*                userDescriptionField()  { return &caseUserDescription; }
+
+    virtual QStringList                         timeStepStrings() { return QStringList(); }
+
 protected:
     static QString                              relocateFile(const QString& fileName, const QString& newProjectPath, const QString& oldProjectPath, 
                                                              bool* foundFile, std::vector<QString>* searchedPaths);

ApplicationCode/ProjectDataModel/RimEclipseCase.cpp
@@ -408,3 +408,35 @@ std::vector<RimView*> RimEclipseCase::views()
     }
     return views;
 }
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+QStringList RimEclipseCase::timeStepStrings()
+{
+    QStringList stringList;
+
+    std::vector<QDateTime> timeStepDates = results(RifReaderInterface::MATRIX_RESULTS)->cellResults()->timeStepDates(0);
+
+    bool showHoursAndMinutes = false;
+    for (size_t i = 0; i < timeStepDates.size(); i++)
+    {
+        if (timeStepDates[i].time().hour() != 0.0 || timeStepDates[i].time().minute() != 0.0)
+        {
+            showHoursAndMinutes = true;
+        }
+    }
+
+    QString formatString = "dd.MMM yyyy";
+    if (showHoursAndMinutes)
+    {
+        formatString += " - hh:mm";
+    }
+
+    for (size_t i = 0; i < timeStepDates.size(); i++)
+    {
+        stringList += timeStepDates[i].toString(formatString);
+    }
+
+    return stringList;
+}

ApplicationCode/ProjectDataModel/RimEclipseCase.h
@@ -85,7 +85,8 @@ public:
     RimIdenticalGridCaseGroup*                  parentGridCaseGroup();
                                                      
     virtual std::vector<RimView*>               views();
-                                                     
+    virtual QStringList                         timeStepStrings();
+
     // Overridden methods from PdmObject
 public:
 

ApplicationCode/ProjectDataModel/RimGeoMechCase.cpp
@@ -23,6 +23,8 @@
 #include "RiaPreferences.h"
 #include "RifOdbReader.h"
 #include "RigGeoMechCaseData.h"
+#include "RigFemPartResultsCollection.h"
+
 #include <QFile>
 
 CAF_PDM_SOURCE_INIT(RimGeoMechCase, "ResInsightGeoMechCase");
@@ -128,4 +130,20 @@ void RimGeoMechCase::initAfterRead()
         riv->setGeoMechCase(this);
     }
 
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+QStringList RimGeoMechCase::timeStepStrings()
+{
+    QStringList stringList;
+
+    std::vector<std::string> stepNames = geoMechData()->femPartResults()->stepNames();
+    for (size_t i = 0; i < stepNames.size(); i++)
+    {
+        stringList += QString::fromStdString(stepNames[i]);
+    }
+
+    return stringList;
 }

ApplicationCode/ProjectDataModel/RimGeoMechCase.h
@@ -56,6 +56,9 @@ public:
     virtual void                            updateFilePathsFromProjectPath(const QString& projectPath, const QString& oldProjectPath);
     virtual std::vector<RimView*>           views();
 
+    virtual QStringList                     timeStepStrings();
+
+
     // Fields:                                        
     caf::PdmChildArrayField<RimGeoMechView*>  geoMechViews;
 

commit 9d03120afa8fc7e20b0548b8c987f42a67973e69

(#512) Fixed missing time step texts for geomech case
Bug introduced in 06e8bc2ec5a4b33f2f8117041673191f6ace738b

ApplicationCode/UserInterface/RiuMainWindow.cpp
@@ -832,7 +832,7 @@ void RiuMainWindow::refreshAnimationActions()
             {
                 if (activeGmv->isTimeStepDependentDataVisible())
                 {
-                    activeGmv->geoMechCase()->timeStepStrings();
+                    timeStepStrings = activeGmv->geoMechCase()->timeStepStrings();
                 }
             }
         }