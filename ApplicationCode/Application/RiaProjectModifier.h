9b288970c1b6f54ddfccbda4353f85e6534b10e1
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2015-09-25 16:44:43 +0200

    (#495) Linux fixes

--- ApplicationCode/Application/RiaProjectModifier.h
+++ ApplicationCode/Application/RiaProjectModifier.h
@@ -19,6 +19,7 @@
 
 #pragma once
 
+#include "cvfBase.h"
 #include "cvfObject.h"
 
 #include <QString>
--- ApplicationCode/ModelVisualization/RivReservoirPartMgr.h
+++ ApplicationCode/ModelVisualization/RivReservoirPartMgr.h
@@ -20,6 +20,7 @@
 
 #pragma once
 
+#include "cvfBase.h"
 #include "cvfArray.h"
 #include "cvfCollection.h"
 
