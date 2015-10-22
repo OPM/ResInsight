OPM/ResInsight

commit b15b23b856ffac31e07cb909e24ee009ece9fbd6

(#540) Removed the Visible Cells Sync from Gui. Moved UpateOverrides from init afterRead to OpenProject

ApplicationCode/Application/RiaApplication.cpp
@@ -94,6 +94,8 @@
 #ifdef WIN32
 #include <fcntl.h>
 #endif
+#include "RimViewLinkerCollection.h"
+#include "RimViewLinker.h"
 
 namespace caf
 {
@@ -402,6 +404,11 @@ bool RiaApplication::loadProject(const QString& projectFileName, ProjectLoadActi
             caseProgress.incrementProgress();
         }
     }
+
+    {
+        m_project->viewLinkerCollection()->viewLinker()->updateOverrides();
+    }
+
     {
         if (m_project->mainPlotCollection() && m_project->mainPlotCollection()->wellLogPlotCollection())
         {

ApplicationCode/ProjectDataModel/RimViewController.cpp
@@ -71,7 +71,10 @@ RimViewController::RimViewController(void)
     CAF_PDM_InitField(&m_syncCellResult,      "SyncCellResult", false,    "Cell Result", "", "", "");
     
     CAF_PDM_InitField(&m_syncVisibleCells,    "SyncVisibleCells", false,  "Visible Cells", "", "", "");
-    //syncVisibleCells.uiCapability()->setUiHidden(true); // For now
+    /// We do not support this. Consider to remove sometime
+    m_syncVisibleCells.uiCapability()->setUiHidden(true);
+    m_syncVisibleCells.xmlCapability()->setIOWritable(false);
+    m_syncVisibleCells.xmlCapability()->setIOReadable(false);
 
     CAF_PDM_InitField(&m_syncRangeFilters,    "SyncRangeFilters", true,   "Range Filters", "", "", "");
     CAF_PDM_InitField(&m_syncPropertyFilters, "SyncPropertyFilters", true,"Property Filters", "", "", "");
@@ -216,8 +219,9 @@ void RimViewController::fieldChangedByUi(const caf::PdmFieldHandle* changedField
 void RimViewController::initAfterRead()
 {
     updateOptionSensitivity();
-    updateOverrides();
     updateDisplayNameAndIcon();
+
+    // Update overrides is now done on load project in Application
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationCode/ProjectDataModel/RimViewLinker.h
@@ -59,6 +59,8 @@ public:
     void                                    updateDependentViews();
     void                                    removeViewController(RimViewController* viewController);
 
+    void                                    updateOverrides();
+
     void                                    updateCamera(RimView* sourceView);
     void                                    updateTimeStep(RimView* sourceView, int timeStep);
     void                                    updateScaleZ(RimView* sourceView, double scaleZ);
@@ -89,7 +91,6 @@ protected:
 private:
     void                                    allViewsForCameraSync(RimView* source, std::vector<RimView*>& views);
     
-    void                                    updateOverrides();
     void                                    removeOverrides();
 
     static bool                             isBoundingBoxesOverlappingOrClose(const cvf::BoundingBox& sourceBB, const cvf::BoundingBox& destBB);

commit b18ae1a14a86f9875dd8c4fb94baa7d93e26ba45

Fixed crash introduced by b15b23b856ffa

ApplicationCode/Application/RiaApplication.cpp
@@ -405,6 +405,7 @@ bool RiaApplication::loadProject(const QString& projectFileName, ProjectLoadActi
         }
     }
 
+    if (m_project->viewLinkerCollection() && m_project->viewLinkerCollection()->viewLinker())
     {
         m_project->viewLinkerCollection()->viewLinker()->updateOverrides();
     }