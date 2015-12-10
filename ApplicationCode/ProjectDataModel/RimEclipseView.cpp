OPM/ResInsight

commit 707e8c68ab5e3ed8470c88f40660eaa5ac68cccc

(#163) Sort simulation wells by name

ApplicationCode/ProjectDataModel/RimEclipseView.cpp
@@ -1138,6 +1138,8 @@ void RimEclipseView::syncronizeWellsWithResults()
     {
         this->wellCollection()->wells()[wIdx]->setReservoirView(this);
     }
+
+    this->wellCollection()->sortWellsByName();
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationCode/ProjectDataModel/RimEclipseWellCollection.cpp
@@ -333,3 +333,16 @@ void RimEclipseWellCollection::calculateIsWellPipesVisible(size_t frameIndex)
         m_isWellPipesVisible[frameIndex][i] = wells[i]->calculateWellPipeVisibility(frameIndex);
     }
 }
+
+bool lessEclipseWell(const caf::PdmPointer<RimEclipseWell>& w1,  const caf::PdmPointer<RimEclipseWell>& w2)
+{
+    return (w1->name() < w2->name());
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+void RimEclipseWellCollection::sortWellsByName()
+{
+   std::sort(wells.begin(), wells.end(), lessEclipseWell);
+}

ApplicationCode/ProjectDataModel/RimEclipseWellCollection.h
@@ -104,13 +104,15 @@ public:
 
     caf::PdmChildArrayField<RimEclipseWell*>     wells;
 
-    RimEclipseWell*                            findWell(QString name);
+    RimEclipseWell*                     findWell(QString name);
     bool                                hasVisibleWellCells();
     bool                                hasVisibleWellPipes();
+    void                                sortWellsByName();
 
     const std::vector<cvf::ubyte>&      isWellPipesVisible(size_t frameIndex);       
     void                                scheduleIsWellPipesVisibleRecalculation();
 
+protected:
     virtual void                        fieldChangedByUi(const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue);
     virtual void                        defineUiOrdering(QString uiConfigName, caf::PdmUiOrdering& uiOrdering);
     virtual caf::PdmFieldHandle*        objectToggleField();

commit 179f0c906b3d6be152cea82b16dca94852e8a018

Fixed simulation well pipe visibility error introduced in 707e8c68ab5e

ApplicationCode/ProjectDataModel/RimEclipseView.cpp
@@ -1108,10 +1108,9 @@ void RimEclipseView::syncronizeWellsWithResults()
         {
             well = new RimEclipseWell;
             well->name = wellResults[wIdx]->m_wellName;
-            
         }
         newWells.push_back(well);
-        well->setWellIndex(wIdx);
+
         well->setWellResults(wellResults[wIdx].p());
     }
 
@@ -1140,6 +1139,13 @@ void RimEclipseView::syncronizeWellsWithResults()
     }
 
     this->wellCollection()->sortWellsByName();
+
+    for (size_t wIdx = 0; wIdx < this->wellCollection()->wells().size(); ++wIdx)
+    {
+        this->wellCollection()->wells()[wIdx]->setWellIndex(wIdx);
+    }
+
+
 }
 
 //--------------------------------------------------------------------------------------------------