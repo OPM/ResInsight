OPM/ResInsight

commit 442d9d2e411f736f61c3dbdd4ab46638639838be

#1380 Major refactor of the management of views to fix the active view problem. Expect some trouble following this.

ApplicationCode/UserInterface/RiuMainPlotWindow.cpp
@@ -490,58 +490,13 @@ void RiuMainPlotWindow::slotSubWindowActivated(QMdiSubWindow* subWindow)
     RimProject * proj = RiaApplication::instance()->project();
     if (!proj) return;
 
+    // Select in Project Tree
+
+    RimViewWindow* viewWindow = RiuInterfaceToViewWindow::viewWindowFromWidget(subWindow->widget());
+
+    if (viewWindow)
     {
-        RiuWellLogPlot* wellLogPlotViewer = dynamic_cast<RiuWellLogPlot*>(subWindow->widget());
-        if (wellLogPlotViewer)
-        {
-            RimWellLogPlot* wellLogPlot = wellLogPlotViewer->ownerPlotDefinition();
-
-            if (wellLogPlot != RiaApplication::instance()->activeWellLogPlot())
-            {
-                RiaApplication::instance()->setActiveWellLogPlot(wellLogPlot);
-                projectTreeView()->selectAsCurrentItem(wellLogPlot);
-            }
-        }
-        else
-        {
-            RiaApplication::instance()->setActiveWellLogPlot(NULL);
-        }
-    }
-
-    {
-        RiuSummaryQwtPlot* summaryPlotViewer = dynamic_cast<RiuSummaryQwtPlot*>(subWindow->widget());
-        if (summaryPlotViewer)
-        {
-            RimSummaryPlot* summaryPlot = summaryPlotViewer->ownerPlotDefinition();
-
-            if (summaryPlot != RiaApplication::instance()->activeSummaryPlot())
-            {
-                RiaApplication::instance()->setActiveSummaryPlot(summaryPlot);
-                projectTreeView()->selectAsCurrentItem(summaryPlot);
-            }
-        }
-        else
-        {
-            RiaApplication::instance()->setActiveSummaryPlot(NULL);
-        }
-    }
-
-    {
-        RiuWellAllocationPlot* wellAllocationPlotWidget = dynamic_cast<RiuWellAllocationPlot*>(subWindow->widget());
-        if (wellAllocationPlotWidget)
-        {
-            RimWellAllocationPlot* wellAllocationPlot = wellAllocationPlotWidget->ownerPlotDefinition();
-
-            if (wellAllocationPlot != RiaApplication::instance()->activeWellAllocationPlot())
-            {
-                RiaApplication::instance()->setActiveWellAllocationPlot(wellAllocationPlot);
-                projectTreeView()->selectAsCurrentItem(wellAllocationPlot);
-            }
-        }
-        else
-        {
-            RiaApplication::instance()->setActiveWellAllocationPlot(nullptr);
-        }
+        projectTreeView()->selectAsCurrentItem(viewWindow);
     }
 }
 
@@ -618,66 +573,28 @@ void RiuMainPlotWindow::selectedObjectsChanged()
 
         if (!firstSelectedObject) return;
 
-        // Well log plot
-
-        bool isActiveObjectChanged = false;
-        
-        RimWellLogPlot* selectedWellLogPlot = dynamic_cast<RimWellLogPlot*>(firstSelectedObject);
-        if (!selectedWellLogPlot)
+        RimViewWindow* selectedWindow = dynamic_cast<RimViewWindow*>(firstSelectedObject);
+        if (!selectedWindow)
         {
-            firstSelectedObject->firstAncestorOrThisOfType(selectedWellLogPlot);
+            firstSelectedObject->firstAncestorOrThisOfType(selectedWindow);
         }
 
-        if (selectedWellLogPlot)
+        // If we cant find the view window as an MDI sub window, we search higher in the 
+        // project tree to find a possible parent view window that has.
+        if (selectedWindow && !findMdiSubWindow(selectedWindow->viewWidget())) 
         {
-            if (selectedWellLogPlot->viewWidget())
+            if (selectedWindow->parentField() && selectedWindow->parentField()->ownerObject())
             {
-                setActiveViewer(selectedWellLogPlot->viewWidget());
+                selectedWindow->parentField()->ownerObject()->firstAncestorOrThisOfType(selectedWindow);
             }
-            isActiveObjectChanged = true;
-            RiaApplication::instance()->setActiveWellLogPlot(selectedWellLogPlot);
         }
 
-        // Summary plot
-
-        RimSummaryPlot* selectedSummaryPlot = dynamic_cast<RimSummaryPlot*>(firstSelectedObject);
-        if (!selectedSummaryPlot)
+        if (selectedWindow)
         {
-            firstSelectedObject->firstAncestorOrThisOfType(selectedSummaryPlot);
-        }
-
-        if (selectedSummaryPlot)
-        {
-            if (selectedSummaryPlot->viewWidget())
+            if (selectedWindow->viewWidget())
             {
-                setActiveViewer(selectedSummaryPlot->viewWidget());
+                setActiveViewer(selectedWindow->viewWidget());
             }
-
-            isActiveObjectChanged = true;
-            RiaApplication::instance()->setActiveSummaryPlot(selectedSummaryPlot);
-        }
-
-        // Flow plot
-
-        RimWellAllocationPlot* wellAllocationPlot = dynamic_cast<RimWellAllocationPlot*>(firstSelectedObject);
-        if (!wellAllocationPlot)
-        {
-            firstSelectedObject->firstAncestorOrThisOfType(wellAllocationPlot);
-        }
-
-        if (wellAllocationPlot)
-        {
-            if (wellAllocationPlot->viewWidget())
-            {
-                setActiveViewer(wellAllocationPlot->viewWidget());
-            }
-
-            isActiveObjectChanged = true;
-            RiaApplication::instance()->setActiveWellAllocationPlot(wellAllocationPlot);
-        }
-
-        if (isActiveObjectChanged)
-        {
             // The only way to get to this code is by selection change initiated from the project tree view
             // As we are activating an MDI-window, the focus is given to this MDI-window
             // Set focus back to the tree view to be able to continue keyboard tree view navigation
@@ -775,33 +692,12 @@ RimMdiWindowGeometry RiuMainPlotWindow::windowGeometryForViewer(QWidget* viewer)
     QMdiSubWindow* mdiWindow = findMdiSubWindow(viewer);
     if (mdiWindow)
     {
-        return windowGeometryForWidget(mdiWindow);
+        return RiuMdiSubWindow::windowGeometryForWidget(mdiWindow);
     }
 
     RimMdiWindowGeometry geo;
     return geo;
 }
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-RimMdiWindowGeometry RiuMainPlotWindow::windowGeometryForWidget(QWidget* widget)
-{
-    RimMdiWindowGeometry geo;
-
-    if (widget)
-    {
-        geo.mainWindowID = 1;
-        geo.x = widget->pos().x();
-        geo.y = widget->pos().y();
-        geo.width = widget->size().width();
-        geo.height = widget->size().height();
-        geo.isMaximized = widget->isMaximized();
-    }
-
-    return geo;
-}
-
 //--------------------------------------------------------------------------------------------------
 /// 
 //--------------------------------------------------------------------------------------------------

ApplicationCode/UserInterface/RiuMainPlotWindow.h
@@ -72,7 +72,6 @@ public:
     void                setExpanded(const caf::PdmUiItem* uiItem, bool expanded);
 
     RimMdiWindowGeometry windowGeometryForViewer(QWidget* viewer) override;
-    RimMdiWindowGeometry windowGeometryForWidget(QWidget* widget);
 
     void                tileWindows();
     bool                isAnyMdiSubWindowVisible();

ApplicationCode/Application/RiaApplication.cpp
@@ -1119,57 +1119,28 @@ RimView* RiaApplication::activeReservoirView()
 //--------------------------------------------------------------------------------------------------
 /// 
 //--------------------------------------------------------------------------------------------------
+RimViewWindow* RiaApplication::activePlotWindow() const
+{
+    RimViewWindow* viewWindow = nullptr;
+
+    if ( m_mainPlotWindow )
+    {
+        QList<QMdiSubWindow*> subwindows = m_mainPlotWindow->subWindowList(QMdiArea::StackingOrder);
+        if ( subwindows.size() > 0 )
+        {
+            viewWindow = RiuInterfaceToViewWindow::viewWindowFromWidget(subwindows.back()->widget());
+        }
+    }
+
+    return viewWindow;
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
 void RiaApplication::setActiveReservoirView(RimView* rv)
 {
     m_activeReservoirView = rv;
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-void RiaApplication::setActiveWellLogPlot(RimWellLogPlot* wlp)
-{
-    m_activeWellLogPlot = wlp;
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-RimWellLogPlot* RiaApplication::activeWellLogPlot()
-{
-   return m_activeWellLogPlot;
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-void RiaApplication::setActiveSummaryPlot(RimSummaryPlot* sp)
-{
-    m_activeSummaryPlot = sp;
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-RimSummaryPlot* RiaApplication::activeSummaryPlot()
-{
-    return m_activeSummaryPlot;
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-void RiaApplication::setActiveWellAllocationPlot(RimWellAllocationPlot* wap)
-{
-    m_activeWellAllocationPlot = wap;
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-RimWellAllocationPlot* RiaApplication::activeWellAllocationPlot()
-{
-    return m_activeWellAllocationPlot;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -1620,42 +1591,20 @@ RimViewWindow* RiaApplication::activeViewWindow()
 {
     RimViewWindow* viewWindow = NULL;
 
-    QWidget* topLevelWidget = RiaApplication::activeWindow();
+    QWidget* mainWindowWidget = RiaApplication::activeWindow();
 
-    if (dynamic_cast<RiuMainWindow*>(topLevelWidget))
+    if (dynamic_cast<RiuMainWindow*>(mainWindowWidget))
     {
         viewWindow = RiaApplication::instance()->activeReservoirView();
     }
-
-    if (dynamic_cast<RiuMainPlotWindow*>(topLevelWidget))
+    else if (dynamic_cast<RiuMainPlotWindow*>(mainWindowWidget))
     {
-        RiuMainPlotWindow* mainPlotWindow = dynamic_cast<RiuMainPlotWindow*>(topLevelWidget);
+        RiuMainPlotWindow* mainPlotWindow = dynamic_cast<RiuMainPlotWindow*>(mainWindowWidget);
+
         QList<QMdiSubWindow*> subwindows = mainPlotWindow->subWindowList(QMdiArea::StackingOrder);
         if (subwindows.size() > 0)
         {
-            RiuSummaryQwtPlot* summaryQwtPlot = dynamic_cast<RiuSummaryQwtPlot*>(subwindows.back()->widget());
-            if (summaryQwtPlot)
-            {
-                viewWindow = summaryQwtPlot->ownerPlotDefinition();
-            }
-
-            RiuWellLogPlot* wellLogPlot = dynamic_cast<RiuWellLogPlot*>(subwindows.back()->widget());
-            if (wellLogPlot)
-            {
-                viewWindow = wellLogPlot->ownerPlotDefinition();
-            }
-
-            RiuWellAllocationPlot* wellAllocationPlot = dynamic_cast<RiuWellAllocationPlot*>(subwindows.back()->widget());
-            if (wellAllocationPlot)
-            {
-                viewWindow = wellAllocationPlot->ownerPlotDefinition();
-            }
-
-            RiuFlowCharacteristicsPlot* flowCharacteristicsPlot = dynamic_cast<RiuFlowCharacteristicsPlot*>(subwindows.back()->widget());
-            if (flowCharacteristicsPlot)
-            {
-                viewWindow = flowCharacteristicsPlot->ownerPlotDefinition();
-            }
+            viewWindow = RiuInterfaceToViewWindow::viewWindowFromWidget(subwindows.back()->widget());
         }
     }
 

ApplicationCode/Application/RiaApplication.h
@@ -96,14 +96,7 @@ public:
     RimView*                activeReservoirView();
     const RimView*          activeReservoirView() const;
 
-    void                    setActiveWellLogPlot(RimWellLogPlot*);
-    RimWellLogPlot*         activeWellLogPlot();
-
-    void                    setActiveSummaryPlot(RimSummaryPlot*);
-    RimSummaryPlot*         activeSummaryPlot();
-
-    void                    setActiveWellAllocationPlot(RimWellAllocationPlot*);
-    RimWellAllocationPlot*  activeWellAllocationPlot();
+    RimViewWindow*          activePlotWindow() const;
 
     void                scheduleDisplayModelUpdateAndRedraw(RimView* resViewToUpdate);
 
@@ -228,9 +221,6 @@ private slots:
 
 private:
     caf::PdmPointer<RimView>            m_activeReservoirView;
-    caf::PdmPointer<RimWellLogPlot>     m_activeWellLogPlot;
-    caf::PdmPointer<RimSummaryPlot>     m_activeSummaryPlot;
-    caf::PdmPointer<RimWellAllocationPlot> m_activeWellAllocationPlot;
 
     caf::PdmPointer<RimProject>         m_project;
 

ApplicationCode/Commands/FlowCommands/RicShowContributingWellsFromPlotFeature.cpp
@@ -35,7 +35,8 @@ CAF_CMD_SOURCE_INIT(RicShowContributingWellsFromPlotFeature, "RicShowContributin
 //--------------------------------------------------------------------------------------------------
 bool RicShowContributingWellsFromPlotFeature::isCommandEnabled()
 {
-    RimWellAllocationPlot* wellAllocationPlot = RiaApplication::instance()->activeWellAllocationPlot();
+    RimWellAllocationPlot* wellAllocationPlot = dynamic_cast<RimWellAllocationPlot*>(RiaApplication::instance()->activePlotWindow());
+
     if (wellAllocationPlot) return true;
     
     return false;
@@ -46,7 +47,8 @@ bool RicShowContributingWellsFromPlotFeature::isCommandEnabled()
 //--------------------------------------------------------------------------------------------------
 void RicShowContributingWellsFromPlotFeature::onActionTriggered(bool isChecked)
 {
-    RimWellAllocationPlot* wellAllocationPlot = RiaApplication::instance()->activeWellAllocationPlot();
+    RimWellAllocationPlot* wellAllocationPlot = dynamic_cast<RimWellAllocationPlot*>(RiaApplication::instance()->activePlotWindow());
+
     if (!wellAllocationPlot) return;
 
     int timeStep = wellAllocationPlot->timeStep();

ApplicationCode/Commands/SummaryPlotCommands/RicNewGridTimeHistoryCurveFeature.cpp
@@ -101,7 +101,7 @@ RimSummaryPlot* RicNewGridTimeHistoryCurveFeature::userSelectedSummaryPlot()
 
         if (!defaultSelectedPlot)
         {
-            defaultSelectedPlot = RiaApplication::instance()->activeSummaryPlot();
+            defaultSelectedPlot = dynamic_cast<RimSummaryPlot*>( RiaApplication::instance()->activePlotWindow() );
         }
 
         if (!defaultSelectedPlot && summaryPlotColl->summaryPlots().size() > 0)

ApplicationCode/Commands/SummaryPlotCommands/RicViewZoomAllFeature.cpp
@@ -66,53 +66,11 @@ void RicViewZoomAllFeature::onActionTriggered(bool isChecked)
         QList<QMdiSubWindow*> subwindows = mainPlotWindow->subWindowList(QMdiArea::StackingOrder);
         if (subwindows.size() > 0)
         {
+            RimViewWindow* viewWindow = RiuInterfaceToViewWindow::viewWindowFromWidget(subwindows.back()->widget());
+
+            if (viewWindow)
             {
-                RiuSummaryQwtPlot* summaryQwtPlot = dynamic_cast<RiuSummaryQwtPlot*>(subwindows.back()->widget());
-                if (summaryQwtPlot)
-                {
-                    RimViewWindow* viewWindow = summaryQwtPlot->ownerPlotDefinition();
-
-                    viewWindow->zoomAll();
-                    summaryQwtPlot->replot();
-
-                    return;
-                }
-            }
-
-            {
-                RiuWellLogPlot* wellLogPlot = dynamic_cast<RiuWellLogPlot*>(subwindows.back()->widget());
-                if (wellLogPlot)
-                {
-                    RimViewWindow* viewWindow = wellLogPlot->ownerPlotDefinition();
-                    viewWindow->zoomAll();
-                    wellLogPlot->update();
-
-                    return;
-                }
-            }
-
-            {
-                RiuWellAllocationPlot* wellAllocationPlot = dynamic_cast<RiuWellAllocationPlot*>(subwindows.back()->widget());
-                if (wellAllocationPlot)
-                {
-                    RimWellAllocationPlot* viewWindow = wellAllocationPlot->ownerPlotDefinition();
-                    viewWindow->zoomAll();
-                    wellAllocationPlot->update();
-
-                    return;
-                }
-            }
-
-            {
-                RiuFlowCharacteristicsPlot* flowCharPlot = dynamic_cast<RiuFlowCharacteristicsPlot*>(subwindows.back()->widget());
-                if (flowCharPlot)
-                {
-                    RimFlowCharacteristicsPlot* viewWindow = flowCharPlot->ownerPlotDefinition();
-                    viewWindow->zoomAll();
-                    flowCharPlot->update();
-
-                    return;
-                }
+                viewWindow->zoomAll();
             }
         }
     }

ApplicationCode/UserInterface/CMakeLists_files.cmake
@@ -53,6 +53,7 @@ ${CEE_CURRENT_LIST_DIR}RiuCursors.cpp
 ${CEE_CURRENT_LIST_DIR}RiuDragDrop.cpp
 ${CEE_CURRENT_LIST_DIR}RiuFemResultTextBuilder.cpp
 ${CEE_CURRENT_LIST_DIR}RiuGeoQuestNavigation.cpp
+${CEE_CURRENT_LIST_DIR}RiuInterfaceToViewWindow.cpp
 ${CEE_CURRENT_LIST_DIR}RiuLineSegmentQwtPlotCurve.cpp
 ${CEE_CURRENT_LIST_DIR}RiuMainPlotWindow.cpp
 ${CEE_CURRENT_LIST_DIR}RiuMainWindow.cpp

commit 03ab71f82f3a0ea2ac7f61a4f8afff1e9de04c4c

#1411 Fixed the window activation bug introduced by 442d9d2e

ApplicationCode/UserInterface/RiuMainPlotWindow.cpp
@@ -64,6 +64,7 @@
 RiuMainPlotWindow::RiuMainPlotWindow()
     : m_pdmRoot(NULL),
     m_mainViewer(NULL),
+    m_activePlotViewWindow(nullptr),
     m_windowMenu(NULL),
     m_blockSlotSubWindowActivated(false)
 {
@@ -484,7 +485,6 @@ void RiuMainPlotWindow::setPdmRoot(caf::PdmObject* pdmRoot)
 void RiuMainPlotWindow::slotSubWindowActivated(QMdiSubWindow* subWindow)
 {
     if (!subWindow) return;
-    if (m_blockSlotSubWindowActivated) return;
 
     RimProject * proj = RiaApplication::instance()->project();
     if (!proj) return;
@@ -493,9 +493,10 @@ void RiuMainPlotWindow::slotSubWindowActivated(QMdiSubWindow* subWindow)
 
     RimViewWindow* viewWindow = RiuInterfaceToViewWindow::viewWindowFromWidget(subWindow->widget());
 
-    if (viewWindow)
+    if (viewWindow && viewWindow != m_activePlotViewWindow)
     {
-        projectTreeView()->selectAsCurrentItem(viewWindow);
+        if (!m_blockSlotSubWindowActivated) projectTreeView()->selectAsCurrentItem(viewWindow);
+        m_activePlotViewWindow = viewWindow;
     }
 }
 

ApplicationCode/UserInterface/RiuMainPlotWindow.h
@@ -31,6 +31,7 @@ class QMdiSubWindow;
 class RiuViewer;
 
 struct RimMdiWindowGeometry;
+class RimViewWindow;
 
 namespace caf
 {
@@ -114,7 +115,8 @@ private:
 
     QMdiArea*           m_mdiArea;
     RiuViewer*          m_mainViewer;
-    
+    RimViewWindow*      m_activePlotViewWindow;
+
     QMenu*              m_windowMenu;
 
     caf::PdmUiTreeView* m_projectTreeView;