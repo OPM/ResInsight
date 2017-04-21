029809bf90a24686e7ed73eb295bee0740da88aa
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2017-04-21 09:17:55 +0200

    Upped to version 2016.11.flow.13

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,7 +1,7 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2016)
 set(RESINSIGHT_MINOR_VERSION 11)
-set(RESINSIGHT_INCREMENT_VERSION "flow.12")
+set(RESINSIGHT_INCREMENT_VERSION "flow.13")
 
 
 # https://github.com/CRAVA/crava/tree/master/libs/nrlib
@@ -10,12 +10,14 @@ set(NRLIB_GITHUB_SHA "ba35d4359882f1c6f5e9dc30eb95fe52af50fd6f")
 # https://github.com/Ensembles/ert
 set(ERT_GITHUB_SHA "e2a5a9cc20705537d07822958d925e092a323367") 
 
-set(OPM_COMMON_GITHUB_SHA "1216bc052542f24ec6fcfbe1947d52e6300ff754")
-set(OPM_PARSER_GITHUB_SHA "a3496df501a4369fd827fbf0ff893d03deff425f")
-
+# https://github.com/OPM/opm-flowdiagnostics
 set(OPM_FLOWDIAGNOSTICS_SHA "a14dc4ba1302bcc1e0aeb35c5de6b4bd39bce98")
+
+# https://github.com/OPM/opm-flowdiagnostics-applications
 set(OPM_FLOWDIAGNOSTICS_APPLICATIONS_SHA "afd3f6c454f52dcfa0a6aa96be9823176a6daa48")
 
+# https://github.com/OPM/opm-parser/blob/master/opm/parser/eclipse/Units/Units.hpp
+# This file was moved from opm-core to opm-parser october 2016
 # sha for Units.hpp 9a679071dd0066236154852c39a9e0b3c3ac4873
 
 set(STRPRODUCTVER ${RESINSIGHT_MAJOR_VERSION}.${RESINSIGHT_MINOR_VERSION}.${RESINSIGHT_INCREMENT_VERSION})
