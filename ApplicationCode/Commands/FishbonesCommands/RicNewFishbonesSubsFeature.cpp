43e50bd8d2bb864edd12dadad4a4f5d8f734d418
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2017-05-16 12:49:36 +0200

    Updated copyright headers

--- ApplicationCode/Commands/FishbonesCommands/RicNewFishbonesSubsFeature.cpp
+++ ApplicationCode/Commands/FishbonesCommands/RicNewFishbonesSubsFeature.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2017-     Statoil ASA
+//  Copyright (C) 2017 Statoil ASA
 // 
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
