56579e7b3e103167cf8878910a9c1034c5c311f6
Jacob Støren <jacob.storen@ceetronSolutions.com>
2017-06-02 09:23:24 +0200

    #1487 Separated fracture files in Rim CMakeLists file

--- ApplicationCode/ProjectDataModel/Completions/CMakeLists_files.cmake
+++ ApplicationCode/ProjectDataModel/Completions/CMakeLists_files.cmake
@@ -5,55 +5,55 @@ if (${CMAKE_VERSION} VERSION_GREATER "2.8.2")
 endif()
 
 set (SOURCE_GROUP_HEADER_FILES
-${CEE_CURRENT_LIST_DIR}RimEllipseFractureTemplate.h
 ${CEE_CURRENT_LIST_DIR}RimCompletionCellIntersectionCalc.h
 ${CEE_CURRENT_LIST_DIR}RimFishbonesCollection.h
 ${CEE_CURRENT_LIST_DIR}RimFishbonesMultipleSubs.h
 ${CEE_CURRENT_LIST_DIR}RimFishbonesPipeProperties.h
 ${CEE_CURRENT_LIST_DIR}RimFishboneWellPath.h
 ${CEE_CURRENT_LIST_DIR}RimFishboneWellPathCollection.h
-${CEE_CURRENT_LIST_DIR}RimFracture.h
-${CEE_CURRENT_LIST_DIR}RimFractureExportSettings.h
-${CEE_CURRENT_LIST_DIR}RimFractureTemplate.h
-${CEE_CURRENT_LIST_DIR}RimFractureTemplateCollection.h
 ${CEE_CURRENT_LIST_DIR}RimPerforationCollection.h
 ${CEE_CURRENT_LIST_DIR}RimPerforationInterval.h
-${CEE_CURRENT_LIST_DIR}RimSimWellFracture.h
-${CEE_CURRENT_LIST_DIR}RimSimWellFractureCollection.h
-${CEE_CURRENT_LIST_DIR}RimStimPlanFractureTemplate.h
 ${CEE_CURRENT_LIST_DIR}RimWellPathCompletions.h
 
 
 # Fracture files goes here
-
+${CEE_CURRENT_LIST_DIR}RimEllipseFractureTemplate.h
+${CEE_CURRENT_LIST_DIR}RimFracture.h
+${CEE_CURRENT_LIST_DIR}RimFractureExportSettings.h
+${CEE_CURRENT_LIST_DIR}RimFractureTemplate.h
+${CEE_CURRENT_LIST_DIR}RimFractureTemplateCollection.h
+${CEE_CURRENT_LIST_DIR}RimSimWellFracture.h
+${CEE_CURRENT_LIST_DIR}RimSimWellFractureCollection.h
+${CEE_CURRENT_LIST_DIR}RimStimPlanFractureTemplate.h
 ${CEE_CURRENT_LIST_DIR}RimWellPathFracture.h
 ${CEE_CURRENT_LIST_DIR}RimWellPathFractureCollection.h
+
 )
 
 set (SOURCE_GROUP_SOURCE_FILES
 ${CEE_CURRENT_LIST_DIR}RimCompletionCellIntersectionCalc.cpp
-${CEE_CURRENT_LIST_DIR}RimEllipseFractureTemplate.cpp
 ${CEE_CURRENT_LIST_DIR}RimFishbonesCollection.cpp
 ${CEE_CURRENT_LIST_DIR}RimFishbonesMultipleSubs.cpp
 ${CEE_CURRENT_LIST_DIR}RimFishbonesPipeProperties.cpp
 ${CEE_CURRENT_LIST_DIR}RimFishboneWellPath.cpp
 ${CEE_CURRENT_LIST_DIR}RimFishboneWellPathCollection.cpp
-${CEE_CURRENT_LIST_DIR}RimFracture.cpp
-${CEE_CURRENT_LIST_DIR}RimFractureExportSettings.cpp
-${CEE_CURRENT_LIST_DIR}RimFractureTemplate.cpp
-${CEE_CURRENT_LIST_DIR}RimFractureTemplateCollection.cpp
 ${CEE_CURRENT_LIST_DIR}RimPerforationCollection.cpp
 ${CEE_CURRENT_LIST_DIR}RimPerforationInterval.cpp
-${CEE_CURRENT_LIST_DIR}RimSimWellFracture.cpp
-${CEE_CURRENT_LIST_DIR}RimSimWellFractureCollection.cpp
-${CEE_CURRENT_LIST_DIR}RimStimPlanFractureTemplate.cpp
 ${CEE_CURRENT_LIST_DIR}RimWellPathCompletions.cpp
 
 
 # Fracture files goes here
-
+${CEE_CURRENT_LIST_DIR}RimEllipseFractureTemplate.cpp
+${CEE_CURRENT_LIST_DIR}RimFracture.cpp
+${CEE_CURRENT_LIST_DIR}RimFractureExportSettings.cpp
+${CEE_CURRENT_LIST_DIR}RimFractureTemplate.cpp
+${CEE_CURRENT_LIST_DIR}RimFractureTemplateCollection.cpp
+${CEE_CURRENT_LIST_DIR}RimSimWellFracture.cpp
+${CEE_CURRENT_LIST_DIR}RimSimWellFractureCollection.cpp
+${CEE_CURRENT_LIST_DIR}RimStimPlanFractureTemplate.cpp
 ${CEE_CURRENT_LIST_DIR}RimWellPathFracture.cpp
 ${CEE_CURRENT_LIST_DIR}RimWellPathFractureCollection.cpp
+
 )
 
 list(APPEND CODE_HEADER_FILES
