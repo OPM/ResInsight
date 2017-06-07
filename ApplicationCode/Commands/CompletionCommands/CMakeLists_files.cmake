960948e68434da6ecef455e6457e8cf328c76ad8
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2017-06-07 15:57:42 +0200

    Move perforation commands to completion commands

--- ApplicationCode/Commands/CompletionCommands/CMakeLists_files.cmake
+++ ApplicationCode/Commands/CompletionCommands/CMakeLists_files.cmake
@@ -6,12 +6,15 @@ endif()
 
 set (SOURCE_GROUP_HEADER_FILES
 ${CEE_CURRENT_LIST_DIR}RicCaseAndFileExportSettingsUi.h
+${CEE_CURRENT_LIST_DIR}RicEditPerforationCollectionFeature.h
 ${CEE_CURRENT_LIST_DIR}RicExportCompletionDataSettingsUi.h
 ${CEE_CURRENT_LIST_DIR}RicExportFishbonesLateralsFeature.h
 ${CEE_CURRENT_LIST_DIR}RicExportFishbonesWellSegmentsFeature.h
 ${CEE_CURRENT_LIST_DIR}RicExportWellSegmentsSettingsUi.h
 ${CEE_CURRENT_LIST_DIR}RicNewFishbonesSubsAtMeasuredDepthFeature.h
 ${CEE_CURRENT_LIST_DIR}RicNewFishbonesSubsFeature.h
+${CEE_CURRENT_LIST_DIR}RicNewPerforationIntervalFeature.h
+${CEE_CURRENT_LIST_DIR}RicNewPerforationIntervalAtMeasuredDepthFeature.h
 ${CEE_CURRENT_LIST_DIR}RicWellPathExportCompletionDataFeature.h
 ${CEE_CURRENT_LIST_DIR}RicWellPathImportCompletionsFileFeature.h
 ${CEE_CURRENT_LIST_DIR}RicWellPathImportPerforationIntervalsFeature.h
@@ -19,12 +22,15 @@ ${CEE_CURRENT_LIST_DIR}RicWellPathImportPerforationIntervalsFeature.h
 
 set (SOURCE_GROUP_SOURCE_FILES
 ${CEE_CURRENT_LIST_DIR}RicCaseAndFileExportSettingsUi.cpp
+${CEE_CURRENT_LIST_DIR}RicEditPerforationCollectionFeature.cpp
 ${CEE_CURRENT_LIST_DIR}RicExportCompletionDataSettingsUi.cpp
 ${CEE_CURRENT_LIST_DIR}RicExportFishbonesLateralsFeature.cpp
 ${CEE_CURRENT_LIST_DIR}RicExportFishbonesWellSegmentsFeature.cpp
 ${CEE_CURRENT_LIST_DIR}RicExportWellSegmentsSettingsUi.cpp
 ${CEE_CURRENT_LIST_DIR}RicNewFishbonesSubsAtMeasuredDepthFeature.cpp
 ${CEE_CURRENT_LIST_DIR}RicNewFishbonesSubsFeature.cpp
+${CEE_CURRENT_LIST_DIR}RicNewPerforationIntervalFeature.cpp
+${CEE_CURRENT_LIST_DIR}RicNewPerforationIntervalAtMeasuredDepthFeature.cpp
 ${CEE_CURRENT_LIST_DIR}RicWellPathExportCompletionDataFeature.cpp
 ${CEE_CURRENT_LIST_DIR}RicWellPathImportCompletionsFileFeature.cpp
 ${CEE_CURRENT_LIST_DIR}RicWellPathImportPerforationIntervalsFeature.cpp
