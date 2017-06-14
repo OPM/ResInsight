12bbaa898df4c37caf7e1f5c7a046d65d4033e90
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2017-06-14 07:53:28 +0200

    Added several missing copyright headers

--- ApplicationCode/ProjectDataModel/RimTreeViewStateSerializer.cpp
+++ ApplicationCode/ProjectDataModel/RimTreeViewStateSerializer.cpp
@@ -1,4 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2017     Statoil ASA
+// 
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+// 
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
 #include "RimTreeViewStateSerializer.h"
+
 #include <QTreeView>
 
 //--------------------------------------------------------------------------------------------------
@@ -124,6 +143,4 @@ void RimTreeViewStateSerializer::encodeStringFromModelIndex(const QModelIndex mi
         localModelIdx = localModelIdx.parent();
     }
 }
-
-
 
