6be1f62c90d388d8d2f65604c876a459052342b5
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2017-07-27 10:12:57 +0200

    #1665 Add exportSnapshots command to command file interface

---
+++ ApplicationCode/CommandFileInterface/RicfExportSnapshots.cpp
@@ -0,0 +1,76 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2017 Statoil ASA
+// 
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+// 
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RicfExportSnapshots.h"
+
+#include "RicfCommandFileExecutor.h"
+
+#include "ExportCommands/RicSnapshotViewToClipboardFeature.h"
+
+#include "RiaApplication.h"
+
+#include "RiuMainWindow.h"
+
+#include <QFileInfo>
+
+CAF_PDM_SOURCE_INIT(RicfExportSnapshots, "exportSnapshots");
+
+namespace caf {
+    template<>
+    void RicfExportSnapshots::SnapshotsTypeEnum::setUp()
+    {
+        addItem(RicfExportSnapshots::ALL,   "ALL",   "All");
+        addItem(RicfExportSnapshots::VIEWS, "VIEWS", "Views");
+        addItem(RicfExportSnapshots::PLOTS, "PLOTS", "Plots");
+        setDefault(RicfExportSnapshots::ALL);
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+RicfExportSnapshots::RicfExportSnapshots()
+{
+    RICF_InitField(&m_type, "type",  RicfExportSnapshots::SnapshotsTypeEnum(), "Type",  "", "", "");
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+void RicfExportSnapshots::execute()
+{
+    RiuMainWindow* mainWnd = RiuMainWindow::instance();
+    CVF_ASSERT(mainWnd);
+    mainWnd->hideAllDockWindows();
+
+    QString absolutePathToSnapshotDir = RicfCommandFileExecutor::instance()->getExportPath(RicfCommandFileExecutor::SNAPSHOTS);
+    if (absolutePathToSnapshotDir.isNull())
+    {
+        absolutePathToSnapshotDir = RiaApplication::instance()->createAbsolutePathFromProjectRelativePath("snapshots");
+    }
+    if (m_type == RicfExportSnapshots::VIEWS || m_type == RicfExportSnapshots::ALL)
+    {
+        RiaApplication::instance()->saveSnapshotForAllViews(absolutePathToSnapshotDir);
+    }
+    if (m_type == RicfExportSnapshots::PLOTS || m_type == RicfExportSnapshots::ALL)
+    {
+        RicSnapshotAllPlotsToFileFeature::exportSnapshotOfAllPlotsIntoFolder(absolutePathToSnapshotDir);
+    }
+
+    mainWnd->loadWinGeoAndDockToolBarLayout();
+}
---
+++ ApplicationCode/CommandFileInterface/RicfExportSnapshots.h
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2017 Statoil ASA
+// 
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+// 
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "RicfCommandObject.h"
+
+#include "cafAppEnum.h"
+#include "cafPdmField.h"
+
+class RicfExportSnapshots : public RicfCommandObject
+{
+    CAF_PDM_HEADER_INIT;
+
+public:
+    enum SnapshotsType
+    {
+        VIEWS,
+        PLOTS,
+        ALL
+    };
+    typedef caf::AppEnum<SnapshotsType> SnapshotsTypeEnum;
+
+public:
+    RicfExportSnapshots();
+
+    virtual void execute() override;
+
+private:
+    caf::PdmField<SnapshotsTypeEnum> m_type;
+};
