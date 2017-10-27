f229ce5c2348d64e5fe701d7c664709bd02fe61a
Bjørn Erik Jensen <bjorn.erik.jensen@acando.no>
2017-10-27 09:35:38 +0200

    PdmStreamOperator. Set timespec to UTC for QDateTime fields during project load

--- Fwk/AppFwk/cafProjectDataModel/cafPdmXml/cafInternalPdmStreamOperators.cpp
+++ Fwk/AppFwk/cafProjectDataModel/cafPdmXml/cafInternalPdmStreamOperators.cpp
@@ -31,6 +31,7 @@ QTextStream&  operator >> (QTextStream& str, QDateTime& value)
     QString text;
     str >> text;
     value = QDateTime::fromString(text, "yyyy_MM_dd-HH:mm:ss");
+    value.setTimeSpec(Qt::UTC);
     return str;
 }
 
