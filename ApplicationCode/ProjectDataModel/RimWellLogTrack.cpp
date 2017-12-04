99dd255ee864788869430d8bbe8f6c762badca5c
Rebecca Cox <rebecca.cox@acando.no>
2017-12-04 10:26:34 +0100

    Formations: Change group name

--- ApplicationCode/ProjectDataModel/RimWellLogTrack.cpp
+++ ApplicationCode/ProjectDataModel/RimWellLogTrack.cpp
@@ -788,7 +788,7 @@ void RimWellLogTrack::defineUiOrdering(QString uiConfigName, caf::PdmUiOrdering&
 {
     uiOrdering.add(&m_userName);
 
-    caf::PdmUiGroup* formationGroup = uiOrdering.addNewGroup("Formation Names Properties");
+    caf::PdmUiGroup* formationGroup = uiOrdering.addNewGroup("Formation Names");
     
     formationGroup->add(&m_showFormations);
 
