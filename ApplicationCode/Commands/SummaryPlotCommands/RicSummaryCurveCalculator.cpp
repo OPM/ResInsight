OPM/ResInsight

commit 8281391fb9b45789213c275e2e9208ac54690d2e

#2211 Curve calc. Create new calculation if empty

ApplicationCode/Commands/SummaryPlotCommands/RicSummaryCurveCalculator.cpp
@@ -51,6 +51,11 @@ RicSummaryCurveCalculator::RicSummaryCurveCalculator()
     RicSummaryCurveCalculator::assignPushButtonEditor(&m_deleteCalculation);
 
     m_calcContextMenuMgr = std::unique_ptr<RiuCalculationsContextMenuManager>(new RiuCalculationsContextMenuManager());
+
+    if (!m_currentCalculation)
+    {
+        createNewCalculation();
+    }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -119,8 +124,7 @@ void RicSummaryCurveCalculator::fieldChangedByUi(const caf::PdmFieldHandle* chan
     {
         m_newCalculation = false;
 
-        RimSummaryCalculation* rimCalc = calculationCollection()->addCalculation();
-        m_currentCalculation = rimCalc;
+        createNewCalculation();
 
         this->updateConnectedEditors();
     }
@@ -287,4 +291,13 @@ void RicSummaryCurveCalculator::onEditorWidgetsCreated()
 
         m_calcContextMenuMgr->attachWidget(widget, this);
     }
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+void RicSummaryCurveCalculator::createNewCalculation()
+{
+    RimSummaryCalculation* rimCalc = calculationCollection()->addCalculation();
+    m_currentCalculation = rimCalc;
 }

ApplicationCode/Commands/SummaryPlotCommands/RicSummaryCurveCalculator.h
@@ -56,6 +56,8 @@ private:
     virtual void onEditorWidgetsCreated();
 
 private:
+    void        createNewCalculation();
+
     // TODO : Move to a common caf helper class
     static void assignPushButtonEditor(caf::PdmFieldHandle* fieldHandle);
     static void assignPushButtonEditorText(caf::PdmUiEditorAttribute* attribute, const QString& text);

commit f79e31be8f16df99bf3ee320685ff02516a200a6

#2216 Disable creation of new calculation object as this causes crash
Introduced in  8281391fb9b45789213c275e2e9208ac54690d2e

ApplicationCode/Commands/SummaryPlotCommands/RicSummaryCurveCalculator.cpp
@@ -51,11 +51,6 @@ RicSummaryCurveCalculator::RicSummaryCurveCalculator()
     RicSummaryCurveCalculator::assignPushButtonEditor(&m_deleteCalculation);
 
     m_calcContextMenuMgr = std::unique_ptr<RiuCalculationsContextMenuManager>(new RiuCalculationsContextMenuManager());
-
-    if (!m_currentCalculation)
-    {
-        createNewCalculation();
-    }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -124,7 +119,8 @@ void RicSummaryCurveCalculator::fieldChangedByUi(const caf::PdmFieldHandle* chan
     {
         m_newCalculation = false;
 
-        createNewCalculation();
+        RimSummaryCalculation* rimCalc = calculationCollection()->addCalculation();
+        m_currentCalculation = rimCalc;
 
         this->updateConnectedEditors();
     }
@@ -291,13 +287,4 @@ void RicSummaryCurveCalculator::onEditorWidgetsCreated()
 
         m_calcContextMenuMgr->attachWidget(widget, this);
     }
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-void RicSummaryCurveCalculator::createNewCalculation()
-{
-    RimSummaryCalculation* rimCalc = calculationCollection()->addCalculation();
-    m_currentCalculation = rimCalc;
 }

ApplicationCode/Commands/SummaryPlotCommands/RicSummaryCurveCalculator.h
@@ -56,8 +56,6 @@ private:
     virtual void onEditorWidgetsCreated();
 
 private:
-    void        createNewCalculation();
-
     // TODO : Move to a common caf helper class
     static void assignPushButtonEditor(caf::PdmFieldHandle* fieldHandle);
     static void assignPushButtonEditorText(caf::PdmUiEditorAttribute* attribute, const QString& text);