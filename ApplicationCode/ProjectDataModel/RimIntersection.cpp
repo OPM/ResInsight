e344fcd47a07b98e565439ffcbfebefe0518859a
Jacob Støren <jacob.storen@ceetronsolutions.com>
2017-12-05 14:03:40 +0100

    #2156 Fixed crash when loading project with intersection referencing missing well path file

--- ApplicationCode/ProjectDataModel/RimIntersection.cpp
+++ ApplicationCode/ProjectDataModel/RimIntersection.cpp
@@ -395,7 +395,7 @@ std::vector< std::vector <cvf::Vec3d> > RimIntersection::polyLines() const
     std::vector< std::vector <cvf::Vec3d> > lines;
     if (type == CS_WELL_PATH)
     {
-        if (wellPath())
+        if (wellPath() && wellPath->wellPathGeometry() )
         {
             lines.push_back(wellPath->wellPathGeometry()->m_wellPathPoints);
             clipToReservoir(lines[0]);
