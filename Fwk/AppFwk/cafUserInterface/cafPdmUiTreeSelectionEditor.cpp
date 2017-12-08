2c4b1e453d98bcff3b2504769421beae4343a09a
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2017-12-08 16:52:31 +0100

    #2197 Rollback : placeholderText was introduced in Qt 4.7

--- Fwk/AppFwk/cafUserInterface/cafPdmUiTreeSelectionEditor.cpp
+++ Fwk/AppFwk/cafUserInterface/cafPdmUiTreeSelectionEditor.cpp
@@ -276,7 +276,11 @@ QWidget* PdmUiTreeSelectionEditor::createEditorWidget(QWidget* parent)
         connect(m_toggleAllCheckBox, SIGNAL(clicked(bool)), this, SLOT(slotToggleAll()));
 
         m_textFilterLineEdit = new QLineEdit();
-        m_textFilterLineEdit->setPlaceholderText("Click to add filter");
+        
+
+        // TODO: This feature was introduced in Qt 4.7
+        // Consider backporting to 4.6
+        //m_textFilterLineEdit->setPlaceholderText("Click to add filter");
 
         headerLayout->addWidget(m_textFilterLineEdit);
 
