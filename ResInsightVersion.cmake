adfd1f38f5565eb941f33b25662e7a593afc916f
Jacob Støren <jacob.storen@ceetronsolutions.com>
2017-12-19 00:37:36 +0100

    Upped to version 2017.05.02-dev.14 for sprint meeting

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,7 +1,7 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2017)
 set(RESINSIGHT_MINOR_VERSION 05)
-set(RESINSIGHT_INCREMENT_VERSION "2-dev.13")
+set(RESINSIGHT_INCREMENT_VERSION "2-dev.14")
 
 
 # https://github.com/CRAVA/crava/tree/master/libs/nrlib
