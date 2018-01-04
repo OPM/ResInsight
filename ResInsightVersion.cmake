780f11713d5ea11b71e4ab08d709f42f2c8006e2
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2018-01-04 13:12:49 +0100

    #2310 Refactor creation of product version string

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,8 +1,16 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2017)
 set(RESINSIGHT_MINOR_VERSION 05)
-set(RESINSIGHT_INCREMENT_VERSION "2-dev.16")
+set(RESINSIGHT_PATCH_VERSION 02)
 
+# Opional text with no restrictions
+set(RESINSIGHT_VERSION_TEXT "-dev")
+
+# Optional text
+# Must be unique and increasing within one combination of major/minor/patch version 
+# The uniqueness of this text is independent of RESINSIGHT_VERSION_TEXT 
+# Format of text must be ".xx"
+set(RESINSIGHT_DEV_VERSION ".16")
 
 # https://github.com/CRAVA/crava/tree/master/libs/nrlib
 set(NRLIB_GITHUB_SHA "ba35d4359882f1c6f5e9dc30eb95fe52af50fd6f") 
@@ -20,5 +28,5 @@ set(OPM_FLOWDIAGNOSTICS_APPLICATIONS_SHA "44f7e47ecdc87ba566ab4146629de49039a73b
 # This file was moved from opm-core to opm-parser october 2016
 # sha for Units.hpp 9a679071dd0066236154852c39a9e0b3c3ac4873
 
-set(STRPRODUCTVER ${RESINSIGHT_MAJOR_VERSION}.${RESINSIGHT_MINOR_VERSION}.${RESINSIGHT_INCREMENT_VERSION})
+set(STRPRODUCTVER ${RESINSIGHT_MAJOR_VERSION}.${RESINSIGHT_MINOR_VERSION}.${RESINSIGHT_PATCH_VERSION}${RESINSIGHT_VERSION_TEXT}${RESINSIGHT_DEV_VERSION})
 
