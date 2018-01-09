3e11f9fbd6dc1ce1e6cdea7b547e671de689ca07
Bjørn Erik Jensen <bjorn.erik.jensen@acando.no>
2018-01-09 15:39:17 +0100

    RifElementPropertyTableReader unit tests

--- ApplicationCode/UnitTests/CMakeLists_files.cmake
+++ ApplicationCode/UnitTests/CMakeLists_files.cmake
@@ -40,6 +40,7 @@ ${CEE_CURRENT_LIST_DIR}FixedWidthDataParser-Test.cpp
 ${CEE_CURRENT_LIST_DIR}RigTimeCurveHistoryMerger-Test.cpp
 ${CEE_CURRENT_LIST_DIR}ListKeywordsForObjectsAndFields-Test.cpp
 ${CEE_CURRENT_LIST_DIR}RiaProjectFileVersionTools-Test.cpp
+${CEE_CURRENT_LIST_DIR}RicElementPropertyTableReader-Test.cpp
 )
 
 if (RESINSIGHT_ENABLE_PROTOTYPE_FEATURE_FRACTURES)
