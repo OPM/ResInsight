ecfb827b41d54c6c26fd837f829b8fe393776c19
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2018-03-16 13:40:49 +0100

    Add missing init macros in constructor

--- ApplicationCode/ProjectDataModel/Rim3dWellLogRftCurve.cpp
+++ ApplicationCode/ProjectDataModel/Rim3dWellLogRftCurve.cpp
@@ -39,10 +39,17 @@ Rim3dWellLogRftCurve::Rim3dWellLogRftCurve()
 {
     CAF_PDM_InitObject("3d Well Log RFT Curve", ":/WellLogCurve16x16.png", "", "");
 
-    m_name = "3D Well Log RFT Curve";
-    
-    m_2dWellLogRftCurve.xmlCapability()->disableIO();
+    CAF_PDM_InitFieldNoDefault(&m_eclipseResultCase, "eclipseResultCase", "", "", "", "");
+    CAF_PDM_InitFieldNoDefault(&m_timeStep, "timeStep", "", "", "", "");
+
+    CAF_PDM_InitFieldNoDefault(&m_wellLogChannelName, "wellLogChannelName", "", "", "", "");
+
+    CAF_PDM_InitFieldNoDefault(&m_2dWellLogRftCurve, "my2dWellLogRftCurve", "", "", "", "");
+
     m_2dWellLogRftCurve = new RimWellLogRftCurve();
+    m_2dWellLogRftCurve.xmlCapability()->disableIO();
+
+    m_name = "3D Well Log RFT Curve";
 }
 
 //--------------------------------------------------------------------------------------------------
