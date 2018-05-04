4713700c24269ae89f4c5ce68b76799a3eae082c
Jacob Støren <jacob.storen@ceetronsolutions.com>
2018-05-04 13:26:34 +0200

    Clean up obsolete code

--- ApplicationCode/UserInterface/RiuCvfOverlayItemWidget.cpp
+++ ApplicationCode/UserInterface/RiuCvfOverlayItemWidget.cpp
@@ -16,8 +16,18 @@
 //
 /////////////////////////////////////////////////////////////////////////////////
 
-
 #include "RiuCvfOverlayItemWidget.h"
+
+#include "RiaApplication.h"
+
+#include "cafViewer.h"
+
+#include "cvfqtUtils.h"
+#include "cvfRenderSequence.h"
+#include "cvfFramebufferObject.h"
+#include "cvfRenderbufferObject.h"
+#include "cvfRendering.h"
+#include "cvfCamera.h"
 
 #include <QApplication>
 #include <QFrame>
@@ -27,6 +37,7 @@
 #include <QResizeEvent>
 #include <QBoxLayout>
 
+#include "glew/GL/glew.h"
 //--------------------------------------------------------------------------------------------------
 /// 
 //--------------------------------------------------------------------------------------------------
@@ -49,16 +60,6 @@ RiuCvfOverlayItemWidget::~RiuCvfOverlayItemWidget()
 {
 
 }
-
-#include "cafViewer.h"
-#include "cvfRendering.h"
-#include "cvfRenderSequence.h"
-#include "cvfFramebufferObject.h"
-#include "cvfRenderbufferObject.h"
-#include "cvfqtUtils.h"
-#include "glew/GL/glew.h"
-#include "RiaApplication.h"
-#include "cvfCamera.h"
 
 //--------------------------------------------------------------------------------------------------
 /// 
@@ -131,84 +132,3 @@ void RiuCvfOverlayItemWidget::updateFromOverlyItem( cvf::OverlayItem * item)
     this->resize(QSize(width, height));
 
 }
-
-#if 0
-
-#include "cafViewer.h"
-#include "cvfRendering.h"
-#include "cvfRenderSequence.h"
-#include "cvfFramebufferObject.h"
-#include "cvfRenderbufferObject.h"
-#include "cvfqtUtils.h"
-#include "glew/GL/glew.h"
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-QPixmap RimLegendConfig::drawLegend()
-{   
-    m_scalarMapperLegend->setTitle("Hei og hopp");
-    m_scalarMapperLegend->computeLayoutAndExtents({0,0}, {100, 400});
-    unsigned int width = m_scalarMapperLegend->minimumWidth() + 100;
-    unsigned int height =  m_scalarMapperLegend->sizeHint().y();
-
-    QGLFormat glFormat;
-    glFormat.setDirectRendering(RiaApplication::instance()->useShaders());
-
-    caf::Viewer*  viewer = new caf::Viewer(glFormat, nullptr);
-    cvf::OpenGLContext* cvfOglContext = viewer->cvfOpenGLContext();
-    viewer->resize(width, height);
-
-
-    // Create a rendering
-
-    cvf::ref<cvf::Rendering> rendering = new cvf::Rendering;
-    m_scalarMapperLegend->setLayoutFixedPosition({0,0});
-    rendering->addOverlayItem(m_scalarMapperLegend.p());
-
-
-    rendering->camera()->setViewport(0,0,width, height);
-    rendering->camera()->viewport()->setClearColor({1,1,1,0});
-
-    cvf::ref<cvf::RenderSequence> renderingSequence = new cvf::RenderSequence;
-    renderingSequence->addRendering(rendering.p());
-
-    if (RiaApplication::instance()->useShaders())
-    {
-        // Set up frame and render buffers    
-
-        cvf::ref<cvf::FramebufferObject> fbo = new cvf::FramebufferObject;
-
-        cvf::ref<cvf::RenderbufferObject> rboColor = new cvf::RenderbufferObject(cvf::RenderbufferObject::RGBA, width, height);
-        cvf::ref<cvf::RenderbufferObject> rboDepth = new cvf::RenderbufferObject(cvf::RenderbufferObject::DEPTH_COMPONENT24, width, height);
-
-        fbo->attachDepthRenderbuffer(rboDepth.p());
-        fbo->attachColorRenderbuffer(0, rboColor.p());
-
-        fbo->applyOpenGL(cvfOglContext);
-        rendering->setTargetFramebuffer(fbo.p());
-        fbo->bind(cvfOglContext);
-    }
-
-    renderingSequence->render(cvfOglContext);
-
-    // Read data from framebuffer
-
-    cvf::UByteArray arr(4*width*height);
-    glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RGBA, GL_UNSIGNED_BYTE, arr.ptr());
-
-    // Create a cvf texture image
-
-    cvf::ref<cvf::TextureImage> img = new cvf::TextureImage;
-    img->setData(arr.ptr(), width, height);
-
-    QImage image = cvfqt::Utils::toQImage(*img.p());
-    //image.save("jjsLegendImageTest.png");
-
-    QPixmap pixmap = QPixmap::fromImage(image);
-
-    delete viewer;
-
-    return pixmap;
-}
-#endif
