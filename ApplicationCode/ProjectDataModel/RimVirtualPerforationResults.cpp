b6979f32c6c2b6857a34f958c7d619d0619144e2
Jacob Støren <jacob.storen@ceetronsolutions.com>
2018-05-07 15:56:23 +0200

    #2808 Add Icon for well CF's

--- ApplicationCode/ProjectDataModel/RimVirtualPerforationResults.cpp
+++ ApplicationCode/ProjectDataModel/RimVirtualPerforationResults.cpp
@@ -33,7 +33,7 @@ RimVirtualPerforationResults::RimVirtualPerforationResults()
 
     QString connectionFactorUiName = "Well Connection Factors";
 
-    CAF_PDM_InitObject(connectionFactorUiName, ":/CellResult.png", "", "");
+    CAF_PDM_InitObject(connectionFactorUiName, ":/WellCF16x16.png", "", "");
 
     CAF_PDM_InitField(&m_isActive,              "ShowConnectionFactors",    false,   "", "", "", "");
     CAF_PDM_InitField(&m_showClosedConnections, "ShowClosedConnections",    true,   "Show On Closed Connections", "", "", "");
