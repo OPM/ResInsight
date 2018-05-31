3505b9d31ad0026bf0a4863423fd707e911ea6ff
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2018-05-31 15:21:47 +0200

    Debug Tools : Add globalCellIndicesToOneBasedIJKText used as input to cell based range filter

--- ApplicationCode/ReservoirDataModel/RigReservoirGridTools.h
+++ ApplicationCode/ReservoirDataModel/RigReservoirGridTools.h
@@ -1,22 +1,26 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2017 Statoil ASA
-// 
+//
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
 //  WARRANTY; without even the implied warranty of MERCHANTABILITY or
 //  FITNESS FOR A PARTICULAR PURPOSE.
-// 
-//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 //  for more details.
 //
 /////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
+
+#include "RigMainGrid.h"
+
+#include <QString>
 
 class RigActiveCellInfo;
 class RigFemPartCollection;
@@ -26,14 +30,14 @@ class Rim3dView;
 
 namespace cvf
 {
-    class StructGridInterface;
+class StructGridInterface;
 }
 
 class QString;
 
 //==================================================================================================
-///  
-///  
+///
+///
 //==================================================================================================
 class RigReservoirGridTools
 {
@@ -43,10 +47,36 @@ public:
     static const cvf::StructGridInterface* gridByIndex(RimCase* rimCase, int gridIndex);
     static QString                         gridName(RimCase* rimCase, int gridIndex);
 
-    static const RigActiveCellInfo*        activeCellInfo(Rim3dView* rimView);
+    static const RigActiveCellInfo* activeCellInfo(Rim3dView* rimView);
+
+    template<class InputIterator>
+    static QString globalCellIndicesToOneBasedIJKText(InputIterator first, InputIterator last, const RigMainGrid* mainGrid)
+    {
+        QString txt;
+
+        if (mainGrid)
+        {
+            while (first != last)
+            {
+                size_t globalCellIndex = *first;
+                size_t i, j, k;
+                mainGrid->ijkFromCellIndex(globalCellIndex, &i, &j, &k);
+                i++;
+                j++;
+                k++;
+
+                QString itemText = QString("%1 %2 %3").arg(i).arg(j).arg(k);
+
+                txt += itemText + "\n";
+
+                ++first;
+            }
+        }
+
+        return txt;
+    }
 
 private:
-    static RigMainGrid*             eclipseMainGrid(RimCase* rimCase);
-    static RigFemPartCollection*    geoMechPartCollection(RimCase* rimCase);
+    static RigMainGrid*          eclipseMainGrid(RimCase* rimCase);
+    static RigFemPartCollection* geoMechPartCollection(RimCase* rimCase);
 };
-
