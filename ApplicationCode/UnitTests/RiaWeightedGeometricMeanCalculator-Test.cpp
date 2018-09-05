bddb8fb6fb9af8157dc7384b89784f21c3943fc9
Bjørn Erik Jensen <bjorn.erik.jensen@acando.no>
2018-09-05 15:11:03 +0200

    Add missing include

--- ApplicationCode/UnitTests/RiaWeightedGeometricMeanCalculator-Test.cpp
+++ ApplicationCode/UnitTests/RiaWeightedGeometricMeanCalculator-Test.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 
 #include "RiaWeightedGeometricMeanCalculator.h"
-
+#include <cmath>
 
 //--------------------------------------------------------------------------------------------------
 /// 
