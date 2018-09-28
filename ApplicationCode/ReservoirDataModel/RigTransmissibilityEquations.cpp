c21655f2b4eabcb3739754b69c5f9d289028a802
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2018-09-28 09:03:14 +0200

    Transmissibiliy Equations : Move function to private section

--- ApplicationCode/ReservoirDataModel/RigTransmissibilityEquations.cpp
+++ ApplicationCode/ReservoirDataModel/RigTransmissibilityEquations.cpp
@@ -1,17 +1,17 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2017-     Statoil ASA
-// 
+//
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
 //  WARRANTY; without even the implied warranty of MERCHANTABILITY or
 //  FITNESS FOR A PARTICULAR PURPOSE.
-// 
-//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 //  for more details.
 //
 /////////////////////////////////////////////////////////////////////////////////
@@ -23,47 +23,24 @@
 
 #include <cmath>
 
-
 //--------------------------------------------------------------------------------------------------
-/// 
+///
 //--------------------------------------------------------------------------------------------------
-double RigTransmissibilityEquations::peacemanRadius(double permeabilityNormalDirection1, 
-                                                    double permeabilityNormalDirection2, 
-                                                    double cellSizeNormalDirection1, 
-                                                    double cellSizeNormalDirection2)
-{
-    double numerator = cvf::Math::sqrt(
-                       pow(cellSizeNormalDirection2, 2.0) * pow(permeabilityNormalDirection1 / permeabilityNormalDirection2, 0.5)
-                     + pow(cellSizeNormalDirection1, 2.0) * pow(permeabilityNormalDirection2 / permeabilityNormalDirection1, 0.5) );
-
-    double denominator = pow((permeabilityNormalDirection1 / permeabilityNormalDirection2), 0.25 )
-                       + pow((permeabilityNormalDirection2 / permeabilityNormalDirection1), 0.25 );
-
-    double r0 = 0.28 * numerator / denominator;
-
-    return r0;
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-double RigTransmissibilityEquations::wellBoreTransmissibilityComponent(double cellPerforationVectorComponent, 
-                                                                       double permeabilityNormalDirection1, 
-                                                                       double permeabilityNormalDirection2, 
-                                                                       double cellSizeNormalDirection1, 
-                                                                       double cellSizeNormalDirection2, 
+double RigTransmissibilityEquations::wellBoreTransmissibilityComponent(double cellPerforationVectorComponent,
+                                                                       double permeabilityNormalDirection1,
+                                                                       double permeabilityNormalDirection2,
+                                                                       double cellSizeNormalDirection1,
+                                                                       double cellSizeNormalDirection2,
                                                                        double wellRadius,
-                                                                       double skinFactor, 
+                                                                       double skinFactor,
                                                                        double cDarcyForRelevantUnit)
 {
     double K = cvf::Math::sqrt(permeabilityNormalDirection1 * permeabilityNormalDirection2);
 
     double nominator = cDarcyForRelevantUnit * 2 * cvf::PI_D * K * cellPerforationVectorComponent;
-    
-    double peaceManRad = peacemanRadius(permeabilityNormalDirection1, 
-                                        permeabilityNormalDirection2, 
-                                        cellSizeNormalDirection1, 
-                                        cellSizeNormalDirection2);
+
+    double peaceManRad = peacemanRadius(
+        permeabilityNormalDirection1, permeabilityNormalDirection2, cellSizeNormalDirection1, cellSizeNormalDirection2);
 
     double denominator = log(peaceManRad / wellRadius) + skinFactor;
 
@@ -72,11 +49,29 @@ double RigTransmissibilityEquations::wellBoreTransmissibilityComponent(double ce
 }
 
 //--------------------------------------------------------------------------------------------------
-/// 
+///
 //--------------------------------------------------------------------------------------------------
 double RigTransmissibilityEquations::totalConnectionFactor(double transX, double transY, double transZ)
 {
-    return  cvf::Math::sqrt(
-        pow(transX, 2.0) + pow(transY, 2.0) + pow(transZ, 2.0));
+    return cvf::Math::sqrt(pow(transX, 2.0) + pow(transY, 2.0) + pow(transZ, 2.0));
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+double RigTransmissibilityEquations::peacemanRadius(double permeabilityNormalDirection1,
+                                                    double permeabilityNormalDirection2,
+                                                    double cellSizeNormalDirection1,
+                                                    double cellSizeNormalDirection2)
+{
+    double numerator = cvf::Math::sqrt(
+        pow(cellSizeNormalDirection2, 2.0) * pow(permeabilityNormalDirection1 / permeabilityNormalDirection2, 0.5) +
+        pow(cellSizeNormalDirection1, 2.0) * pow(permeabilityNormalDirection2 / permeabilityNormalDirection1, 0.5));
+
+    double denominator = pow((permeabilityNormalDirection1 / permeabilityNormalDirection2), 0.25) +
+                         pow((permeabilityNormalDirection2 / permeabilityNormalDirection1), 0.25);
+
+    double r0 = 0.28 * numerator / denominator;
+
+    return r0;
+}
--- ApplicationCode/ReservoirDataModel/RigTransmissibilityEquations.h
+++ ApplicationCode/ReservoirDataModel/RigTransmissibilityEquations.h
@@ -1,52 +1,47 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2017-     Statoil ASA
-// 
+//
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
 //  WARRANTY; without even the implied warranty of MERCHANTABILITY or
 //  FITNESS FOR A PARTICULAR PURPOSE.
-// 
-//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 //  for more details.
 //
 /////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
-
 class RigTransmissibilityEquations
 {
 public:
-
     // Calculations are assuming an orthogonal coordinate system
 
-    // If using wellBoreTransmissibilityComponent to calculate Tx (transmissibility in x direction), 
+    static double wellBoreTransmissibilityComponent(double cellPerforationVectorComponent,
+                                                    double permeabilityNormalDirection1,
+                                                    double permeabilityNormalDirection2,
+                                                    double cellSizeNormalDirection1,
+                                                    double cellSizeNormalDirection2,
+                                                    double wellRadius,
+                                                    double skinFactor,
+                                                    double cDarcyForRelevantUnit);
+
+    static double totalConnectionFactor(double transX, double transY, double transZ);
+
+private:
+    // If using wellBoreTransmissibilityComponent to calculate Tx (transmissibility in x direction),
     // perforationVectorComponent is the x component (in the cell local coordinate system) of the perforation vector
     // permeability and cell size for Z and Y are to be specified as "normal directions" 1 and 2
     // but normal directions 1 and 2 are interchangeable (so Z=1, Y=2 and Z=2, Y=1 gives same result)
 
-    static double               peacemanRadius(double permeabilityNormalDirection1,
-                                               double permeabilityNormalDirection2,
-                                               double cellSizeNormalDirection1,
-                                               double cellSizeNormalDirection2);
-
-    static double               wellBoreTransmissibilityComponent(double cellPerforationVectorComponent, 
-                                                                  double permeabilityNormalDirection1,
-                                                                  double permeabilityNormalDirection2,
-                                                                  double cellSizeNormalDirection1,
-                                                                  double cellSizeNormalDirection2,
-                                                                  double wellRadius, 
-                                                                  double skinFactor, 
-                                                                  double cDarcyForRelevantUnit);
-
-    static double               totalConnectionFactor(double transX,
-                                                      double transY,
-                                                      double transZ);
-
+    static double peacemanRadius(double permeabilityNormalDirection1,
+                                 double permeabilityNormalDirection2,
+                                 double cellSizeNormalDirection1,
+                                 double cellSizeNormalDirection2);
 };
-
