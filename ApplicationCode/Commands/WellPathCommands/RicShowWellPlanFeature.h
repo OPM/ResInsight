0565e6e7e1a923442daf11cd3ecbdceffb8d3bd8
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2018-10-10 08:22:36 +0200

    System : Use uppercase E in copyright header

--- ApplicationCode/Commands/WellPathCommands/RicShowWellPlanFeature.h
+++ ApplicationCode/Commands/WellPathCommands/RicShowWellPlanFeature.h
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2018 equinor ASA
+//  Copyright (C) 2018 Equinor ASA
 // 
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
