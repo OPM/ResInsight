2e8c2e28a8957ac4211b0f3417bb14712e4de560
Gaute Lindkvist <lindkvis@gmail.com>
2018-11-30 14:01:29 +0100

    #3780 Get rid of debug output from painter
    * don't really need the boundingRect override for RiuQwtSymbol
    * And it needs a painter
    * Just get rid of it and pass in the painter for the labelBoundingRect

--- ApplicationCode/UserInterface/RiuQwtSymbol.h
+++ ApplicationCode/UserInterface/RiuQwtSymbol.h
@@ -55,15 +55,13 @@ public:
 
     RiuQwtSymbol(PointSymbolEnum riuStyle, const QString& label, LabelPosition labelPosition = LabelAboveSymbol);
 
-    void renderSymbols(QPainter *painter, const QPointF *points, int numPoints) const override;
-    void         renderSymbolLabel(QPainter *painter, const QPointF& position) const;
+    void    renderSymbols(QPainter *painter, const QPointF *points, int numPoints) const override;
+    void    renderSymbolLabel(QPainter *painter, const QPointF& position) const;
     QString label() const { return m_label; }
 
     void    setLabelPosition(LabelPosition labelPosition);
-    QRect boundingRect() const override;
-
 private:
-    QRect   labelBoundingRect(const QRect& symbolRect) const;
+    QRect   labelBoundingRect(const QPainter* painter, const QRect& symbolRect) const;
 
 private:
     QString       m_label;
