c1c897d7a4bfa57e95bf9b740e101ea1e11878e9
Gaute Lindkvist <lindkvis@gmail.com>
2018-12-05 08:23:06 +0100

    #3616 Make 1m the default length of packers

--- ApplicationCode/ProjectDataModel/RimWellPathAttribute.cpp
+++ ApplicationCode/ProjectDataModel/RimWellPathAttribute.cpp
@@ -229,14 +229,14 @@ void RimWellPathAttribute::fieldChangedByUi(const caf::PdmFieldHandle* changedFi
         }
         else if (m_type() == RiaDefines::PACKER)
         {
-            m_endMD = m_startMD + 2;
+            m_endMD = m_startMD + 1;
         }
     }
     if (changedField == &m_startMD)
     {
         if (m_type() == RiaDefines::PACKER)
         {
-            m_endMD = m_startMD + 2;
+            m_endMD = m_startMD + 1;
         }
     }
 
