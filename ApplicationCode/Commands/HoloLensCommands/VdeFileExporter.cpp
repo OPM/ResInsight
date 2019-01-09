6db80f40b91919faa0bf6816232e97a4507dff99
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-01-09 15:21:38 +0100

    #3856 Update copyright headers

--- ApplicationCode/Commands/HoloLensCommands/VdeFileExporter.cpp
+++ ApplicationCode/Commands/HoloLensCommands/VdeFileExporter.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2018     Statoil ASA
+//  Copyright (C) 2018-     Equinor ASA
 //
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
