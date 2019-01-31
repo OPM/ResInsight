b804e44a36294c5c8b211fb91c0b184aff7e8c37
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-01-31 08:06:18 +0100

    Set version to 2018.11.2-patch

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,10 +1,10 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2018)
 set(RESINSIGHT_MINOR_VERSION 11)
-set(RESINSIGHT_PATCH_VERSION 1)
+set(RESINSIGHT_PATCH_VERSION 2)
 
 # Opional text with no restrictions
-#set(RESINSIGHT_VERSION_TEXT "-patch_RC02")
+set(RESINSIGHT_VERSION_TEXT "-patch_RC01")
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
