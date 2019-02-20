10f6e2a0f6a14df9edd5f8c3387f1645dd6f45ea
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-02-20 11:35:31 +0100

    Revert "#4114 Regression Test : Remove cached pointer to eclipse case"
    This reverts commit f2146c600705280c85391f88a861a070812ae83a.

--- ApplicationCode/ProjectDataModel/RimEclipseResultDefinition.cpp
+++ ApplicationCode/ProjectDataModel/RimEclipseResultDefinition.cpp
@@ -180,6 +180,8 @@ void RimEclipseResultDefinition::simpleCopy(const RimEclipseResultDefinition* ot
 //--------------------------------------------------------------------------------------------------
 void RimEclipseResultDefinition::setEclipseCase(RimEclipseCase* eclipseCase)
 {
+    m_eclipseCase = eclipseCase;
+
     assignFlowSolutionFromCase();
 }
 
@@ -188,9 +190,9 @@ void RimEclipseResultDefinition::setEclipseCase(RimEclipseCase* eclipseCase)
 //--------------------------------------------------------------------------------------------------
 RigCaseCellResultsData* RimEclipseResultDefinition::currentGridCellResults() const
 {
-    if (!ownerEclipseCase()) return nullptr;
+    if (!m_eclipseCase) return nullptr;
 
-    return ownerEclipseCase()->results(m_porosityModel());
+    return m_eclipseCase->results(m_porosityModel());
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -513,7 +515,7 @@ QList<caf::PdmOptionItemInfo> RimEclipseResultDefinition::calculateValueOptions(
     if (fieldNeedingOptions == &m_resultTypeUiField)
     {
         bool                  hasSourSimRLFile = false;
-        RimEclipseResultCase* eclResCase       = dynamic_cast<RimEclipseResultCase*>(ownerEclipseCase());
+        RimEclipseResultCase* eclResCase       = dynamic_cast<RimEclipseResultCase*>(m_eclipseCase.p());
         if (eclResCase && eclResCase->eclipseCaseData())
         {
             hasSourSimRLFile = eclResCase->hasSourSimFile();
@@ -528,9 +530,9 @@ QList<caf::PdmOptionItemInfo> RimEclipseResultDefinition::calculateValueOptions(
         bool enableSouring = false;
 
 #ifdef ENABLE_SOURING
-        if (ownerEclipseCase())
+        if (m_eclipseCase.notNull())
         {
-            RigCaseCellResultsData* cellResultsData = ownerEclipseCase()->results(this->porosityModel());
+            RigCaseCellResultsData* cellResultsData = m_eclipseCase->results(this->porosityModel());
 
             if (cellResultsData->hasFlowDiagUsableFluxes())
             {
@@ -585,7 +587,7 @@ QList<caf::PdmOptionItemInfo> RimEclipseResultDefinition::calculateValueOptions(
         }
         else if (fieldNeedingOptions == &m_flowSolutionUiField)
         {
-            RimEclipseResultCase* eclCase = dynamic_cast<RimEclipseResultCase*>(ownerEclipseCase());
+            RimEclipseResultCase* eclCase = dynamic_cast<RimEclipseResultCase*>(m_eclipseCase.p());
             if (eclCase)
             {
                 std::vector<RimFlowDiagSolution*> flowSols = eclCase->flowDiagSolutions();
@@ -918,11 +920,11 @@ void RimEclipseResultDefinition::loadResult()
 
     if (isFlowDiagOrInjectionFlooding()) return; // Will load automatically on access
 
-    if (ownerEclipseCase())
+    if (m_eclipseCase)
     {
-        if (!ownerEclipseCase()->ensureReservoirCaseIsOpen())
+        if (!m_eclipseCase->ensureReservoirCaseIsOpen())
         {
-            RiaLogging::error("Could not open the Eclipse Grid file: " + ownerEclipseCase()->gridFileName());
+            RiaLogging::error("Could not open the Eclipse Grid file: " + m_eclipseCase->gridFileName());
             return;
         }
     }
@@ -931,7 +933,7 @@ void RimEclipseResultDefinition::loadResult()
     {
         if (!m_differenceCase->ensureReservoirCaseIsOpen())
         {
-            RiaLogging::error("Could not open the Eclipse Grid file: " + m_differenceCase->gridFileName());
+            RiaLogging::error("Could not open the Eclipse Grid file: " + m_eclipseCase->gridFileName());
             return;
         }
     }
@@ -946,7 +948,6 @@ void RimEclipseResultDefinition::loadResult()
 
         gridCellResults->ensureKnownResultLoaded(this->eclipseResultAddress());
     }
-  
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -1195,8 +1196,8 @@ bool RimEclipseResultDefinition::isCompletionTypeSelected() const
 //--------------------------------------------------------------------------------------------------
 bool RimEclipseResultDefinition::hasCategoryResult() const
 {
-    if (this->m_resultType() == RiaDefines::FORMATION_NAMES && ownerEclipseCase() && ownerEclipseCase()->eclipseCaseData() &&
-        ownerEclipseCase()->eclipseCaseData()->activeFormationNames())
+    if (this->m_resultType() == RiaDefines::FORMATION_NAMES && m_eclipseCase && m_eclipseCase->eclipseCaseData() &&
+        m_eclipseCase->eclipseCaseData()->activeFormationNames())
         return true;
 
     if (this->m_resultType() == RiaDefines::DYNAMIC_NATIVE && this->resultVariable() == RiaDefines::completionTypeResultName())
@@ -1366,7 +1367,7 @@ void RimEclipseResultDefinition::assignFlowSolutionFromCase()
 {
     RimFlowDiagSolution* defaultFlowDiagSolution = nullptr;
 
-    RimEclipseResultCase* eclCase = dynamic_cast<RimEclipseResultCase*>(ownerEclipseCase());
+    RimEclipseResultCase* eclCase = dynamic_cast<RimEclipseResultCase*>(m_eclipseCase.p());
 
     if (eclCase)
     {
@@ -1376,24 +1377,13 @@ void RimEclipseResultDefinition::assignFlowSolutionFromCase()
 }
 
 //--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-RimEclipseCase* RimEclipseResultDefinition::ownerEclipseCase() const
-{
-    RimEclipseCase* eclipseCase = nullptr;
-    this->firstAncestorOrThisOfType(eclipseCase);
-
-    return eclipseCase;
-}
-
-//--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
 bool RimEclipseResultDefinition::hasDualPorFractureResult()
 {
-    if (ownerEclipseCase() && ownerEclipseCase()->eclipseCaseData())
+    if (m_eclipseCase && m_eclipseCase->eclipseCaseData())
     {
-        return ownerEclipseCase()->eclipseCaseData()->hasFractureResults();
+        return m_eclipseCase->eclipseCaseData()->hasFractureResults();
     }
 
     return false;
