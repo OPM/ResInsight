d3f0d0159adb9c90b201926f526a742e4b28affd
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-05-09 10:10:17 +0200

    #4266 : libecl :  Update libecl sha reference

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -18,7 +18,7 @@ set(NRLIB_GITHUB_SHA "ba35d4359882f1c6f5e9dc30eb95fe52af50fd6f")
 # https://github.com/Statoil/libecl
 # Note:
 # Apply patches fix-synthetic-odb-cases.patch and install-ert.patch after update
-set(ECL_GITHUB_SHA "7f93730c08a4d981a4b738b42146d099977572ce")
+set(ECL_GITHUB_SHA "0e1e780fd6f18ce93119061e36a4fca9711bc020")
 
 # https://github.com/OPM/opm-flowdiagnostics
 set(OPM_FLOWDIAGNOSTICS_SHA "8bb60d6111063f2b7557502ecaa329a2d5c13b41")
