721d85aa6bf3c2ec78936c74b71b872a8fe39431
Gaute Lindkvist <lindkvis@gmail.com>
2019-05-09 10:53:22 +0200

    Start of sensible client module

--- Python/ResInsight.py
+++ Python/ResInsight.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import grpc
+import os
 
 import CaseInfo_pb2
 import CaseInfo_pb2_grpc
@@ -13,8 +14,29 @@ import ProjectInfo_pb2_grpc
 
 MAX_MESSAGE_LENGTH = 32 * 1024 * 1024
 
-class ResInsight:
-	def __init__(self, location):	    
+class CommandExecutor:
+	def __init__(self, channel, commandParams):
+		self.commands      = Commands_pb2_grpc.CommandsStub(channel)
+		self.commandParams = commandParams
+	def execute(self):
+		try:
+			self.commands.Execute(self.commandParams)	
+		except grpc.RpcError as e:
+			if e.code() == grpc.StatusCode.NOT_FOUND:
+				print("Command not found")
+			else:
+				print("Other error")
+
+
+class Instance:
+	def __init__(self, port=None):
+		if port is None:
+			port = os.environ.get('RESINSIGHT_GRPC_PORT')
+		else:
+			port = str(port)
+		if port is None:
+			port = str(50051)
+		location = "localhost:" + port
 		self.channel = grpc.insecure_channel(location, options=[('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH)])
 		self.GridCount = GridInfo_pb2_grpc.GridCountStub(self.channel)		
 		self.AllGridDimensions = GridInfo_pb2_grpc.AllGridDimensionsStub(self.channel)		
@@ -27,6 +49,8 @@ class ResInsight:
 	def Empty():
 		return CaseInfo_pb2.Empty();
 	
-	def SetTimeStep(caseId, timeStep):
-		return Commands_pb2.CommandParams(setTimeStep=Commands_pb2.SetTimeStepParams(caseId=caseId, timeStep=timeStep))
+	def setTimeStep(self, caseId, timeStep):
+		return CommandExecutor(self.channel, Commands_pb2.CommandParams(setTimeStep=Commands_pb2.SetTimeStepParams(caseId=caseId, timeStep=timeStep))).execute()
 		
+	def setMainWindowSize(self, width, height):
+		return CommandExecutor(self.channel, Commands_pb2.CommandParams(setMainWindowSize=Commands_pb2.SetMainWindowSizeParams(width=width, height=height))).execute()
--- Python/SetTimeStep.py
+++ Python/SetTimeStep.py
@@ -1,20 +1,8 @@
-from ResInsight import ResInsight
-import grpc
-import sys
+import ResInsight
 
-def run():
-	try:
-		port = str(50051)
-		if len(sys.argv) > 1:
-			port = sys.argv[1]
-		resInsight  = ResInsight("localhost:" + port)
-		commands = resInsight.Commands.Execute(ResInsight.SetTimeStep(0, 2))
+# Load instance
+resInsight = ResInsight.Instance()
 
-	except grpc.RpcError as e:
-		if e.code() == grpc.StatusCode.NOT_FOUND:
-			print("Case id not found")
-		else:
-			print("Other error")
-
-if __name__ == '__main__':
-    run()
+# Run a couple of commands
+resInsight.setTimeStep(caseId=0, timeStep=3)
+resInsight.setMainWindowSize(width=800, height=500)
