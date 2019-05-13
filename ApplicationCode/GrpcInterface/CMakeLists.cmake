cdf35f0f488246e4218808a8d27974b693b71be7
Gaute Lindkvist <lindkvis@gmail.com>
2019-05-13 20:12:17 +0200

    Add python generation and copying to CMake

--- ApplicationCode/GrpcInterface/CMakeLists.cmake
+++ ApplicationCode/GrpcInterface/CMakeLists.cmake
@@ -41,18 +41,22 @@ set(_PROTOBUF_PROTOC $<TARGET_FILE:protobuf::protoc>)
 find_package(gRPC CONFIG REQUIRED NO_MODULE)
 message(STATUS "Using gRPC ${gRPC_VERSION}")
 
-set(_GRPC_GRPCPP_UNSECURE gRPC::grpc++_unsecure grpc_unsecure gpr)
+set(_GRPC_GRPCPP_UNSECURE gRPC::grpc++_unsecure gPRC::grpc_unsecure gRPC::gpr)
 set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:gRPC::grpc_cpp_plugin>)	
 set(GRPC_LIBRARIES ${_GRPC_GRPCPP_UNSECURE} ${_PROTOBUF_LIBPROTOBUF})
-
-message(STATUS ${GRPC_LIBRARIES})
 
 if (MSVC)
 	set_target_properties(${GRPC_LIBRARIES} PROPERTIES
 		MAP_IMPORTED_CONFIG_MINSIZEREL RELEASE
 		MAP_IMPORTED_CONFIG_RELWITHDEBINFO RELEASE
 	)
-endif()
+endif(MSVC)
+
+# Cannot use the nice new FindPackage modules since that is CMake 3.12+
+if(PYTHON_EXECUTABLE AND EXISTS ${PYTHON_EXECUTABLE})
+	message(STATUS "Using Python ${PYTHON_EXECUTABLE}")
+endif(PYTHON_EXECUTABLE AND EXISTS ${PYTHON_EXECUTABLE})
+
 # Proto files
 set(PROTO_FILES
 	"Empty"
@@ -63,36 +67,72 @@ set(PROTO_FILES
 	"ResInfo"
 )
 
+set(python_source_path "${CMAKE_BINARY_DIR}/Python/generated")
+
 foreach(proto_file ${PROTO_FILES})		
-	get_filename_component(hw_proto "${CMAKE_CURRENT_LIST_DIR}/GrpcProtos/${proto_file}.proto" ABSOLUTE)
-	get_filename_component(hw_proto_path "${hw_proto}" PATH)
+	get_filename_component(rips_proto "${CMAKE_CURRENT_LIST_DIR}/GrpcProtos/${proto_file}.proto" ABSOLUTE)
+	get_filename_component(rips_proto_path "${rips_proto}" PATH)
 
-	set(hw_proto_srcs "${CMAKE_BINARY_DIR}/Generated/${proto_file}.pb.cc")
-	set(hw_proto_hdrs "${CMAKE_BINARY_DIR}/Generated/${proto_file}.pb.h")
-	set(hw_grpc_srcs  "${CMAKE_BINARY_DIR}/Generated/${proto_file}.grpc.pb.cc")
-	set(hw_grpc_hdrs  "${CMAKE_BINARY_DIR}/Generated/${proto_file}.grpc.pb.h")
-
+	set(rips_proto_srcs "${CMAKE_BINARY_DIR}/Generated/${proto_file}.pb.cc")
+	set(rips_proto_hdrs "${CMAKE_BINARY_DIR}/Generated/${proto_file}.pb.h")
+	set(rips_grpc_srcs  "${CMAKE_BINARY_DIR}/Generated/${proto_file}.grpc.pb.cc")
+	set(rips_grpc_hdrs  "${CMAKE_BINARY_DIR}/Generated/${proto_file}.grpc.pb.h")
+	
 	add_custom_command(
-		OUTPUT "${hw_proto_srcs}" "${hw_proto_hdrs}" "${hw_grpc_srcs}" "${hw_grpc_hdrs}"
+		OUTPUT "${rips_proto_srcs}" "${rips_proto_hdrs}" "${rips_grpc_srcs}" "${rips_grpc_hdrs}"
 		COMMAND ${_PROTOBUF_PROTOC}
 		ARGS --grpc_out "${CMAKE_BINARY_DIR}/Generated/"
 			--cpp_out "${CMAKE_BINARY_DIR}/Generated/"
-			-I "${hw_proto_path}"
+			-I "${rips_proto_path}"
 			--plugin=protoc-gen-grpc="${_GRPC_CPP_PLUGIN_EXECUTABLE}"
-			"${hw_proto}"
-		DEPENDS "${hw_proto}"
+			"${rips_proto}"
+		DEPENDS "${rips_proto}"
 	)
 
+	if (PYTHON_EXECUTABLE AND EXISTS ${PYTHON_EXECUTABLE})
+		set(rips_proto_python "${python_source_path}/${proto_file}_pb2.py")
+		set(rips_grpc_python "${python_source_path}/${proto_file}_pb2_grpc.py")
+
+		add_custom_command(
+			OUTPUT "${rips_proto_python}" "${rips_grpc_python}"
+			COMMAND ${PYTHON_EXECUTABLE}
+			ARGS -m grpc_tools.protoc
+				-I "${rips_proto_path}"
+				--python_out "${python_source_path}"
+				--grpc_python_out "${python_source_path}"
+				"${rips_proto}"
+			DEPENDS "${rips_proto}"
+		)
+
+	endif(PYTHON_EXECUTABLE AND EXISTS ${PYTHON_EXECUTABLE})
+
 	list( APPEND GRPC_HEADER_FILES
-		 ${hw_proto_hdrs}
-		${hw_grpc_hdrs}
+		  ${rips_proto_hdrs}
+		  ${rips_grpc_hdrs}
 	)
 	
 	list( APPEND GRPC_CPP_SOURCES
-		 ${hw_proto_srcs}
-		${hw_grpc_srcs}
+		  ${rips_proto_srcs}
+		  ${rips_grpc_srcs}
+	)
+
+	list (APPEND GRPC_PYTHON_SOURCES
+	      ${rips_proto_python}
+		  ${rips_grpc_python}
 	)
 endforeach(proto_file)
+
+if (PYTHON_EXECUTABLE AND EXISTS ${PYTHON_EXECUTABLE})
+	set(PYTHON_INTERFACE_FILES
+		"ResInsight.py"
+		"CommandExample.py"
+		"GridInfoStreamingExample.py"
+		"test_sample.py")
+
+	foreach (python_interface_file ${PYTHON_INTERFACE_FILES})
+		list (APPEND GRPC_PYTHON_SOURCES "${python_source_path}/${python_interface_file}")
+	endforeach(python_interface_file)
+endif(PYTHON_EXECUTABLE AND EXISTS ${PYTHON_EXECUTABLE})
 
 list ( APPEND GRPC_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
 list ( APPEND GRPC_CPP_SOURCES ${SOURCE_GROUP_SOURCE_FILES})
