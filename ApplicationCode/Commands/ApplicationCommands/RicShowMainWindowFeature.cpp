bfe8e7176a6f195b9ffebc070974f12efbc3c315
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-05-15 13:54:05 +0200

    #4410 Dock Window State :  Restore dock widgets when opening main window

--- ApplicationCode/Commands/ApplicationCommands/RicShowMainWindowFeature.cpp
+++ ApplicationCode/Commands/ApplicationCommands/RicShowMainWindowFeature.cpp
@@ -1,17 +1,17 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2016 Statoil ASA
-// 
+//
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
 //  WARRANTY; without even the implied warranty of MERCHANTABILITY or
 //  FITNESS FOR A PARTICULAR PURPOSE.
-// 
-//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 //  for more details.
 //
 /////////////////////////////////////////////////////////////////////////////////
@@ -27,23 +27,11 @@
 CAF_CMD_SOURCE_INIT(RicShowMainWindowFeature, "RicShowMainWindowFeature");
 
 //--------------------------------------------------------------------------------------------------
-/// 
+///
 //--------------------------------------------------------------------------------------------------
-bool RicShowMainWindowFeature::isCommandEnabled()
+void RicShowMainWindowFeature::showMainWindow()
 {
-    return true;
-}
-
-//--------------------------------------------------------------------------------------------------
-/// 
-//--------------------------------------------------------------------------------------------------
-void RicShowMainWindowFeature::onActionTriggered(bool isChecked)
-{
-    this->disableModelChangeContribution();
-
     RiuMainWindow* mainWnd = RiuMainWindow::instance();
-
-    bool triggerReloadOfDockWindowVisibilities = !mainWnd->isVisible();
 
     if (mainWnd->isMinimized())
     {
@@ -57,14 +45,29 @@ void RicShowMainWindowFeature::onActionTriggered(bool isChecked)
 
     mainWnd->raise();
 
-    if (triggerReloadOfDockWindowVisibilities)
-    {
-        mainWnd->restoreDockWidgetVisibilities();
-    }
+    mainWnd->restoreDockWidgetVisibilities();
 }
 
 //--------------------------------------------------------------------------------------------------
-/// 
+///
+//--------------------------------------------------------------------------------------------------
+bool RicShowMainWindowFeature::isCommandEnabled()
+{
+    return true;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RicShowMainWindowFeature::onActionTriggered(bool isChecked)
+{
+    this->disableModelChangeContribution();
+
+    RicShowMainWindowFeature::showMainWindow();
+}
+
+//--------------------------------------------------------------------------------------------------
+///
 //--------------------------------------------------------------------------------------------------
 void RicShowMainWindowFeature::setupActionLook(QAction* actionToSetup)
 {
