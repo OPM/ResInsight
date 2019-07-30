6e76e043c62117829d3bc34724a97c73856fecf0
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-07-30 10:36:06 +0200

    Fix copyright header

--- ApplicationCode/CommandFileInterface/RicfExportFlowCharacteristics.h
+++ ApplicationCode/CommandFileInterface/RicfExportFlowCharacteristics.h
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2017 Statoil ASA
+//  Copyright (C) 2019-     Equinor ASA
 //
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
