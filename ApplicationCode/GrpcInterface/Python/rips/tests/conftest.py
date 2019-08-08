4381f679e5a909906bd5517efa2b49d9c436bdf7
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-08-08 08:15:04 +0200

    Python pytest : Issue project.close() after each test

--- ApplicationCode/GrpcInterface/Python/rips/tests/conftest.py
+++ ApplicationCode/GrpcInterface/Python/rips/tests/conftest.py
@@ -14,7 +14,9 @@ def rips_instance():
 
 @pytest.fixture
 def initializeTest():
-    _rips_instance.project.close()
+    _rips_instance.project.close() # make sure ResInsight is clean before execution of test
+    yield initializeTest
+    _rips_instance.project.close() # make sure ResInsight is clean after test
 
 def pytest_addoption(parser):
     parser.addoption("--console", action="store_true", default=False, help="Run as console application")
