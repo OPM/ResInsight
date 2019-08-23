45b53af1be06fcb1cb1e4ef3e0a7c2ed928effc9
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2019-08-23 11:03:18 +0200

    #4611 Python : Remove reference do deleted module App

--- ApplicationCode/GrpcInterface/Python/rips/__init__.py
+++ ApplicationCode/GrpcInterface/Python/rips/__init__.py
@@ -3,7 +3,6 @@ from .Case import Case
 from .Grid import Grid
 from .Properties import Properties
 from .Instance import Instance
-from .App import App
 from .Commands import Commands
 from .PdmObject import PdmObject
 from .View import View
