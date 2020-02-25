84f5c5d0686f02f6c51a79ffb7bbe7b9045f3c86
Gaute Lindkvist <lindkvis@gmail.com>
2020-02-25 13:20:12 +0100

    Fix Python access to contour maps

--- ApplicationCode/GrpcInterface/Python/rips/__init__.py
+++ ApplicationCode/GrpcInterface/Python/rips/__init__.py
@@ -4,14 +4,14 @@ import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'generated'))
 
-from rips.case import Case
+from rips.case import Case, EclipseCase, GeoMechCase
 from rips.grid import Grid
 from rips.instance import Instance
 from rips.pdmobject import PdmObject
 from rips.view import View
 from rips.project import Project
 from rips.plot import Plot, PlotWindow
-from rips.contour_map import ContourMap, ContourMapType
+from rips.contour_map import EclipseContourMap, GeoMechContourMap
 from rips.well_log_plot import WellLogPlot
 from rips.well_bore_stability_plot import WellBoreStabilityPlot, WbsParameters
 from rips.simulation_well import SimulationWell
