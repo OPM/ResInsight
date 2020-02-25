5a880df9228f2a43746528c08b2b2128ef4497d2
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2020-02-26 00:21:19 +0100

    #4477 Add python method to set NNC connection values.
    Equivalent to SetNNCProperty in Octave.

--- ApplicationCode/GrpcInterface/Python/rips/tests/test_nnc_properties.py
+++ ApplicationCode/GrpcInterface/Python/rips/tests/test_nnc_properties.py
@@ -46,6 +46,28 @@ def test_10kSync(rips_instance, initialize_test):
     for a in allen_vals:
         assert(isinstance(a, float))
 
+    # Generate some data
+    new_data = []
+    for (c, _) in enumerate(nnc_connections):
+        new_data.append(float(c))
+
+    property_name = "NEW_PROP"
+    case.set_nnc_connections_values(new_data, property_name, 0)
+    new_prop_vals = case.nnc_connections_generated_values(property_name, 0)
+    assert(len(new_prop_vals) == len(new_data))
+    for i in range(0, len(new_data)):
+        assert(new_data[i] == new_prop_vals[i])
+
+    # Set some other data for second time step
+    for i in range(0, len(new_data)):
+        new_data[i] = new_data[i] * 2.0
+
+    case.set_nnc_connections_values(new_data, property_name, 1)
+    new_prop_vals = case.nnc_connections_generated_values(property_name, 1)
+    assert(len(new_prop_vals) == len(nnc_connections))
+    for i in range(0, len(new_data)):
+        assert(new_data[i] == new_prop_vals[i])
+
 def test_non_existing_dynamic_values(rips_instance, initialize_test):
     casePath = dataroot.PATH + "/TEST10K_FLT_LGR_NNC/TEST10K_FLT_LGR_NNC.EGRID"
     case = rips_instance.project.load_case(path=casePath)
