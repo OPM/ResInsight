acd151744adc5ce215cae330d9453c4a1dd4e6e7
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2020-04-01 22:21:21 +0200

    #5757 Python : Add resampling of summary data

---
+++ ApplicationCode/ProjectDataModelCommands/RimcSummaryResampleData.cpp
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2020- Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RimcSummaryResampleData.h"
+
+#include "cafPdmFieldIOScriptability.h"
+
+#include "cafPdmObjectScriptability.h"
+
+CAF_PDM_SOURCE_INIT( RimcSummaryResampleData, "ResampleData" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimcSummaryResampleData::RimcSummaryResampleData()
+{
+    CAF_PDM_InitScriptableObject( "Resample Data", "", "", "" );
+    CAF_PDM_InitScriptableFieldWithIONoDefault( &m_timeValues, "TimeValues", "Data Time Values", "", "", "" );
+    CAF_PDM_InitScriptableFieldWithIONoDefault( &m_doubleValues, "DoubleValues", "Data Double Values", "", "", "" );
+}
