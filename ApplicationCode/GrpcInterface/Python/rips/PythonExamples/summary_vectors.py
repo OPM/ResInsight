8e8d1d7fe3848d7371b866cec2b4b09cbacde34f
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2020-04-07 10:16:27 +0200

    Summary Data : Update example

--- ApplicationCode/GrpcInterface/Python/rips/PythonExamples/summary_vectors.py
+++ ApplicationCode/GrpcInterface/Python/rips/PythonExamples/summary_vectors.py
@@ -1,18 +1,18 @@
-# Load ResInsight Processing Server Client Library
 import rips
-# Connect to ResInsight instance
+
 resinsight = rips.Instance.find()
-# Example code
-print("ResInsight version: " + resinsight.version_string())
 
 project = resinsight.project
-
 summary_cases = project.descendants(rips.SummaryCase)
 
+# Assumes at least one summery case loaded
 firstCase = summary_cases[0]
 
 vector_name = "FOPT"
 summary_data = firstCase.summary_vector_values(vector_name)
 
 print("Data for summary vector " + vector_name)
-print(summary_data.double_values)
+print(summary_data.values)
+
+time_steps = firstCase.available_time_steps()
+print(time_steps.values)
