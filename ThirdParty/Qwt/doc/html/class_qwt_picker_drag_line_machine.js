c801d768d3fa647e7d7beb8ce33e39951a236ed4
Gaute Lindkvist <lindkvis@gmail.com>
2020-04-24 13:15:11 +0200

    Removed qwt from source code tree and added submodule

--- ThirdParty/Qwt/doc/html/class_qwt_picker_drag_line_machine.js
+++
@@ -1,5 +0,0 @@
-var class_qwt_picker_drag_line_machine =
-[
-    [ "QwtPickerDragLineMachine", "class_qwt_picker_drag_line_machine.html#acf3157352ff3d68fdd000e8a944467b3", null ],
-    [ "transition", "class_qwt_picker_drag_line_machine.html#a3896893cd1950d5a00eb1ce01861a24a", null ]
-];
