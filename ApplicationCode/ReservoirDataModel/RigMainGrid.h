82a1ddcd5ce4a0010edf20c75db893b2cf3bf42f
Gaute Lindkvist <lindkvis@gmail.com>
2020-05-14 10:17:42 +0200

    Refactored some code to make it possible to parallelise

--- ApplicationCode/ReservoirDataModel/RigMainGrid.h
+++ ApplicationCode/ReservoirDataModel/RigMainGrid.h
@@ -72,6 +72,15 @@ public:
     cvf::Collection<RigFault>&       faults();
     void calculateFaults( const RigActiveCellInfo* activeCellInfo, bool computeNncs, bool includeInactiveCells );
 
+    void addUnNamedFaultFaces( int                               gcIdx,
+                               const RigActiveCellInfo*          activeCellInfo,
+                               const std::vector<cvf::Vec3d>&    vxs,
+                               int                               unNamedFaultIdx,
+                               int                               unNamedFaultWithInactiveIdx,
+                               std::vector<RigFault::FaultFace>& unNamedFaultFaces,
+                               std::vector<RigFault::FaultFace>& unNamedFaultFacesInactive,
+                               RigFaultsPrCellAccumulator*       faultsPrCellAcc ) const;
+
     void distributeNNCsToFaults();
 
     const RigFault* findFaultFromCellIndexAndCellFace( size_t                             reservoirCellIndex,
