f779f456f69269d8b1f2c6f69c6442d5c5e7a456
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2020-09-16 13:34:44 +0200

    #6514 Dark Theme : Fix color of rubber band zoom

--- ApplicationCode/UserInterface/RiuSummaryQwtPlot.cpp
+++ ApplicationCode/UserInterface/RiuSummaryQwtPlot.cpp
@@ -103,9 +103,7 @@ RiuSummaryQwtPlot::RiuSummaryQwtPlot( RimSummaryPlot* plot, QWidget* parent /*= 
 {
     // LeftButton for the zooming
     m_zoomerLeft = new RiuQwtPlotZoomer( canvas() );
-    m_zoomerLeft->setRubberBandPen( QColor( Qt::black ) );
     m_zoomerLeft->setTrackerMode( QwtPicker::AlwaysOff );
-    m_zoomerLeft->setTrackerPen( QColor( Qt::black ) );
     m_zoomerLeft->initMousePattern( 1 );
 
     // Attach a zoomer for the right axis
