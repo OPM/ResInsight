17ba1e49b687774119b1db90df20ffff00fd6b6f
Gaute Lindkvist <lindkvis@gmail.com>
2020-09-24 15:51:59 +0200

    Simplify some code with C++17

--- ApplicationCode/Commands/CompletionExportCommands/RicWellPathExportMswCompletionsImpl.cpp
+++ ApplicationCode/Commands/CompletionExportCommands/RicWellPathExportMswCompletionsImpl.cpp
@@ -1676,7 +1676,6 @@ void RicWellPathExportMswCompletionsImpl::writeMainBoreWelsegsSegment( std::shar
         prevOutMD  = previousSegment->outputMD();
         prevOutTVD = previousSegment->outputTVD();
     }
-
     for ( const auto& [subStartMD, subEndMD] : subSegments )
     {
         auto startPoint = exportInfo.wellPath()->wellPathGeometry()->interpolatedPointAlongWellPath( subStartMD );
