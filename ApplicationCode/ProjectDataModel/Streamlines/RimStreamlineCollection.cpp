8f0db455599c802642378c73aa56c43dff0e3540
Jon Jenssen <jon@soundsoft.no>
2020-09-25 18:11:12 +0200

    Add streamlines project data model skeleton Add streamlines folder to project tree

---
+++ ApplicationCode/ProjectDataModel/Streamlines/RimStreamlineCollection.cpp
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2020-     Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RimStreamlineCollection.h"
+#include "RimStreamline.h"
+
+#include "RiaLogging.h"
+
+#include "cafPdmFieldScriptingCapability.h"
+#include "cafPdmObjectScriptingCapability.h"
+
+CAF_PDM_SOURCE_INIT( RimStreamlineCollection, "StreamlineCollection" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimStreamlineCollection::RimStreamlineCollection()
+{
+    CAF_PDM_InitScriptableObject( "Streamlines", ":/Erase.png", "", "" );
+
+    CAF_PDM_InitScriptableFieldNoDefault( &m_collectionName, "Name", "Name", "", "", "" );
+    m_collectionName = "Streamlines";
+
+    // CAF_PDM_InitScriptableFieldNoDefault( &m_streamlines, "Streamlines", "Streamlines", "", "", "" );
+    // m_streamlines.uiCapability()->setUiTreeHidden( true );
+
+    // we are a topmost folder, do not delete us
+    setDeletable( false );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimStreamlineCollection::~RimStreamlineCollection()
+{
+}
