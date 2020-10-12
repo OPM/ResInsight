41b7b99832a14caa85a0127171539ecc87b1b6ce
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2020-10-12 18:52:57 +0200

    Update to 2020.10.0-RC03

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -4,7 +4,7 @@ set(RESINSIGHT_MINOR_VERSION 10)
 set(RESINSIGHT_PATCH_VERSION 0)
 
 # Opional text with no restrictions
-set(RESINSIGHT_VERSION_TEXT "-RC02")
+set(RESINSIGHT_VERSION_TEXT "-RC03")
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
