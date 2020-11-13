d1fc39a51c63e066d588af99e323ae7aac6ce6dd
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2020-11-13 10:56:25 +0100

    Merged in VFP-tables commit 95f1742eab2b9dae391a253a5452b0f143d07abd.

---
+++ ApplicationCode/ProjectDataModel/RimVfpPlot.h
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2020- Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "RimPlot.h"
+
+#include "cafPdmPtrField.h"
+
+#include <QPointer>
+
+class RimEclipseResultCase;
+class RimFlowDiagSolution;
+class RigTofWellDistributionCalculator;
+class RiuQwtPlotWidget;
+
+//--------------------------------------------------------------------------------------------------
+/// Vertical Flow Performance Plot
+//--------------------------------------------------------------------------------------------------
+class RimVfpPlot : public RimPlot
+{
+    CAF_PDM_HEADER_INIT;
+
+public:
+    RimVfpPlot();
+    ~RimVfpPlot() override;
+
+    void setDataSourceParameters( RimEclipseResultCase* eclipseResultCase, QString targetWellName );
+    // void setPlotOptions( bool groupSmallContributions, double smallContributionsRelativeThreshold, double maximumTof );
+
+    RiaDefines::PhaseType phase() const;
+
+    // RimPlot implementations
+    RiuQwtPlotWidget* viewer() override;
+    void              setAutoScaleXEnabled( bool enabled ) override;
+    void              setAutoScaleYEnabled( bool enabled ) override;
+    void              updateAxes() override;
+    void              updateLegend() override;
+    void              updateZoomInQwt() override;
+    void              updateZoomFromQwt() override;
+    QString           asciiDataForPlotExport() const override;
+    void              reattachAllCurves() override;
+    void              detachAllCurves() override;
+    caf::PdmObject*   findPdmObjectFromQwtCurve( const QwtPlotCurve* curve ) const override;
+    void              onAxisSelected( int axis, bool toggle ) override;
+
+    // RimPlotWindow implementations
+    QString description() const override;
+
+    // RimViewWindow implementations
+    QWidget* viewWidget() override;
+    QImage   snapshotWindowContent() override;
+    void     zoomAll() override;
+
+private:
+    // RimPlot implementations
+    void doRemoveFromCollection() override;
+
+    // RimViewWindow implementations
+    void deleteViewWidget() override;
+    void onLoadDataAndUpdate() override;
+
+private:
+    RiuQwtPlotWidget* doCreatePlotViewWidget( QWidget* mainWindowParent ) override;
+
+    void        fixupDependentFieldsAfterCaseChange();
+    static void populatePlotWidgetWithCurveData( RiuQwtPlotWidget* plotWidget );
+
+    void defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering ) override;
+    void fieldChangedByUi( const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue ) override;
+
+    QList<caf::PdmOptionItemInfo> calculateValueOptions( const caf::PdmFieldHandle* fieldNeedingOptions,
+                                                         bool*                      useOptionsOnly ) override;
+
+private:
+    caf::PdmPtrField<RimEclipseResultCase*> m_case;
+    caf::PdmField<QString>                  m_wellName;
+
+    QPointer<RiuQwtPlotWidget> m_plotWidget;
+};
