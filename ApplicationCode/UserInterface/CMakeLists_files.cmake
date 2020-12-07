85e6bc7ddcfb7aef1924e6a3d0209fde58b005a4
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2020-12-07 16:40:46 +0100

    Added text content frame.

--- ApplicationCode/UserInterface/CMakeLists_files.cmake
+++ ApplicationCode/UserInterface/CMakeLists_files.cmake
@@ -190,6 +190,7 @@ ${CMAKE_CURRENT_LIST_DIR}/RiuFileDialogTools.cpp
 ${CMAKE_CURRENT_LIST_DIR}/RiuGuiTheme.cpp
 ${CMAKE_CURRENT_LIST_DIR}/RiuQssSyntaxHighlighter.cpp
 ${CMAKE_CURRENT_LIST_DIR}/RiuTextEditWithCompletion.cpp
+${CMAKE_CURRENT_LIST_DIR}/RiuTextContentFrame.cpp
 )
 
 list(APPEND CODE_HEADER_FILES
@@ -246,6 +247,7 @@ ${CMAKE_CURRENT_LIST_DIR}/RiuAbstractLegendFrame.h
 ${CMAKE_CURRENT_LIST_DIR}/RiuCategoryLegendFrame.h
 ${CMAKE_CURRENT_LIST_DIR}/RiuScalarMapperLegendFrame.h
 ${CMAKE_CURRENT_LIST_DIR}/RiuTextEditWithCompletion.h
+${CMAKE_CURRENT_LIST_DIR}/RiuTextContentFrame.h
 )
 
 list(APPEND QT_UI_FILES
