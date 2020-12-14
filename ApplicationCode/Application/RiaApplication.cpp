OPM/ResInsight

commit e847ce7cd36281271c6ebc296bd7b517ded2147a

Use PdmPointers for command queue in RiaApplication

ApplicationCode/Application/RiaApplication.cpp
@@ -1302,31 +1302,25 @@ void RiaApplication::addCommandObject( RimCommandObject* commandObject )
 void RiaApplication::executeCommandObjects()
 {
     {
-        std::list<RimCommandObject*>::iterator it = m_commandQueue.begin();
-        while ( it != m_commandQueue.end() )
+        auto currentCommandQueue = m_commandQueue;
+        for ( auto command : currentCommandQueue )
         {
-            RimCommandObject* toBeRemoved = *it;
-            if ( !toBeRemoved->isAsyncronous() )
+            if ( command->isAsyncronous() )
             {
-                toBeRemoved->redo();
-
-                ++it;
-                m_commandQueue.remove( toBeRemoved );
-            }
-            else
-            {
-                ++it;
+                command->redo();
+                m_commandQueue.remove( command );
             }
         }
     }
 
     if ( !m_commandQueue.empty() )
     {
-        std::list<RimCommandObject*>::iterator it = m_commandQueue.begin();
-
-        RimCommandObject* first = *it;
-        first->redo();
-
+        auto it = m_commandQueue.begin();
+        if ( it->notNull() )
+        {
+            RimCommandObject* first = *it;
+            first->redo();
+        }
         m_commandQueue.pop_front();
     }
     else

ApplicationCode/Application/RiaApplication.h
@@ -262,8 +262,8 @@ protected:
     QString                    m_commandLineHelpText;
     QMap<QString, QVariant>    m_sessionCache; // Session cache used to store username/passwords per session
 
-    std::list<RimCommandObject*> m_commandQueue;
-    QMutex                       m_commandQueueLock;
+    std::list<caf::PdmPointer<RimCommandObject>> m_commandQueue;
+    QMutex                                       m_commandQueueLock;
 
     bool m_runningWorkerProcess;
 

commit 95e73a04df8b552b4a1556d706bcd7d13c9cb884

Fix inverted test on command objects
Regression introduced in e847ce7c

ApplicationCode/Application/RiaApplication.cpp
@@ -1305,7 +1305,7 @@ void RiaApplication::executeCommandObjects()
         auto currentCommandQueue = m_commandQueue;
         for ( auto command : currentCommandQueue )
         {
-            if ( command->isAsyncronous() )
+            if ( !command->isAsyncronous() )
             {
                 command->redo();
                 m_commandQueue.remove( command );