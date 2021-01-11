97114f45548a43c93c131c02e220fcfd3b3e0984
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2021-01-11 15:44:27 +0100

    #7190 Correlation Parameters : Fix cache inconsistencies

--- ApplicationLibCode/ProjectDataModel/Summary/RimSummaryCaseCollection.h
+++ ApplicationLibCode/ProjectDataModel/Summary/RimSummaryCaseCollection.h
@@ -117,7 +117,7 @@ public:
     int                                        ensembleId() const;
     bool                                       hasEnsembleParameters() const;
 
-    const std::vector<EnsembleParameter>& variationSortedEnsembleParameters( bool excludeNoVariation = false ) const;
+    std::vector<EnsembleParameter> variationSortedEnsembleParameters( bool excludeNoVariation = false ) const;
     std::vector<std::pair<EnsembleParameter, double>>
         correlationSortedEnsembleParameters( const RifEclipseSummaryAddress& address ) const;
     std::vector<std::pair<EnsembleParameter, double>>
