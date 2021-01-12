c34a28a5b6fe8088411aeac3342a8f55cbd99297
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2021-01-12 20:45:53 +0100

    Set version to 2020.10.2-RC01

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,10 +1,10 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2020)
 set(RESINSIGHT_MINOR_VERSION 10)
-set(RESINSIGHT_PATCH_VERSION 1)
+set(RESINSIGHT_PATCH_VERSION 2)
 
 # Opional text with no restrictions
-#set(RESINSIGHT_VERSION_TEXT "-RC08")
+set(RESINSIGHT_VERSION_TEXT "-RC01")
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
