a43414a9cdfdb216b45b127254c864f4b459c6bf
jonjenssen <jon@soundsoft.no>
2021-04-06 14:32:38 +0200

    Update parameter XML format and reader to use groups

---
+++ ApplicationLibCode/ProjectDataModel/Parameters/RimParameterGroup.cpp
@@ -0,0 +1,146 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2021 -    Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RimParameterGroup.h"
+
+#include "cafPdmFieldScriptingCapability.h"
+#include "cafPdmObjectScriptingCapability.h"
+#include "cafPdmUiTableViewEditor.h"
+
+#include "RimGenericParameter.h"
+
+#include <cmath>
+
+CAF_PDM_SOURCE_INIT( RimParameterGroup, "ParameterGroup" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimParameterGroup::RimParameterGroup()
+{
+    CAF_PDM_InitFieldNoDefault( &m_parameters, "Parameters", "Parameters", "", "", "" );
+    m_parameters.uiCapability()->setUiEditorTypeName( caf::PdmUiTableViewEditor::uiEditorTypeName() );
+    m_parameters.uiCapability()->setUiLabelPosition( caf::PdmUiItemInfo::HIDDEN );
+    m_parameters.uiCapability()->setCustomContextMenuEnabled( true );
+
+    CAF_PDM_InitFieldNoDefault( &m_name, "Name", "Name", "", "", "" );
+    m_name.uiCapability()->setUiHidden( true );
+    m_name.uiCapability()->setUiReadOnly( true );
+
+    CAF_PDM_InitFieldNoDefault( &m_showExpanded, "Expanded", "Expanded", "", "", "" );
+    m_name.uiCapability()->setUiHidden( true );
+    m_name.uiCapability()->setUiReadOnly( true );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimParameterGroup::~RimParameterGroup()
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimParameterGroup::addParameter( RimGenericParameter* parameter )
+{
+    m_parameters.push_back( parameter );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimParameterGroup::appendParametersToList( std::list<RimGenericParameter*>& parameterList )
+{
+    for ( auto p : m_parameters() )
+    {
+        parameterList.push_back( p );
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimParameterGroup::fieldChangedByUi( const caf::PdmFieldHandle* changedField,
+                                          const QVariant&            oldValue,
+                                          const QVariant&            newValue )
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimParameterGroup::defineEditorAttribute( const caf::PdmFieldHandle* field,
+                                               QString                    uiConfigName,
+                                               caf::PdmUiEditorAttribute* attribute )
+{
+    if ( field == &m_parameters )
+    {
+        auto tvAttribute = dynamic_cast<caf::PdmUiTableViewEditorAttribute*>( attribute );
+        if ( tvAttribute )
+        {
+            tvAttribute->resizePolicy              = caf::PdmUiTableViewEditorAttribute::RESIZE_TO_FILL_CONTAINER;
+            tvAttribute->alwaysEnforceResizePolicy = true;
+            tvAttribute->minimumHeight             = 300;
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimParameterGroup::defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering )
+{
+    auto group = uiOrdering.addNewGroup( name() );
+    group->add( &m_parameters );
+    group->setCollapsedByDefault( isExpanded() );
+
+    uiOrdering.skipRemainingFields( true );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimParameterGroup::setName( QString name )
+{
+    m_name = name;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimParameterGroup::setExpanded( bool expand )
+{
+    m_showExpanded = expand;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+bool RimParameterGroup::isExpanded() const
+{
+    return m_showExpanded;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+QString RimParameterGroup::name() const
+{
+    return m_name;
+}
---
+++ ApplicationLibCode/ProjectDataModel/Parameters/RimParameterGroup.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2021 -    Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "cafPdmChildArrayField.h"
+#include "cafPdmField.h"
+#include "cafPdmObject.h"
+
+#include <QString>
+
+class RimGenericParameter;
+
+//==================================================================================================
+///
+///
+//==================================================================================================
+class RimParameterGroup : public caf::PdmObject
+{
+    CAF_PDM_HEADER_INIT;
+
+public:
+    RimParameterGroup();
+    ~RimParameterGroup() override;
+
+    void addParameter( RimGenericParameter* p );
+    void appendParametersToList( std::list<RimGenericParameter*>& parameterList );
+
+    void setName( QString name );
+    void setExpanded( bool expand );
+
+    bool    isExpanded() const;
+    QString name() const;
+
+private:
+    void fieldChangedByUi( const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue ) override;
+    void defineEditorAttribute( const caf::PdmFieldHandle* field,
+                                QString                    uiConfigName,
+                                caf::PdmUiEditorAttribute* attribute ) override;
+    void defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering ) override;
+
+private:
+    caf::PdmChildArrayField<RimGenericParameter*> m_parameters;
+    caf::PdmField<bool>                           m_showExpanded;
+    caf::PdmField<QString>                        m_name;
+};
