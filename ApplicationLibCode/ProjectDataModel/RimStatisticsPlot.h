ae39fdc18998b5eefea135c88d01fa207fa860b0
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2021-05-12 13:55:29 +0200

    #7682 Statistics Plot: Add user-defined histogram bar width.

--- ApplicationLibCode/ProjectDataModel/RimStatisticsPlot.h
+++ ApplicationLibCode/ProjectDataModel/RimStatisticsPlot.h
@@ -85,4 +85,5 @@ protected:
     caf::PdmField<QString>      m_plotWindowTitle;
     caf::PdmField<int>          m_numHistogramBins;
     caf::PdmField<cvf::Color3f> m_histogramBarColor;
+    caf::PdmField<double>       m_histogramBarWidth;
 };
