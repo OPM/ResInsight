ca0433ca3a1787a2cc6ca519ef144a7873afe748
jonjenssen <jon@soundsoft.no>
2021-07-13 16:58:02 +0200

    Move well related pdm files into subfolders to clean up a bit

---
+++ ApplicationLibCode/ProjectDataModel/WellPath/CMakeLists_files.cmake
@@ -0,0 +1,38 @@
+
+set (SOURCE_GROUP_HEADER_FILES
+${CMAKE_CURRENT_LIST_DIR}/RimWellPath.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathGroup.h
+${CMAKE_CURRENT_LIST_DIR}/RimFileWellPath.h
+${CMAKE_CURRENT_LIST_DIR}/RimModeledWellPath.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathGeometryDef.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathGeometryDefTools.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathAttribute.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathAttributeCollection.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathCollection.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathTarget.h
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathTieIn.h
+)
+
+set (SOURCE_GROUP_SOURCE_FILES
+${CMAKE_CURRENT_LIST_DIR}/RimFileWellPath.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimModeledWellPath.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPath.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathAttribute.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathAttributeCollection.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathCollection.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathGeometryDef.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathGeometryDefTools.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathGroup.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathTarget.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimWellPathTieIn.cpp
+)
+
+list(APPEND CODE_HEADER_FILES
+${SOURCE_GROUP_HEADER_FILES}
+)
+
+list(APPEND CODE_SOURCE_FILES
+${SOURCE_GROUP_SOURCE_FILES}
+)
+
+source_group( "ProjectDataModel\\WellPath" FILES ${SOURCE_GROUP_HEADER_FILES} ${SOURCE_GROUP_SOURCE_FILES} ${CMAKE_CURRENT_LIST_DIR}/CMakeLists_files.cmake )
