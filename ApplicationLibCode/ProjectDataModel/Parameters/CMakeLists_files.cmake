1aae201e22bb95e8da0df0f740411a144d30b4b0
jonjenssen <jonjenssen@users.noreply.github.com>
2021-09-02 13:36:11 +0000

    Fixes by cmake-format

--- ApplicationLibCode/ProjectDataModel/Parameters/CMakeLists_files.cmake
+++ ApplicationLibCode/ProjectDataModel/Parameters/CMakeLists_files.cmake
@@ -1,22 +1,20 @@
-set (SOURCE_GROUP_HEADER_FILES
-${CMAKE_CURRENT_LIST_DIR}/RimGenericParameter.h
-${CMAKE_CURRENT_LIST_DIR}/RimDoubleParameter.h
-${CMAKE_CURRENT_LIST_DIR}/RimIntegerParameter.h
-${CMAKE_CURRENT_LIST_DIR}/RimStringParameter.h
-${CMAKE_CURRENT_LIST_DIR}/RimListParameter.h
-${CMAKE_CURRENT_LIST_DIR}/RimParameterGroup.h
-${CMAKE_CURRENT_LIST_DIR}/RimParameterGroups.h
-)
+set(SOURCE_GROUP_HEADER_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RimGenericParameter.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimDoubleParameter.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimIntegerParameter.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimStringParameter.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimListParameter.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimParameterGroup.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimParameterGroups.h)
 
-set (SOURCE_GROUP_SOURCE_FILES
-${CMAKE_CURRENT_LIST_DIR}/RimGenericParameter.cpp
-${CMAKE_CURRENT_LIST_DIR}/RimDoubleParameter.cpp
-${CMAKE_CURRENT_LIST_DIR}/RimIntegerParameter.cpp
-${CMAKE_CURRENT_LIST_DIR}/RimStringParameter.cpp
-${CMAKE_CURRENT_LIST_DIR}/RimListParameter.cpp
-${CMAKE_CURRENT_LIST_DIR}/RimParameterGroup.cpp
-${CMAKE_CURRENT_LIST_DIR}/RimParameterGroups.cpp
-)
+set(SOURCE_GROUP_SOURCE_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RimGenericParameter.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimDoubleParameter.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimIntegerParameter.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimStringParameter.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimListParameter.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimParameterGroup.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimParameterGroups.cpp)
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
 
