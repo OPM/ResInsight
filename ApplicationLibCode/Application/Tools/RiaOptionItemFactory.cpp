f4b8f2e3e5a2331c036d3f09551817c43507cbc8
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2021-11-05 14:59:47 +0100

    #8246 Summary Curves : Use icons for summary categories

--- ApplicationLibCode/Application/Tools/RiaOptionItemFactory.cpp
+++ ApplicationLibCode/Application/Tools/RiaOptionItemFactory.cpp
@@ -54,3 +54,81 @@ void RiaOptionItemFactory::appendOptionItemsForEnsembleCurveSets( QList<caf::Pdm
         options->push_back( caf::PdmOptionItemInfo( ensembleCurveSet->name(), ensembleCurveSet ) );
     }
 }
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+caf::PdmOptionItemInfo
+    RiaOptionItemFactory::optionItemFromSummaryType( RifEclipseSummaryAddress::SummaryVarCategory summaryType )
+{
+    auto uiText = caf::AppEnum<RifEclipseSummaryAddress::SummaryVarCategory>::uiText( summaryType );
+
+    // Use icons from https://github.com/equinor/webviz-subsurface-components
+
+    QString iconText;
+
+    switch ( summaryType )
+    {
+        case RifEclipseSummaryAddress::SUMMARY_INVALID:
+            iconText = ":/summary/components/images/invalid.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_FIELD:
+            iconText = ":/summary/components/images/field.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_AQUIFER:
+            iconText = ":/summary/components/images/aquifer.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_NETWORK:
+            iconText = ":/summary/components/images/network.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_MISC:
+            iconText = ":/summary/components/images/misc.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_REGION:
+            iconText = ":/summary/components/images/region.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_REGION_2_REGION:
+            iconText = ":/summary/components/images/region-region.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_WELL_GROUP:
+            iconText = ":/summary/components/images/group.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_WELL:
+            iconText = ":/summary/components/images/well.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_WELL_COMPLETION:
+            iconText = ":/summary/components/images/well-completion.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_WELL_LGR:
+            iconText = ":/summary/components/images/well.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_WELL_COMPLETION_LGR:
+            iconText = ":/summary/components/images/well-completion.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_WELL_SEGMENT:
+            iconText = ":/summary/components/images/segment.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_BLOCK:
+            iconText = ":/summary/components/images/block.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_BLOCK_LGR:
+            iconText = ":/summary/components/images/block.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_CALCULATED:
+            iconText = ":/summary/components/images/calculated.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_IMPORTED:
+            iconText = ":/summary/components/images/others.svg";
+            break;
+        case RifEclipseSummaryAddress::SUMMARY_ENSEMBLE_STATISTICS:
+            break;
+    }
+
+    if ( iconText.isEmpty() )
+    {
+        return caf::PdmOptionItemInfo( uiText, summaryType );
+    }
+
+    caf::IconProvider iconProvider( iconText );
+    return caf::PdmOptionItemInfo( uiText, summaryType, false, iconProvider );
+}
