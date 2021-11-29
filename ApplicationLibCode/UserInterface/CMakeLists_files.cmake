03b8db175a32e972e5005652017248196b7d33e8
kriben <kriben@users.noreply.github.com>
2021-11-29 11:50:10 +0000

    Fixes by cmake-format

--- ApplicationLibCode/UserInterface/CMakeLists_files.cmake
+++ ApplicationLibCode/UserInterface/CMakeLists_files.cmake
@@ -195,25 +195,32 @@ set(SOURCE_GROUP_SOURCE_FILES
 )
 
 if(Qt5Charts_FOUND)
-  list(APPEND CODE_HEADER_FILES ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartView.h
-       ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotCurve.h
-       ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotWidget.h
-       ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotTools.h
-       ${CMAKE_CURRENT_LIST_DIR}/RiuSummaryQtChartsPlot.h
-
+  list(
+    APPEND
+    CODE_HEADER_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartView.h
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotCurve.h
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotWidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotTools.h
+    ${CMAKE_CURRENT_LIST_DIR}/RiuSummaryQtChartsPlot.h
   )
 
-  list(APPEND CODE_SOURCE_FILES ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartView.cpp
-       ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotCurve.cpp
-       ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotWidget.cpp
-       ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotTools.cpp
-       ${CMAKE_CURRENT_LIST_DIR}/RiuSummaryQtChartsPlot.cpp
+  list(
+    APPEND
+    CODE_SOURCE_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartView.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotCurve.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotWidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotTools.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RiuSummaryQtChartsPlot.cpp
   )
 
-  list(APPEND QT_MOC_HEADERS
-       # ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartView.h
-       ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotWidget.h
-       ${CMAKE_CURRENT_LIST_DIR}/RiuSummaryQtChartsPlot.h
+  list(
+    APPEND
+    QT_MOC_HEADERS
+    # ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartView.h
+    ${CMAKE_CURRENT_LIST_DIR}/RiuQtChartsPlotWidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/RiuSummaryQtChartsPlot.h
   )
 endif()
 
