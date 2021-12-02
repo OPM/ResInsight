5a6a41cb0bbcb5890141a4c27f87c1886c105bc3
magnesj <magnesj@users.noreply.github.com>
2021-12-02 14:48:29 +0000

    Fixes by cmake-format

--- ApplicationLibCode/Commands/PlotBuilderCommands/CMakeLists_files.cmake
+++ ApplicationLibCode/Commands/PlotBuilderCommands/CMakeLists_files.cmake
@@ -1,21 +1,17 @@
-set(SOURCE_GROUP_HEADER_FILES
-${CMAKE_CURRENT_LIST_DIR}/RicNewMultiPlotFeature.h
-${CMAKE_CURRENT_LIST_DIR}/RicSummaryPlotBuilder.h
+set(SOURCE_GROUP_HEADER_FILES ${CMAKE_CURRENT_LIST_DIR}/RicNewMultiPlotFeature.h
+                              ${CMAKE_CURRENT_LIST_DIR}/RicSummaryPlotBuilder.h
 )
 
 set(SOURCE_GROUP_SOURCE_FILES
-${CMAKE_CURRENT_LIST_DIR}/RicNewMultiPlotFeature.cpp
-${CMAKE_CURRENT_LIST_DIR}/RicSummaryPlotBuilder.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RicNewMultiPlotFeature.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RicSummaryPlotBuilder.cpp
 )
 
 list(APPEND COMMAND_CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
 
 list(APPEND COMMAND_CODE_SOURCE_FILES ${SOURCE_GROUP_SOURCE_FILES})
 
-list(
-  APPEND
-  COMMAND_QT_MOC_HEADERS
-)
+list(APPEND COMMAND_QT_MOC_HEADERS)
 
 source_group(
   "CommandFeature\\PlotBuilder"
