a65ffa968cc799ba37d2c122efb90037d627f583
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2021-12-17 13:56:23 +0100

    Add missing virtual keywords.

--- ApplicationLibCode/UserInterface/RiuQtChartsPlotWidget.h
+++ ApplicationLibCode/UserInterface/RiuQtChartsPlotWidget.h
@@ -98,14 +98,14 @@ public:
     void           setPlotTitleFontSize( int titleFontSize ) override;
 
     void setLegendFontSize( int fontSize ) override;
-    void setInternalLegendVisible( bool visible );
+    void setInternalLegendVisible( bool visible ) override;
     void insertLegend( RiuPlotWidget::Legend ) override;
     void clearLegend() override;
 
     std::pair<double, double> axisRange( RiaDefines::PlotAxis axis ) const override;
     void                      setAxisRange( RiaDefines::PlotAxis axis, double min, double max ) override;
 
-    void setAxisInverted( RiaDefines::PlotAxis axis, bool isInverted );
+    void setAxisInverted( RiaDefines::PlotAxis axis, bool isInverted ) override;
     void setAxisLabelsAndTicksEnabled( RiaDefines::PlotAxis axis, bool enableLabels, bool enableTicks ) override;
 
     void enableGridLines( RiaDefines::PlotAxis axis, bool majorGridLines, bool minorGridLines );
@@ -147,8 +147,8 @@ public:
 
     void removeEventFilter() override;
 
-    void updateLegend();
-    void updateAxes();
+    void updateLegend() override;
+    void updateAxes() override;
 
     RiuPlotCurve* createPlotCurve( const QString& title, const QColor& color ) override;
 
