c31f740b7e9d0f25ed9fc1bf3cfd5feaa8dfdc84
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2021-12-23 08:23:49 +0100

    Summary multi plot

---
+++ ApplicationLibCode/Commands/PlotBuilderCommands/RicNewSummaryMultiPlotFeature.cpp
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2015-     Statoil ASA
+//  Copyright (C) 2015-     Ceetron Solutions AS
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RicNewSummaryMultiPlotFeature.h"
+
+#include "RimMultiSummaryPlot.h"
+#include "RimPlot.h"
+
+#include "cafSelectionManager.h"
+#include "cvfAssert.h"
+
+#include <QAction>
+
+RICF_SOURCE_INIT( RicNewSummaryMultiPlotFeature, "RicNewSummaryMultiPlotFeature", "createSummaryMultiPlot" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RicNewSummaryMultiPlotFeature::RicNewSummaryMultiPlotFeature()
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+caf::PdmScriptResponse RicNewSummaryMultiPlotFeature::execute()
+{
+    std::vector<RimPlot*> plots;
+    RimMultiSummaryPlot::createAndAppendMultiPlot( plots );
+
+    return caf::PdmScriptResponse();
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+bool RicNewSummaryMultiPlotFeature::isCommandEnabled()
+{
+    return true;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RicNewSummaryMultiPlotFeature::onActionTriggered( bool isChecked )
+{
+    execute();
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RicNewSummaryMultiPlotFeature::setupActionLook( QAction* actionToSetup )
+{
+    actionToSetup->setText( "Create Summary Multi Plot" );
+    actionToSetup->setIcon( QIcon( ":/MultiPlot16x16.png" ) );
+}
