0dcb684396a64e3cfa070ddd9af781ff8d2f27cc
magnesj <magnesj@users.noreply.github.com>
2022-02-02 15:29:45 +0000

    Python code linting changes detected by black

--- GrpcInterface/Python/rips/pdmobject.py
+++ GrpcInterface/Python/rips/pdmobject.py
@@ -224,8 +224,8 @@ class PdmObjectBase:
         list_string = list_string.lstrip("[")
         list_string = list_string.rstrip("]")
         if not list_string:
-           # Return empty list if empty string. Otherwise, the split function will return [''] 
-           return []
+            # Return empty list if empty string. Otherwise, the split function will return ['']
+            return []
         strings = list_string.split(", ")
         values = []
         for string in strings:
