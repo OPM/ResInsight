a8c08483859b70b074f40bbd7c7e1fd76f05566f
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2022-02-13 17:18:40 +0100

    Add test for both positive and negative numbers

--- ApplicationLibCode/UnitTests/RimWellLogExtractionCurveImpl-Test.cpp
+++ ApplicationLibCode/UnitTests/RimWellLogExtractionCurveImpl-Test.cpp
@@ -18,7 +18,8 @@ TEST( RimWellLogExtractionCurveImplTest, StripOffInvalidValAtEndsOfVector )
     values.push_back( 3.0 );
     values.push_back( HUGE_VAL );
 
-    auto valuesIntervals = RiaCurveDataTools::calculateIntervalsOfValidValues( values, false );
+    bool includePositiveValuesOnly = false;
+    auto valuesIntervals = RiaCurveDataTools::calculateIntervalsOfValidValues( values, includePositiveValuesOnly );
 
     EXPECT_EQ( 1, static_cast<int>( valuesIntervals.size() ) );
     EXPECT_EQ( 2, static_cast<int>( valuesIntervals[0].first ) );
@@ -40,11 +41,40 @@ TEST( RimWellLogExtractionCurveImplTest, StripOffHugeValAtEndsAndInteriorOfVecto
     values.push_back( 3.0 );
     values.push_back( HUGE_VAL );
 
-    auto valuesIntervals = RiaCurveDataTools::calculateIntervalsOfValidValues( values, false );
+    bool includePositiveValuesOnly = false;
+    auto valuesIntervals = RiaCurveDataTools::calculateIntervalsOfValidValues( values, includePositiveValuesOnly );
 
     EXPECT_EQ( 2, static_cast<int>( valuesIntervals.size() ) );
     EXPECT_EQ( 2, static_cast<int>( valuesIntervals[0].first ) );
     EXPECT_EQ( 2, static_cast<int>( valuesIntervals[0].second ) );
     EXPECT_EQ( 5, static_cast<int>( valuesIntervals[1].first ) );
     EXPECT_EQ( 6, static_cast<int>( valuesIntervals[1].second ) );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( RimWellLogExtractionCurveImplTest, PositiveValuesOnly )
+{
+    std::vector<double> values;
+    values.push_back( HUGE_VAL );
+    values.push_back( HUGE_VAL );
+    values.push_back( -1.0 );
+    values.push_back( 0.0 );
+    values.push_back( 0.1 );
+    values.push_back( 0.2 );
+    values.push_back( -1.0 );
+    values.push_back( HUGE_VAL );
+    values.push_back( 2.0 );
+    values.push_back( 3.0 );
+    values.push_back( HUGE_VAL );
+
+    bool includePositiveValuesOnly = true;
+    auto valuesIntervals = RiaCurveDataTools::calculateIntervalsOfValidValues( values, includePositiveValuesOnly );
+
+    EXPECT_EQ( 2, static_cast<int>( valuesIntervals.size() ) );
+    EXPECT_EQ( 4, static_cast<int>( valuesIntervals[0].first ) );
+    EXPECT_EQ( 5, static_cast<int>( valuesIntervals[0].second ) );
+    EXPECT_EQ( 8, static_cast<int>( valuesIntervals[1].first ) );
+    EXPECT_EQ( 9, static_cast<int>( valuesIntervals[1].second ) );
 }
