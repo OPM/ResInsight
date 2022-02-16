ee888237f315489c2fd57ff2b7c10a51349eca0c
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2022-02-16 15:37:12 +0100

    #8558 Fix reading of simulation dates for restart case
    The date import was wrong for some restart cases.

--- ApplicationLibCode/FileInterface/RifEclipseOutputFileTools.cpp
+++ ApplicationLibCode/FileInterface/RifEclipseOutputFileTools.cpp
@@ -200,7 +200,8 @@ void RifEclipseOutputFileTools::timeSteps( const ecl_file_type*    ecl_file,
         // Some simulations might end up with wrong data reported for day/month/year. If this situation is detected,
         // base all time step on double values and use start of simulation as first date
         // See https://github.com/OPM/ResInsight/issues/4850
-        if ( uniqueDays.size() == dayValues.size() ) useStartOfSimulationDate = false;
+        // https://github.com/OPM/ResInsight/issues/8558
+        if ( uniqueDays.size() == uniqueDayValues.size() ) useStartOfSimulationDate = false;
     }
 
     std::set<QDateTime> existingTimesteps;
