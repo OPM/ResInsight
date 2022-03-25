871d0cd379615497bcf1ccb2bc8f5b7e01fa7b21
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2022-03-25 18:19:49 +0100

    Merge pull request #8402 from OPM/8401-flow-vector-result
    Use OpenMP for vector calculations
    
    Janitor: Use field of double instead of float to avoid rounding errors. If float is used in field, the current editor is has rounding effects that makes the text representation of 0.1 turn into 0.09999999....

--- ApplicationLibCode/ProjectDataModel/RimElementVectorResult.cpp
+++ ApplicationLibCode/ProjectDataModel/RimElementVectorResult.cpp
@@ -93,17 +93,17 @@ RimElementVectorResult::RimElementVectorResult()
     m_vectorSurfaceCrossingLocation.uiCapability()->setUiReadOnly(
         m_vectorView() == RimElementVectorResult::VectorView::CELL_CENTER_TOTAL );
 
-    CAF_PDM_InitField( &m_showVectorI, "ShowVectorI", true, "I", "", "", "" );
-    CAF_PDM_InitField( &m_showVectorJ, "ShowVectorJ", true, "J", "", "", "" );
-    CAF_PDM_InitField( &m_showVectorK, "ShowVectorK", true, "K", "", "", "" );
-    CAF_PDM_InitField( &m_showNncData, "ShowNncData", true, "Show NNC Data", "", "", "" );
-    CAF_PDM_InitField( &m_threshold, "Threshold", 0.0f, "Threshold", "", "", "" );
+    CAF_PDM_InitField( &m_showVectorI, "ShowVectorI", true, "I" );
+    CAF_PDM_InitField( &m_showVectorJ, "ShowVectorJ", true, "J" );
+    CAF_PDM_InitField( &m_showVectorK, "ShowVectorK", true, "K" );
+    CAF_PDM_InitField( &m_showNncData, "ShowNncData", true, "Show NNC Data" );
+    CAF_PDM_InitField( &m_threshold, "Threshold", 0.0, "Threshold" );
 
     CAF_PDM_InitFieldNoDefault( &m_vectorColor, "VectorColor", "Color", "", "", "" );
     cvf::Color3f defaultUniformColor = cvf::Color3f::BLACK;
     CAF_PDM_InitField( &m_uniformVectorColor, "UniformVectorColor", defaultUniformColor, "Uniform Vector Color", "", "", "" );
 
-    CAF_PDM_InitField( &m_sizeScale, "SizeScale", 1.0f, "Size Scale", "", "", "" );
+    CAF_PDM_InitField( &m_sizeScale, "SizeScale", 1.0, "Size Scale" );
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -209,7 +209,7 @@ RimElementVectorResult::VectorSurfaceCrossingLocation RimElementVectorResult::ve
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-float RimElementVectorResult::threshold() const
+double RimElementVectorResult::threshold() const
 {
     return m_threshold();
 }
@@ -217,7 +217,7 @@ float RimElementVectorResult::threshold() const
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-float RimElementVectorResult::sizeScale() const
+double RimElementVectorResult::sizeScale() const
 {
     return m_sizeScale();
 }
