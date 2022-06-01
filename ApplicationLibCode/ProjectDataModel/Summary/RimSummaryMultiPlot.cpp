cf9f69b62062e23139459e7810d95a56dc9d60ef
Jon Jenssen <jon@soundsoft.no>
2022-06-01 13:41:16 +0200

    Fix plot layout after plot visibility filter has been applied

--- ApplicationLibCode/ProjectDataModel/Summary/RimSummaryMultiPlot.cpp
+++ ApplicationLibCode/ProjectDataModel/Summary/RimSummaryMultiPlot.cpp
@@ -147,11 +147,11 @@ RimSummaryMultiPlot::RimSummaryMultiPlot()
 
     CAF_PDM_InitFieldNoDefault( &m_axisRangeAggregation, "AxisRangeAggregation", "Axis Range Aggregation" );
 
-    CAF_PDM_InitField( &m_hidePlotsWithValuesBelow, "HidePlotsWithValuesBelow", false, "Hide Plots With Values Below" );
+    CAF_PDM_InitField( &m_hidePlotsWithValuesBelow, "HidePlotsWithValuesBelow", false, "" );
     m_hidePlotsWithValuesBelow.xmlCapability()->disableIO();
     m_hidePlotsWithValuesBelow.uiCapability()->setUiEditorTypeName( caf::PdmUiPushButtonEditor::uiEditorTypeName() );
 
-    CAF_PDM_InitField( &m_plotFilterYAxisThreshold, "PlotFilterYAxisThreshold", 0.0, "Y-Axis Threshold" );
+    CAF_PDM_InitField( &m_plotFilterYAxisThreshold, "PlotFilterYAxisThreshold", 0.0, "Y-Axis Filter Threshold" );
 
     CAF_PDM_InitFieldNoDefault( &m_sourceStepping, "SourceStepping", "" );
 
@@ -382,8 +382,8 @@ void RimSummaryMultiPlot::defineUiOrdering( QString uiConfigName, caf::PdmUiOrde
     if ( m_autoAdjustAppearance() ) m_linkSubPlotAxes = false;
 
     auto plotVisibilityFilterGroup = uiOrdering.addNewGroup( "Plot Visibility Filter" );
-    plotVisibilityFilterGroup->add( &m_hidePlotsWithValuesBelow );
     plotVisibilityFilterGroup->add( &m_plotFilterYAxisThreshold );
+    plotVisibilityFilterGroup->add( &m_hidePlotsWithValuesBelow );
 
     auto dataSourceGroup = uiOrdering.addNewGroup( "Data Source" );
     dataSourceGroup->setCollapsedByDefault( true );
@@ -1035,6 +1035,8 @@ void RimSummaryMultiPlot::updatePlotVisibility()
         bool hasValueAboveLimit = hasValuesAboveLimit( plot, m_plotFilterYAxisThreshold );
         plot->setShowWindow( hasValueAboveLimit );
     }
+
+    updateLayout();
 
     if ( !m_viewer.isNull() ) m_viewer->scheduleUpdate();
 }
