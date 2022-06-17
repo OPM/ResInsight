1e5b62c0ee79550da5bcdd7a9fb7a1e19529a780
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2022-06-17 12:18:21 +0200

    Add update

--- ApplicationLibCode/ProjectDataModel/WellLog/RimWellLogTrack.cpp
+++ ApplicationLibCode/ProjectDataModel/WellLog/RimWellLogTrack.cpp
@@ -1338,6 +1338,8 @@ void RimWellLogTrack::onLoadDataAndUpdate()
     m_explicitTickIntervals.uiCapability()->setUiReadOnly( emptyRange );
     m_propertyValueAxisGridVisibility.uiCapability()->setUiReadOnly( emptyRange );
 
+    updateDepthZoom();
+
     updateLegend();
 }
 
