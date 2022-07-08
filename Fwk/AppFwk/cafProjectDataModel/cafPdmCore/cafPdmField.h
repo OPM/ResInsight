3d3343105e4ac393946fea8de4ba9878b8b7b53b
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2022-07-08 10:35:47 +0200

    Fwk : Remove obsolete helper template

--- Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafPdmField.h
+++ Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafPdmField.h
@@ -6,20 +6,3 @@
 #undef PdmDataValueField
 #undef CAF_IS_DEFINING_PDM_FIELD
 
-#ifndef __clang__
-namespace caf
-{
-// Specialization to create compiler errors to help finding the PdmField's to rename
-
-#ifdef WIN32
-
-template <typename DataType>
-class PdmField<DataType*> : public Rename_PdmField_of_pointer_to_PdmChildField // You must rename PdmField<T*> to
-                                                                               // PdmChildField<T*>
-{
-};
-
-#endif // WIN32
-
-} // namespace caf
-#endif // __clang__
