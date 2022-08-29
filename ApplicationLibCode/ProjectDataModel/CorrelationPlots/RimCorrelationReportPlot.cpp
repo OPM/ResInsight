OPM/ResInsight

commit 71d18b9e761e9079cbca2ffbb430b0afdf85291b

#9093 MultiPlot: improve axis alignment by resizing scales

ApplicationLibCode/UserInterface/RiuMultiPlotPage.cpp
@@ -49,10 +49,12 @@
 
 #include "cvfAssert.h"
 
+#include "qwt_axis.h"
 #include "qwt_legend.h"
 #include "qwt_plot_layout.h"
 #include "qwt_plot_renderer.h"
 #include "qwt_scale_draw.h"
+#include "qwt_scale_widget.h"
 
 #include <QDebug>
 #include <QDesktopWidget>
@@ -579,6 +581,7 @@ void RiuMultiPlotPage::performUpdate( RiaDefines::MultiPlotPageUpdateType whatTo
 
         reinsertPlotWidgets();
         alignCanvasTops();
+        alignAxes();
         return;
     }
 
@@ -586,7 +589,9 @@ void RiuMultiPlotPage::performUpdate( RiaDefines::MultiPlotPageUpdateType whatTo
     {
         refreshLegends();
         alignCanvasTops();
+        alignAxes();
     }
+
     if ( ( whatToUpdate & RiaDefines::MultiPlotPageUpdateType::TITLE ) == RiaDefines::MultiPlotPageUpdateType::TITLE )
     {
         updateSubTitles();
@@ -624,6 +629,8 @@ void RiuMultiPlotPage::reinsertPlotWidgets()
     QList<QPointer<RiuQwtPlotLegend>> legends     = this->legendsForVisiblePlots();
     QList<QPointer<RiuPlotWidget>>    plotWidgets = this->visiblePlotWidgets();
 
+    m_visibleIndexToPositionMapping.clear();
+
     if ( !plotWidgets.empty() )
     {
         auto [rowCount, columnCount] = this->rowAndColumnCount( plotWidgets.size() );
@@ -636,7 +643,10 @@ void RiuMultiPlotPage::reinsertPlotWidgets()
             int colSpan         = std::min( expectedColSpan, columnCount );
             int rowSpan         = plotWidgets[visibleIndex]->rowSpan();
 
-            std::tie( row, column ) = findAvailableRowAndColumn( row, column, colSpan, columnCount );
+            auto position           = findAvailableRowAndColumn( row, column, colSpan, columnCount );
+            std::tie( row, column ) = position;
+
+            m_visibleIndexToPositionMapping[visibleIndex] = position;
 
             m_gridLayout->addWidget( subTitles[visibleIndex], 3 * row, column, 1, colSpan );
             if ( legends[visibleIndex] )
@@ -927,4 +937,88 @@ void RiuMultiPlotPage::updateTitleFont()
     auto titleFont = m_plotTitle->font();
     titleFont.setPixelSize( m_titleFontPixelSize );
     m_plotTitle->setFont( titleFont );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RiuMultiPlotPage::alignAxes()
+{
+    auto [rowCount, columnCount] = rowAndColumnCount( visiblePlotWidgets().size() );
+
+    auto matchRow = []( int row, int column, int targetRow ) { return row == targetRow; };
+
+    for ( int row = 0; row < rowCount; row++ )
+    {
+        alignAxis( QwtAxisId( QwtAxis::Position::XTop, 0 ), row, matchRow );
+        alignAxis( QwtAxisId( QwtAxis::Position::XBottom, 0 ), row, matchRow );
+    }
+
+    auto matchColumn = []( int row, int column, int targetColumn ) { return column == targetColumn; };
+
+    for ( int column = 0; column < columnCount; column++ )
+    {
+        alignAxis( QwtAxisId( QwtAxis::Position::YLeft, 0 ), column, matchColumn );
+        alignAxis( QwtAxisId( QwtAxis::Position::YRight, 0 ), column, matchColumn );
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RiuMultiPlotPage::alignAxis( QwtAxisId axis, int targetRowOrColumn, std::function<bool( int, int, int )> matchPosition )
+{
+    auto rowAndColumnFromIdx = [this]( int idx ) {
+        auto hit = m_visibleIndexToPositionMapping.find( idx );
+        CAF_ASSERT( hit != m_visibleIndexToPositionMapping.end() );
+        return hit->second;
+    };
+
+    QList<QPointer<RiuPlotWidget>> plotWidgets = visiblePlotWidgets();
+
+    // Find the max extent of the "scale draws" for the given axis
+    double maxExtent = 0;
+    for ( int tIdx = 0; tIdx < plotWidgets.size(); ++tIdx )
+    {
+        RiuPlotWidget* plotWidget = plotWidgets[tIdx];
+        auto [row, column]        = rowAndColumnFromIdx( tIdx );
+        bool matchesRowOrColumn   = matchPosition( row, column, targetRowOrColumn );
+        if ( plotWidget && matchesRowOrColumn )
+        {
+            RiuQwtPlotWidget* riuQwtPlotWidget = dynamic_cast<RiuQwtPlotWidget*>( plotWidget );
+
+            if ( riuQwtPlotWidget )
+            {
+                QwtPlot* p = riuQwtPlotWidget->qwtPlot();
+                if ( p )
+                {
+                    QwtScaleWidget* scaleWidget = p->axisWidget( axis );
+                    QwtScaleDraw*   sd          = scaleWidget->scaleDraw();
+                    sd->setMinimumExtent( 0.0 );
+                    maxExtent = std::max( sd->extent( scaleWidget->font() ), maxExtent );
+                }
+            }
+        }
+    }
+
+    // Set minimum extent for all "scale draws" for the given axis.
+    for ( int tIdx = 0; tIdx < plotWidgets.size(); ++tIdx )
+    {
+        RiuPlotWidget* plotWidget = plotWidgets[tIdx];
+        auto [row, column]        = rowAndColumnFromIdx( tIdx );
+        bool matchesRowOrColumn   = matchPosition( row, column, targetRowOrColumn );
+        if ( plotWidget && matchesRowOrColumn )
+        {
+            RiuQwtPlotWidget* riuQwtPlotWidget = dynamic_cast<RiuQwtPlotWidget*>( plotWidget );
+            if ( riuQwtPlotWidget )
+            {
+                QwtPlot* p = riuQwtPlotWidget->qwtPlot();
+                if ( p )
+                {
+                    QwtScaleWidget* scaleWidget = p->axisWidget( axis );
+                    scaleWidget->scaleDraw()->setMinimumExtent( maxExtent );
+                }
+            }
+        }
+    }
 }

ApplicationLibCode/UserInterface/RiuMultiPlotPage.h
@@ -24,6 +24,8 @@
 #include "cafPdmPointer.h"
 #include "cafSelectionChangedReceiver.h"
 
+#include "qwt_axis_id.h"
+
 #include <QFrame>
 #include <QGridLayout>
 #include <QHBoxLayout>
@@ -31,6 +33,7 @@
 #include <QPointer>
 #include <QWidget>
 
+#include <functional>
 #include <map>
 
 class RiaPlotWindowRedrawScheduler;
@@ -105,6 +108,9 @@ protected:
 
     std::pair<int, int> rowAndColumnCount( int plotWidgetCount ) const;
 
+    void alignAxes();
+    void alignAxis( QwtAxisId axis, int row, std::function<bool( int, int, int )> positionMatcher );
+
     void onSelectionManagerSelectionChanged( const std::set<int>& changedSelectionLevels ) override;
 
     virtual bool showYAxis( int row, int column ) const;
@@ -128,15 +134,16 @@ private slots:
     void onLegendUpdated();
 
 protected:
-    QPointer<QVBoxLayout>             m_layout;
-    QPointer<QHBoxLayout>             m_plotLayout;
-    QPointer<QFrame>                  m_plotWidgetFrame;
-    QPointer<QGridLayout>             m_gridLayout;
-    QPointer<QLabel>                  m_plotTitle;
-    QList<QPointer<QLabel>>           m_subTitles;
-    QList<QPointer<RiuQwtPlotLegend>> m_legends;
-    QList<QPointer<RiuPlotWidget>>    m_plotWidgets;
-    caf::PdmPointer<RimPlotWindow>    m_plotDefinition;
+    QPointer<QVBoxLayout>              m_layout;
+    QPointer<QHBoxLayout>              m_plotLayout;
+    QPointer<QFrame>                   m_plotWidgetFrame;
+    QPointer<QGridLayout>              m_gridLayout;
+    QPointer<QLabel>                   m_plotTitle;
+    QList<QPointer<QLabel>>            m_subTitles;
+    QList<QPointer<RiuQwtPlotLegend>>  m_legends;
+    QList<QPointer<RiuPlotWidget>>     m_plotWidgets;
+    std::map<int, std::pair<int, int>> m_visibleIndexToPositionMapping;
+    caf::PdmPointer<RimPlotWindow>     m_plotDefinition;
 
     int m_titleFontPixelSize;
     int m_subTitleFontPixelSize;

ApplicationLibCode/UserInterface/RiuSummaryMultiPlotPage.cpp
@@ -73,6 +73,8 @@ void RiuSummaryMultiPlotPage::reinsertPlotWidgets()
     QList<QPointer<RiuQwtPlotLegend>> legends     = this->legendsForVisiblePlots();
     QList<QPointer<RiuPlotWidget>>    plotWidgets = this->visiblePlotWidgets();
 
+    m_visibleIndexToPositionMapping.clear();
+
     int visibleIndex = 0;
     int phIndex      = 0;
 
@@ -89,6 +91,8 @@ void RiuSummaryMultiPlotPage::reinsertPlotWidgets()
                 phIndex++;
                 continue;
             }
+
+            m_visibleIndexToPositionMapping[visibleIndex] = std::make_pair( row, col );
 
             auto plotWidget      = plotWidgets[visibleIndex];
             int  expectedColSpan = plotWidget->colSpan();

ApplicationLibCode/UserInterface/RiuWellLogPlot.cpp
@@ -160,4 +160,5 @@ void RiuWellLogPlot::performUpdate( RiaDefines::MultiPlotPageUpdateType /* whatT
     reinsertScrollbar();
     int axisShift = alignCanvasTops();
     alignScrollbar( axisShift );
+    alignAxes();
 }

commit 5c30bc0e625c64572a1a3e902b811cf2abb59e47

#9233 Correction Report Plot: disable axes alignment for better spacing.
Regression introduced in 71d18b9e761e.

ApplicationLibCode/ProjectDataModel/CorrelationPlots/RimCorrelationReportPlot.cpp
@@ -281,6 +281,7 @@ void RimCorrelationReportPlot::doRenderWindowContent( QPaintDevice* paintDevice 
 QWidget* RimCorrelationReportPlot::createViewWidget( QWidget* mainWindowParent /*= nullptr */ )
 {
     m_viewer = new RiuMultiPlotPage( this, mainWindowParent );
+    m_viewer->setAutoAlignAxes( false );
     m_viewer->setPlotTitle( m_plotWindowTitle() );
     recreatePlotWidgets();
 

ApplicationLibCode/UserInterface/RiuMultiPlotPage.cpp
@@ -82,6 +82,7 @@ RiuMultiPlotPage::RiuMultiPlotPage( RimPlotWindow* plotDefinition, QWidget* pare
     , m_plotDefinition( plotDefinition )
     , m_previewMode( false )
     , m_showSubTitles( false )
+    , m_autoAlignAxes( true )
     , m_titleFontPixelSize( 12 )
     , m_subTitleFontPixelSize( 11 )
     , m_legendFontPixelSize( 8 )
@@ -595,7 +596,7 @@ void RiuMultiPlotPage::performUpdate( RiaDefines::MultiPlotPageUpdateType whatTo
 
         reinsertPlotWidgets();
         alignCanvasTops();
-        alignAxes();
+        if ( m_autoAlignAxes ) alignAxes();
         return;
     }
 
@@ -603,7 +604,7 @@ void RiuMultiPlotPage::performUpdate( RiaDefines::MultiPlotPageUpdateType whatTo
     {
         refreshLegends();
         alignCanvasTops();
-        alignAxes();
+        if ( m_autoAlignAxes ) alignAxes();
     }
 
     if ( ( whatToUpdate & RiaDefines::MultiPlotPageUpdateType::TITLE ) == RiaDefines::MultiPlotPageUpdateType::TITLE )
@@ -1176,4 +1177,12 @@ void RiuMultiPlotPage::alignAxis( QwtAxisId axis, int targetRowOrColumn, std::fu
             }
         }
     }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RiuMultiPlotPage::setAutoAlignAxes( bool autoAlignAxes )
+{
+    m_autoAlignAxes = autoAlignAxes;
 }

ApplicationLibCode/UserInterface/RiuMultiPlotPage.h
@@ -82,6 +82,8 @@ public:
     bool previewModeEnabled() const;
     void setPagePreviewModeEnabled( bool previewMode );
 
+    void setAutoAlignAxes( bool autoAlignAxes );
+
     void scheduleUpdate( RiaDefines::MultiPlotPageUpdateType whatToUpdate = RiaDefines::MultiPlotPageUpdateType::ALL );
     void scheduleReplotOfAllPlots();
     virtual void updateVerticalScrollBar( double visibleMin, double visibleMax, double totalMin, double totalMax ) {}
@@ -185,6 +187,7 @@ protected:
 
     bool m_previewMode;
     bool m_showSubTitles;
+    bool m_autoAlignAxes;
 
     std::map<RiuQwtPlotLegend*, int> m_childCountForAdjustSizeOperation;
 