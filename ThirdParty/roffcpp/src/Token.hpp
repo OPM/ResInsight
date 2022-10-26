c0a5a4b1f6c280e1cdd1b4b6a787baa1f6bbdc50
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2022-10-26 12:04:22 +0200

    Roff: cmake scaffolding for roff parsing.

---
+++ ThirdParty/roffcpp/src/Token.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+
+class Token
+{
+public:
+    enum class Kind
+    {
+        ROFF_ASC,
+        ROFF_BIN,
+        COMMENT,
+        TAG,
+        ENDTAG,
+        STRING_LITERAL,
+        NUMERIC_VALUE,
+        BINARY_NUMERIC_VALUE,
+        NAME,
+        CHAR,
+        BOOL,
+        BYTE,
+        INT,
+        FLOAT,
+        DOUBLE,
+        ARRAY,
+        ARRAYBLOB,
+    };
+
+    Token( Kind, size_t start, size_t end );
+
+    Kind   kind() const;
+    size_t start() const;
+    size_t end() const;
+
+private:
+    Kind   m_kind;
+    size_t m_start;
+    size_t m_end;
+};
