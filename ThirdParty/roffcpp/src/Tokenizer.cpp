e17bc9e9bd54a509aa329ea876077c12e44b75a7
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2022-10-26 15:39:29 +0200

    Roff: add tokenizer for space, comments and ascii strings.

--- ThirdParty/roffcpp/src/Tokenizer.cpp
+++ ThirdParty/roffcpp/src/Tokenizer.cpp
@@ -1,6 +1,128 @@
 #include "Tokenizer.hpp"
 
+#include <cctype>
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 Token Tokenizer::tokenizeComment( std::istream& stream )
 {
-    return Token( Token::Kind::TAG, 0, 0 );
+    auto start    = stream.tellg();
+    char readChar = stream.get();
+    if ( readChar != '#' )
+    {
+        stream.seekg( start );
+        throw std::runtime_error( "Expected comment." );
+    }
+
+    auto end = stream.tellg();
+    readChar = stream.get();
+    while ( stream.good() && readChar != '#' )
+    {
+        readChar = stream.get();
+    }
+
+    if ( !stream.good() )
+    {
+        stream.seekg( start );
+        throw std::runtime_error( "Reached end of stream while reading comment." );
+    }
+
+    end = stream.tellg();
+    return Token( Token::Kind::COMMENT, start, end );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void Tokenizer::tokenizeSpace( std::istream& stream )
+{
+    auto start    = stream.tellg();
+    char readChar = stream.get();
+    if ( !std::isspace( readChar ) )
+    {
+        stream.seekg( start );
+        throw std::runtime_error( "Expected space." );
+    }
+
+    auto end = stream.tellg();
+    readChar = stream.get();
+    while ( stream.good() && std::isspace( readChar ) )
+    {
+        end      = stream.tellg();
+        readChar = stream.get();
+    }
+
+    stream.seekg( end );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+Token Tokenizer::tokenizeString( std::istream& stream )
+{
+    tokenizeDelimiter( stream );
+
+    // First part of string should be double-quote
+    auto start    = stream.tellg();
+    char readChar = stream.get();
+    if ( readChar != '"' )
+    {
+        stream.seekg( start );
+        throw std::runtime_error( "Expected string." );
+    }
+
+    // Read until closing double-quote.
+    start    = stream.tellg();
+    auto end = stream.tellg();
+    readChar = stream.get();
+    while ( stream.good() && readChar != '"' )
+    {
+        end      = stream.tellg();
+        readChar = stream.get();
+    }
+
+    if ( !stream.good() )
+    {
+        // Reached unexpected end of file.
+        stream.seekg( start );
+        throw std::runtime_error( "Reached end of stream while reading string literal." );
+    }
+
+    return Token( Token::Kind::STRING_LITERAL, start, end );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void Tokenizer::tokenizeDelimiter( std::istream& stream )
+{
+    // TODO: improve this code
+    auto f1 = []( std::istream& s ) {
+        try
+        {
+            tokenizeSpace( s );
+            return true;
+        }
+        catch ( const std::runtime_error& e )
+        {
+            return false;
+        }
+    };
+
+    auto f2 = []( std::istream& s ) {
+        try
+        {
+            tokenizeComment( s );
+            return true;
+        }
+        catch ( const std::runtime_error& e )
+        {
+            return false;
+        }
+    };
+
+    while ( stream.good() && ( f1( stream ) || f2( stream ) ) )
+    {
+    }
 }
--- ThirdParty/roffcpp/tests/TokenizerTests.cpp
+++ ThirdParty/roffcpp/tests/TokenizerTests.cpp
@@ -10,6 +10,84 @@
 //--------------------------------------------------------------------------------------------------
 TEST( TokenizerTests, testTokenizeComment )
 {
-    Token token = Tokenizer::tokenizeComment( "# This is a comment. #" );
+    std::stringstream stream( "#This is a comment.#" );
+    Token             token = Tokenizer::tokenizeComment( stream );
     ASSERT_EQ( Token::Kind::COMMENT, token.kind() );
+    ASSERT_EQ( 0, token.start() );
+    ASSERT_EQ( 20, token.end() );
+    ASSERT_EQ( 20, stream.tellg() );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( TokenizerTests, testTokenizeCommentWithoutStartTag )
+{
+    std::stringstream stream( "This comment did not have a start comment tag#" );
+    ASSERT_THROW( Tokenizer::tokenizeComment( stream ), std::runtime_error );
+    ASSERT_EQ( 0, stream.tellg() );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( TokenizerTests, testTokenizeCommentWithoutEndTag )
+{
+    std::stringstream stream( "#This is an incomplete comment." );
+    ASSERT_THROW( Tokenizer::tokenizeComment( stream ), std::runtime_error );
+    ASSERT_EQ( -1, stream.tellg() );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( TokenizerTests, testTokenizeSpace )
+{
+    std::stringstream stream( "     This is a string with leading spaces" );
+    Tokenizer::tokenizeSpace( stream );
+    // No token is produced, but stream position is moved to first non-space
+    ASSERT_EQ( 5, stream.tellg() );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( TokenizerTests, testTokenizeNonSpaceAsSpace )
+{
+    std::stringstream stream( "not-really-space" );
+    ASSERT_THROW( Tokenizer::tokenizeSpace( stream ), std::runtime_error );
+    ASSERT_EQ( 0, stream.tellg() );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( TokenizerTests, testTokenizeDelimiterSpace )
+{
+    std::stringstream stream( "   s" );
+    Tokenizer::tokenizeDelimiter( stream );
+    ASSERT_EQ( 3, stream.tellg() );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( TokenizerTests, testTokenizeDelimiterComment )
+{
+    std::stringstream stream( "   #comment here before more space#    s" );
+    Tokenizer::tokenizeDelimiter( stream );
+    ASSERT_EQ( 39, stream.tellg() );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+TEST( TokenizerTests, testTokenizeAsciiString )
+{
+    std::stringstream stream( "\"string\"" );
+    Token             token = Tokenizer::tokenizeString( stream );
+    ASSERT_EQ( Token::Kind::STRING_LITERAL, token.kind() );
+    ASSERT_EQ( 1, token.start() );
+    ASSERT_EQ( 7, token.end() );
+    ASSERT_EQ( 8, stream.tellg() );
 }
