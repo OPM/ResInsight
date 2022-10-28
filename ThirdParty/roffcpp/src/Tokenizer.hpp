2c600e3e0a3a85e0d7a68f5df727a4bd180c5421
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2022-10-28 16:08:37 +0200

    Roff: add tokenizer for simple tag.

--- ThirdParty/roffcpp/src/Tokenizer.hpp
+++ ThirdParty/roffcpp/src/Tokenizer.hpp
@@ -3,16 +3,22 @@
 #include "Token.hpp"
 
 #include <istream>
+#include <vector>
 
 class Tokenizer
 {
 public:
-    static Token tokenizeComment( std::istream& stream );
-    static void  tokenizeSpace( std::istream& stream );
-    static void  tokenizeDelimiter( std::istream& stream );
-    static Token tokenizeString( std::istream& stream );
-    static Token tokenizeAsciiNumber( std::istream& stream );
-    static Token tokenizeName( std::istream& stream );
-    static Token tokenizeKeyword( std::istream& stream );
+    static Token              tokenizeComment( std::istream& stream );
+    static void               tokenizeSpace( std::istream& stream );
+    static void               tokenizeDelimiter( std::istream& stream );
+    static Token              tokenizeString( std::istream& stream );
+    static Token              tokenizeAsciiNumber( std::istream& stream );
+    static Token              tokenizeName( std::istream& stream );
+    static Token              tokenizeKeyword( std::istream& stream );
+    static Token              tokenizeSimpleType( std::istream& stream );
+    static Token              tokenizeValue( std::istream& stream );
+    static std::vector<Token> tokenizeAsciiTagKey( std::istream& stream );
+
+    static Token tokenizeKeyword( std::istream& stream, const std::vector<std::pair<Token::Kind, std::string>>& keywords );
     static Token tokenizeWord( std::istream& stream, const std::string& keywork, Token::Kind kind );
 };
