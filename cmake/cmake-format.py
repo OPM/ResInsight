45f6db2b7ce29243d907feed18690bbb5dc13042
Jon Jenssen <jon@soundsoft.no>
2022-12-06 13:33:27 +0100

    Revert cmake format test.

--- cmake/cmake-format.py
+++ cmake/cmake-format.py
@@ -18,7 +18,3 @@ with section("format"):
     # If a statement is wrapped to more than one line, than dangle the closing
     # parenthesis on its own line.
     dangle_parens = True
-
-    # If an argument group contains more than this many sub-groups (parg or kwarg
-    # groups) then force it to a vertical layout.
-    max_subgroups_hwrap = 1
