f5c5c90706134bfad7cd370a3277cb4b2f93da13
jorgenherje <jorgenherje@users.noreply.github.com>
2023-01-05 10:52:43 +0000

    Fixes by cmake-format

--- ApplicationLibCode/ProjectDataModel/Flow/CMakeLists_files.cmake
+++ ApplicationLibCode/ProjectDataModel/Flow/CMakeLists_files.cmake
@@ -15,9 +15,8 @@ set(SOURCE_GROUP_HEADER_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlot.h
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlotCollection.h
     ${CMAKE_CURRENT_LIST_DIR}/RimHistoryWellAllocationPlot.h
-	${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationTools.h
-	${CMAKE_CURRENT_LIST_DIR}/RimHistoryWellFlowDataCollection.h
-	
+    ${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationTools.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimHistoryWellFlowDataCollection.h
 )
 
 set(SOURCE_GROUP_SOURCE_FILES
@@ -37,8 +36,8 @@ set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlot.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlotCollection.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RimHistoryWellAllocationPlot.cpp
-	${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationTools.cpp
-	${CMAKE_CURRENT_LIST_DIR}/RimHistoryWellFlowDataCollection.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationTools.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimHistoryWellFlowDataCollection.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
