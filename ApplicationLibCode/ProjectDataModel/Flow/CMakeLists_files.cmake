812382a1c0b75c0d4fbf58cccce43086dfc392d9
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2023-01-10 11:43:44 +0100

    Refactor WellAllocationPlot
    - Move utility function to common WellAllocationTools namespace
    - Remove unnecessary ifdef from RimWellAllocationPlot
    - Utilize `findSortedWellNames()`method for eclipseCaseData

--- ApplicationLibCode/ProjectDataModel/Flow/CMakeLists_files.cmake
+++ ApplicationLibCode/ProjectDataModel/Flow/CMakeLists_files.cmake
@@ -14,6 +14,10 @@ set(SOURCE_GROUP_HEADER_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RimWellRftEnsembleCurveSet.h
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlot.h
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlotCollection.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationOverTimePlot.h
+	${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationTools.h
+	${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationOverTimeCollection.h
+	
 )
 
 set(SOURCE_GROUP_SOURCE_FILES
@@ -32,6 +36,9 @@ set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RimWellRftEnsembleCurveSet.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlot.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RimWellDistributionPlotCollection.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationOverTimePlot.cpp
+	${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationTools.cpp
+	${CMAKE_CURRENT_LIST_DIR}/RimWellAllocationOverTimeCollection.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
