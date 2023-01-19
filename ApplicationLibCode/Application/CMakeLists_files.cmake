a356e76d52c2bab79ff6e824ec03e744a7983fac
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2023-01-19 14:06:48 +0100

    Add RiaDefined for LAS file depth property names

--- ApplicationLibCode/Application/CMakeLists_files.cmake
+++ ApplicationLibCode/Application/CMakeLists_files.cmake
@@ -31,6 +31,7 @@ set(SOURCE_GROUP_HEADER_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RiaPlotCollectionScheduler.h
     ${CMAKE_CURRENT_LIST_DIR}/RiaScheduler.h
     ${CMAKE_CURRENT_LIST_DIR}/RiaSummaryDefines.h
+	${CMAKE_CURRENT_LIST_DIR}/RiaLasDefines.h
 )
 
 set(SOURCE_GROUP_SOURCE_FILES
@@ -66,6 +67,7 @@ set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RiaPlotCollectionScheduler.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RiaScheduler.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RiaSummaryDefines.cpp
+	${CMAKE_CURRENT_LIST_DIR}/RiaLasDefines.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
