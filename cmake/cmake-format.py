a7138a560a2e35f70dd77a1e4c0acc13aae43eb9
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-01-27 18:28:21 +0100

    Reset width to 80

--- cmake/cmake-format.py
+++ cmake/cmake-format.py
@@ -4,7 +4,7 @@
 # -----------------------------
 with section("format"):
     # How wide to allow formatted cmake files
-    line_width = 100
+    line_width = 80
 
     # How many spaces to tab for indent
     tab_size = 2
