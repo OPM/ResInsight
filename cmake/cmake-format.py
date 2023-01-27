325c7e9990153f2e2f4988f1234df0dec83ab2fc
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-01-27 16:44:26 +0100

    Set cmake line width to 120

--- cmake/cmake-format.py
+++ cmake/cmake-format.py
@@ -4,7 +4,7 @@
 # -----------------------------
 with section("format"):
     # How wide to allow formatted cmake files
-    line_width = 80
+    line_width = 120
 
     # How many spaces to tab for indent
     tab_size = 2
