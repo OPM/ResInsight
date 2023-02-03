9e73b2df500666ebc4276fcfa2f245033f64ebbe
jonjenssen <jon@soundsoft.no>
2023-02-03 15:44:01 +0100

    Use new reader interface

--- ApplicationLibCode/FileInterface/RifSeismicZGYReader.cpp
+++ ApplicationLibCode/FileInterface/RifSeismicZGYReader.cpp
@@ -18,7 +18,7 @@
 
 #include "RifSeismicZGYReader.h"
 
-#include <openzgy/openzgy.h>
+#include <zgyaccess/zgyreader.h>
 
 //--------------------------------------------------------------------------------------------------
 ///
@@ -27,7 +27,6 @@ RifSeismicZGYReader::RifSeismicZGYReader( QString filename )
     : m_filename( filename )
     , m_reader( nullptr )
 {
-    //    m_reader = OpenZGY::IZgyReader::open( filename.toStdString() );
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -46,7 +45,8 @@ bool RifSeismicZGYReader::Open()
 
     try
     {
-        m_reader = OpenZGY::IZgyReader::open( m_filename.toStdString() );
+        m_reader = std::make_shared<ZGYAccess::ZGYReader>();
+        m_reader->Open( m_filename.toStdString() );
     }
     catch ( const std::exception& err )
     {
@@ -66,7 +66,7 @@ void RifSeismicZGYReader::Close()
 
     try
     {
-        m_reader->close();
+        m_reader->Close();
     }
     catch ( const std::exception& )
     {
@@ -86,95 +86,12 @@ std::vector<std::pair<QString, QString>> RifSeismicZGYReader::metaData()
 
     if ( m_reader == nullptr ) return retValues;
 
-    auto stats = m_reader->filestats();
+    auto stats = m_reader->MetaData();
 
-    retValues.push_back( std::make_pair( "ZGY version", QString::number( stats->fileVersion() ) ) );
-    if ( stats->isCompressed() )
+    for ( auto& [name, val] : stats )
     {
-        retValues.push_back( std::make_pair( "Compressed", "yes" ) );
-        retValues.push_back( std::make_pair( "Compression factor", QString::number( stats->compressionFactor() ) ) );
+        retValues.push_back( std::make_pair( QString::fromStdString( name ), QString::fromStdString( val ) ) );
     }
-    retValues.push_back(
-        std::make_pair( "File size", QString::number( 1.0 * stats->fileSize() / 1024.0 / 1024.0 ) + " MBytes" ) );
-    retValues.push_back( std::make_pair( "Header size", QString::number( stats->headerSize() ) + "Bytes" ) );
-
-    QString tmp;
-
-    for ( auto& a : m_reader->brickcount() )
-    {
-        tmp.append( QString( "%1x%2x%3 " ).arg( a[0] ).arg( a[1] ).arg( a[2] ) );
-    }
-    retValues.push_back( std::make_pair( "Brick count", tmp ) );
-    retValues.push_back( std::make_pair( "Levels of details", QString::number( m_reader->nlods() ) ) );
-
-    const auto& bricksize = m_reader->bricksize();
-    retValues.push_back(
-        std::make_pair( "Brick size",
-                        QString( "%1 x %2 x %3" ).arg( bricksize[0] ).arg( bricksize[1] ).arg( bricksize[2] ) ) );
-
-    switch ( m_reader->datatype() )
-    {
-        default:
-        case OpenZGY::SampleDataType::unknown:
-            tmp = "Unknown";
-            break;
-        case OpenZGY::SampleDataType::int8:
-            tmp = "Signed 8-bit";
-            break;
-        case OpenZGY::SampleDataType::int16:
-            tmp = "Signed 16-bit";
-            break;
-        case OpenZGY::SampleDataType::float32:
-            tmp = " 32-bit Floating point";
-            break;
-    }
-    retValues.push_back( std::make_pair( "Native data type", tmp ) );
-
-    const auto& datarange = m_reader->datarange();
-    retValues.push_back( std::make_pair( "Data range", QString( "%1 - %2" ).arg( datarange[0] ).arg( datarange[1] ) ) );
-
-    retValues.push_back( std::make_pair( "Depth unit", QString::fromStdString( m_reader->zunitname() ) ) );
-    retValues.push_back( std::make_pair( "Depth offset", QString::number( m_reader->zstart() ) ) );
-    retValues.push_back( std::make_pair( "Depth increment", QString::number( m_reader->zinc() ) ) );
-
-    retValues.push_back( std::make_pair( "Horizontal unit", QString::fromStdString( m_reader->hunitname() ) ) );
-
-    const auto& annotstart = m_reader->annotstart();
-    retValues.push_back( std::make_pair( "First inline", QString::number( annotstart[0] ) ) );
-    retValues.push_back( std::make_pair( "First crossline", QString::number( annotstart[1] ) ) );
-
-    const auto& annotinc = m_reader->annotinc();
-    retValues.push_back( std::make_pair( "Inline increment", QString::number( annotinc[0] ) ) );
-    retValues.push_back( std::make_pair( "Crossline increment", QString::number( annotinc[1] ) ) );
-
-    tmp = "";
-    for ( auto& c : m_reader->corners() )
-    {
-        tmp = tmp + cornerToString( c ) + " ";
-    }
-    retValues.push_back( std::make_pair( "World coord. corners", tmp ) );
-
-    tmp = "";
-    for ( auto& c : m_reader->indexcorners() )
-    {
-        tmp = tmp + cornerToString( c ) + " ";
-    }
-    retValues.push_back( std::make_pair( "Brick index corners", tmp ) );
-
-    tmp = "";
-    for ( auto& c : m_reader->annotcorners() )
-    {
-        tmp = tmp + cornerToString( c ) + " ";
-    }
-    retValues.push_back( std::make_pair( "Inline/crossline corners", tmp ) );
 
     return retValues;
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-QString RifSeismicZGYReader::cornerToString( std::array<double, 2> corner )
-{
-    return QString( "(%1, %2)" ).arg( corner[0] ).arg( corner[1] );
 }
