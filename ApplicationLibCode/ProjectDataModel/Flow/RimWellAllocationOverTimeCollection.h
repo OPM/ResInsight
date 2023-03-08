3db7ca84e1d2efb64037f887a4b186b1fa3756d3
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2023-03-08 11:09:37 +0100

    Add fraction values to WellAllocationOverTimeCollection
    Add calculation of flow rate and accumulated flow volume fraction values

--- ApplicationLibCode/ProjectDataModel/Flow/RimWellAllocationOverTimeCollection.h
+++ ApplicationLibCode/ProjectDataModel/Flow/RimWellAllocationOverTimeCollection.h
@@ -35,15 +35,27 @@ public:
     const std::vector<QDateTime>                         timeStepDates() const { return m_timeStepDates; }
     const std::map<QString, std::map<QDateTime, double>> wellValuesMap() const { return m_wellValuesMap; }
 
+    void fillWithFlowRateFractionValues();
     void fillWithFlowRatePercentageValues();
     void fillWithFlowRateValues();
     void fillWithFlowVolumeValues();
     void fillWithAccumulatedFlowVolumeValues( double smallContributionsThreshold );
+    void fillWithAccumulatedFlowVolumeFractionValues( double smallContributionsThreshold );
     void fillWithAccumulatedFlowVolumePercentageValues( double smallContributionsThreshold );
 
 private:
+    enum class FractionOrPercentage
+    {
+        FRACTION,
+        PERCENTAGE
+    };
+
+    void fillWithAccumulatedFlowVolumeFractionOrPercentageValues( FractionOrPercentage selection,
+                                                                  double               smallContributionsThreshold );
+
     void groupAccumulatedFlowVolumes( std::map<QString, std::map<QDateTime, double>>& rWellValuesMap, double threshold );
-    void groupAccumulatedFlowVolumePercentages( std::map<QString, std::map<QDateTime, double>>& rWellValuesMap, double threshold );
+    void groupAccumulatedFlowVolumeFractionsOrPercentages( std::map<QString, std::map<QDateTime, double>>& rWellValuesMap,
+                                                           double                                          threshold );
 
 private:
     std::map<QDateTime, RigAccWellFlowCalculator>  m_timeStepAndCalculatorPairs;
