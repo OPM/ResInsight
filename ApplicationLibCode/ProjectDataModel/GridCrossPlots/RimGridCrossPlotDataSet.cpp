3afffdb04970153709aedbe120b127005ef6f786
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-04-03 07:37:10 +0200

    Fix typo

--- ApplicationLibCode/ProjectDataModel/GridCrossPlots/RimGridCrossPlotDataSet.cpp
+++ ApplicationLibCode/ProjectDataModel/GridCrossPlots/RimGridCrossPlotDataSet.cpp
@@ -1106,9 +1106,13 @@ bool RimGridCrossPlotDataSet::groupingEnabled() const
 {
     if ( m_grouping != NO_GROUPING )
     {
-        // NOLINTBEGIN(readability-else-after-return)
-        return !( m_grouping == GROUP_BY_RESULT && !m_groupingProperty->eclipseResultAddress().isValid() );
-        // NOLINTEND(readability-else-after-return)
+        // NOLINTBEGIN(readability-simplify-boolean-expr)
+        if ( m_grouping == GROUP_BY_RESULT && !m_groupingProperty->eclipseResultAddress().isValid() )
+        {
+            return false;
+        }
+        return true;
+        // NOLINTEND(readability-simplify-boolean-expr)
     }
     return false;
 }
