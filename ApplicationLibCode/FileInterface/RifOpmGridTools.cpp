6d96e51e8c42a5ec327a6afddb1ac74abce64027
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-04-14 21:29:53 +0200

    Fix invalid type compare

--- ApplicationLibCode/FileInterface/RifOpmGridTools.cpp
+++ ApplicationLibCode/FileInterface/RifOpmGridTools.cpp
@@ -151,7 +151,7 @@ void RifOpmGridTools::transferCoordinates( Opm::EclIO::EGrid& opmMainGrid, Opm::
 
     std::vector<cvf::Vec3d> snapToCoordinatesFromMainGrid;
 
-    for ( int opmCellIndex = 0; opmCellIndex < cellCount; opmCellIndex++ )
+    for ( int opmCellIndex = 0; opmCellIndex < static_cast<int>( cellCount ); opmCellIndex++ )
     {
         opmGrid.getCellCorners( opmCellIndex, opmX, opmY, opmZ );
 
@@ -348,8 +348,7 @@ std::vector<std::vector<cvf::Vec3d>>
 
     std::sort( zDistanceAlongPillar.begin(), zDistanceAlongPillar.end() );
 
-    auto normalize = []( const std::vector<double>& values ) -> std::vector<double>
-    {
+    auto normalize = []( const std::vector<double>& values ) -> std::vector<double> {
         if ( values.size() < 2 ) return {};
 
         std::vector<double> normalizedValues;
