8fc44e4834e12d7724fcbb9de832328217ec7a49
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2023-04-24 12:08:18 +0200

    Fix bug selected well log file for source well
    Missing selection of well log file for source when generating depth adjusted las file - chose first las file

--- ApplicationLibCode/Commands/ExportCommands/RicCreateDepthAdjustedLasFilesImpl.cpp
+++ ApplicationLibCode/Commands/ExportCommands/RicCreateDepthAdjustedLasFilesImpl.cpp
@@ -114,12 +114,15 @@ LasDepthValueAndIndexPerKLayer
 //--------------------------------------------------------------------------------------------------
 void RicCreateDepthAdjustedLasFilesImpl::createDestinationWellsLasFiles( RimCase*                        selectedCase,
                                                                          RimWellPath*                    sourceWell,
+                                                                         RimWellLogFile*                 soureWellLogFile,
                                                                          const std::vector<RimWellPath*> destinationWells,
                                                                          const std::vector<QString>&     selectedResultProperties,
                                                                          const QString&                  exportFolder,
                                                                          double                          rkbDiff )
 {
-    auto*      sourceWellLogData  = sourceWell->wellLogFiles()[0]->wellLogFileData();
+    if ( !selectedCase || !sourceWell || !soureWellLogFile || destinationWells.empty() ) return;
+
+    auto*      sourceWellLogData  = soureWellLogFile->wellLogFileData();
     const auto defaultPropertyMap = createDefaultPropertyMap( selectedResultProperties, sourceWellLogData );
 
     // NOTE: map createIndexKDepthDataMapFromCase is created using well extractor, while sourceWellLogData depth
