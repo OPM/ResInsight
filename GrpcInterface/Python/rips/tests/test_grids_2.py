0f842abb5c6b11a8c84052a74aa10d2e8600c1dc
jorgenherje <jorgenherje@users.noreply.github.com>
2023-04-25 12:43:21 +0000

    Python code linting changes detected by black

--- GrpcInterface/Python/rips/tests/test_grids_2.py
+++ GrpcInterface/Python/rips/tests/test_grids_2.py
@@ -12,13 +12,9 @@ def test_load_grdecl_grid_with_properties(rips_instance, initialize_test):
     casePath: str = dataroot.PATH + "/reek/GRID.GRDECL"
     property_file_paths: List[str] = [
         dataroot.PATH + "/reek/EQLNUM.GRDECL",
-        dataroot.PATH + "/reek/PORO.GRDECL"
+        dataroot.PATH + "/reek/PORO.GRDECL",
     ]
 
     # Load case and import properties
     case = rips_instance.project.load_case(path=casePath)
     case.import_properties(file_names=property_file_paths)
-    
-
-
-    
