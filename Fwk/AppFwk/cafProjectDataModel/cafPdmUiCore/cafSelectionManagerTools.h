6260e7b56ae3a9a98d712da60f1fba4e3f29ce00
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-05-12 15:03:52 +0200

    Add nodiscard

--- Fwk/AppFwk/cafProjectDataModel/cafPdmUiCore/cafSelectionManagerTools.h
+++ Fwk/AppFwk/cafProjectDataModel/cafPdmUiCore/cafSelectionManagerTools.h
@@ -45,8 +45,10 @@ namespace caf
 /// first object of given type searching upwards to root using firstAncestorOrThisOfType()
 //--------------------------------------------------------------------------------------------------
 template <typename T>
-T* firstAncestorOfTypeFromSelectedObject()
+T* [[nodiscard]] firstAncestorOfTypeFromSelectedObject()
 {
+    static_assert( !std::is_pointer<T>::value );
+
     caf::PdmUiItem* pdmUiItem = caf::SelectionManager::instance()->selectedItem();
 
     caf::PdmObjectHandle* objHandle = dynamic_cast<caf::PdmObjectHandle*>( pdmUiItem );
@@ -65,7 +67,7 @@ T* firstAncestorOfTypeFromSelectedObject()
 /// have been updated to use this function instead of SelectionManager::objectsByType
 //--------------------------------------------------------------------------------------------------
 template <typename T>
-std::vector<T> selectedObjectsByType()
+std::vector<T> [[nodiscard]] selectedObjectsByType()
 {
     std::vector<T> objectByType;
     caf::SelectionManager::instance()->objectsByType( &objectByType );
@@ -78,7 +80,7 @@ std::vector<T> selectedObjectsByType()
 /// If objects of different type are selected, an empty list is returned.
 //--------------------------------------------------------------------------------------------------
 template <typename T>
-std::vector<T> selectedObjectsByTypeStrict()
+std::vector<T> [[nodiscard]] selectedObjectsByTypeStrict()
 {
     std::vector<T> objectByType;
     caf::SelectionManager::instance()->objectsByTypeStrict( &objectByType );
