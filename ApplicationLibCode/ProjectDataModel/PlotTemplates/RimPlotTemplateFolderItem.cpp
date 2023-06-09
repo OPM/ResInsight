136c9b3b19dc97885b7c829d387ca3f70f62ad3b
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-06-09 14:45:53 +0200

    Simplify icons for ensemble

--- ApplicationLibCode/ProjectDataModel/PlotTemplates/RimPlotTemplateFolderItem.cpp
+++ ApplicationLibCode/ProjectDataModel/PlotTemplates/RimPlotTemplateFolderItem.cpp
@@ -244,7 +244,7 @@ void RimPlotTemplateFolderItem::appendOptionItemsForPlotTemplatesRecursively( QL
     }
 
     caf::IconProvider templateIcon( ":/plot-template-standard.svg" );
-    caf::IconProvider ensTemplateIcon( ":/SummaryEnsembleTemplate16x16.png" );
+    caf::IconProvider ensTemplateIcon( ":/plot-template-ensemble.svg" );
 
     auto files = templateFolderItem->fileItems();
     for ( auto file : files )
