ad4930db794b28eceed8bbae6c4e3d16151692f2
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-07-06 12:58:22 +0200

    Add script used to compute statistics on generated data in Python

--- GrpcInterface/Python/rips/PythonExamples/case_grid_group.py
+++ GrpcInterface/Python/rips/PythonExamples/case_grid_group.py
@@ -3,13 +3,14 @@ import rips
 
 resinsight = rips.Instance.find()
 
+test_model_path = "e:/gitroot-second/ResInsight/TestModels"
+
 case_paths = []
-case_paths.append(
-    "C:/Users/lindk/source/repos/ResInsight/TestModels/Case_with_10_timesteps/Real0/BRUGGE_0000.EGRID"
-)
-case_paths.append(
-    "C:/Users/lindk/source/repos/ResInsight/TestModels/Case_with_10_timesteps/Real10/BRUGGE_0010.EGRID"
-)
+case_paths.append(test_model_path + "/Case_with_10_timesteps/Real0/BRUGGE_0000.EGRID")
+case_paths.append(test_model_path + "/Case_with_10_timesteps/Real10/BRUGGE_0010.EGRID")
+case_paths.append(test_model_path + "/Case_with_10_timesteps/Real30/BRUGGE_0030.EGRID")
+case_paths.append(test_model_path + "/Case_with_10_timesteps/Real40/BRUGGE_0040.EGRID")
+
 for case_path in case_paths:
     assert os.path.exists(
         case_path
