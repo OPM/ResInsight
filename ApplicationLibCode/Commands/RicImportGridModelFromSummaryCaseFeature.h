e54c101fc7bc7a073b694e171aa6f20ca99b84eb
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-08-14 17:57:26 +0200

    add forward declare

--- ApplicationLibCode/Commands/RicImportGridModelFromSummaryCaseFeature.h
+++ ApplicationLibCode/Commands/RicImportGridModelFromSummaryCaseFeature.h
@@ -22,6 +22,7 @@
 
 class RimEclipseCase;
 class RimFileSummaryCase;
+class RimSummaryCase;
 
 //==================================================================================================
 ///
