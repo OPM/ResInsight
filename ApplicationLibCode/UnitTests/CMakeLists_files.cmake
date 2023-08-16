4bf156fd95aeeb75c775afeefb2f2af59d234fb2
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2023-08-16 08:27:02 +0200

    #10437 Add utility for exporting Abaqus INP files.

--- ApplicationLibCode/UnitTests/CMakeLists_files.cmake
+++ ApplicationLibCode/UnitTests/CMakeLists_files.cmake
@@ -96,6 +96,7 @@ set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RimWellLogCalculatedCurve-Test.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RigWellLogCurveData-Test.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RiaNumericalTools-Test.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RifInpExportTools-Test.cpp
 )
 
 if(RESINSIGHT_ENABLE_GRPC)
