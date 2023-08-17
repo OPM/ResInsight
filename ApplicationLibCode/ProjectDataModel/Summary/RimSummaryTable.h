e91182671e2e82a5b40a1a0d41c7108de15cc92a
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-08-17 08:49:01 +0200

    Add missing forward declare

--- ApplicationLibCode/ProjectDataModel/Summary/RimSummaryTable.h
+++ ApplicationLibCode/ProjectDataModel/Summary/RimSummaryTable.h
@@ -38,6 +38,7 @@ class RifSummaryReaderInterface;
 class RimSummaryCase;
 class RimRegularLegendConfig;
 class RiuMatrixPlotWidget;
+class RifEclipseSummaryAddress;
 
 //==================================================================================================
 ///
