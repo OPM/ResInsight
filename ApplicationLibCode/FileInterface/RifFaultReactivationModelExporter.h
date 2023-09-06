514eb68e4085ff8b98f45868b4d767c9a05ad30a
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2023-09-06 10:20:24 +0200

    Refactor: Split INP exporter into parts.

--- ApplicationLibCode/FileInterface/RifFaultReactivationModelExporter.h
+++ ApplicationLibCode/FileInterface/RifFaultReactivationModelExporter.h
@@ -18,8 +18,11 @@
 
 #pragma once
 
-#include "cvfVector3.h"
+#include "RigFaultReactivationModel.h"
 
+#include "RigGriddedPart3d.h"
+
+#include <map>
 #include <ostream>
 #include <string>
 #include <vector>
@@ -34,4 +37,25 @@ class RifFaultReactivationModelExporter
 public:
     static std::pair<bool, std::string> exportToStream( std::ostream& stream, const RigFaultReactivationModel& model );
     static std::pair<bool, std::string> exportToFile( const std::string& filePath, const RigFaultReactivationModel& model );
+
+private:
+    static std::pair<bool, std::string> printHeading( std::ostream& stream, const std::string& applicationNameAndVersion );
+    static std::pair<bool, std::string>
+                                        printParts( std::ostream&                                                                                         stream,
+                                                    const RigFaultReactivationModel&                                                                      model,
+                                                    const std::vector<std::pair<RigGriddedPart3d::BorderSurface, std::string>>&                           borders,
+                                                    const std::map<std::pair<RigFaultReactivationModel::GridPart, RigGriddedPart3d::BorderSurface>, int>& faces );
+    static std::pair<bool, std::string> printAssembly( std::ostream&                                            stream,
+                                                       const RigFaultReactivationModel&                         model,
+                                                       const std::map<RigGriddedPart3d::Boundary, std::string>& boundaries );
+
+    static std::pair<bool, std::string> printMaterials( std::ostream& stream );
+
+    static std::pair<bool, std::string> printInteractionProperties( std::ostream& stream, double faultFriction );
+    static std::pair<bool, std::string> printBoundaryConditions( std::ostream&                                            stream,
+                                                                 const std::map<RigGriddedPart3d::Boundary, std::string>& boundaries );
+    static std::pair<bool, std::string> printPredefinedFields( std::ostream& stream );
+    static std::pair<bool, std::string> printSteps( std::ostream& stream );
+    static std::pair<bool, std::string>
+        printInteractions( std::ostream& stream, const std::vector<std::pair<RigGriddedPart3d::BorderSurface, std::string>>& borders );
 };
