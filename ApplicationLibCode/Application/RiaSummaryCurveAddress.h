43296664d8bcfb7cf6985e5347580f3df518f1fd
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-09-18 14:44:28 +0200

    Add summarycurveaddress

---
+++ ApplicationLibCode/Application/RiaSummaryCurveAddress.h
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2023 Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "RifEclipseSummaryAddress.h"
+
+//==================================================================================================
+///
+//==================================================================================================
+class RiaSummaryCurveAddress
+{
+public:
+    explicit RiaSummaryCurveAddress( const RifEclipseSummaryAddress& summaryAddressX, const RifEclipseSummaryAddress& summaryAddressY );
+
+    RifEclipseSummaryAddress summaryAddressX() const;
+    RifEclipseSummaryAddress summaryAddressY() const;
+
+    auto operator<=>( const RiaSummaryCurveAddress& rhs ) const = default;
+
+private:
+    RifEclipseSummaryAddress m_summaryAddressX;
+    RifEclipseSummaryAddress m_summaryAddressY;
+};
