4c431d481cc9ff527d2ff73ec55976b78ee9edb7
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-09-18 14:44:28 +0200

    Add summarycurveaddress

---
+++ ApplicationLibCode/Application/RiaSummaryCurveAddress.cpp
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2023 Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RiaSummaryCurveAddress.h"
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RiaSummaryCurveAddress::RiaSummaryCurveAddress( const RifEclipseSummaryAddress& summaryAddressX, const RifEclipseSummaryAddress& summaryAddressY )
+    : m_summaryAddressX( summaryAddressX )
+    , m_summaryAddressY( summaryAddressY )
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RifEclipseSummaryAddress RiaSummaryCurveAddress::summaryAddressX() const
+{
+    return m_summaryAddressX;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RifEclipseSummaryAddress RiaSummaryCurveAddress::summaryAddressY() const
+{
+    return m_summaryAddressY;
+}
