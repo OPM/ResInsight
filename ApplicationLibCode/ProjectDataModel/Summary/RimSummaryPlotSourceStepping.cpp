OPM/ResInsight

commit fb288f602ded223c46bf666094748f3e654f4433

Support ensemble cross plot curves
* Remove use of RifEnsembleStatisticsReader
* Add RimSummaryAddressSelector and RimEnsembleCrossPlotStatisticsCase
* Remove RifEnsembleStatisticsReader
* Add support for cross plot x-axis selection
* Support source stepping on ensemble cross plot curves
* Add isXAxisSummaryVector

ApplicationLibCode/ProjectDataModel/Summary/RimSummaryPlotSourceStepping.cpp
@@ -329,6 +329,7 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
     if ( dataSourceSteppingObject() ) curves = dataSourceSteppingObject()->allCurves( m_sourceSteppingType );
 
     bool isAutoZoomAllowed = false;
+    bool doZoomAll         = false;
 
     if ( changedField == &m_stepDimension )
     {
@@ -360,6 +361,25 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
     }
 
     bool triggerLoadDataAndUpdate = false;
+
+    auto updateEnsembleAddresses = [&doZoomAll, &oldValue, &newValue]( const std::vector<RimEnsembleCurveSet*>& curveSets )
+    {
+        for ( auto curveSet : curveSets )
+        {
+            auto curveAdr = curveSet->curveAddress();
+
+            auto yAddressToModify = curveAdr.summaryAddressY();
+            RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, yAddressToModify );
+
+            auto xAddressToModify = curveAdr.summaryAddressX();
+            RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, xAddressToModify );
+
+            curveSet->setCurveAddress( RiaSummaryCurveAddress( xAddressToModify, yAddressToModify ) );
+            curveSet->updateConnectedEditors();
+
+            doZoomAll = true;
+        }
+    };
 
     if ( changedField == &m_summaryCase )
     {
@@ -431,11 +451,7 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
 
         if ( dataSourceSteppingObject() )
         {
-            for ( auto curveSet : dataSourceSteppingObject()->curveSets() )
-            {
-                auto adr = curveSet->summaryAddress();
-                if ( RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, &adr ) ) curveSet->setSummaryAddress( adr );
-            }
+            updateEnsembleAddresses( dataSourceSteppingObject()->curveSets() );
         }
 
         m_vectorName.uiCapability()->updateConnectedEditors();
@@ -486,26 +502,21 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
                 if ( isYAxisStepping() )
                 {
                     RifEclipseSummaryAddress adr = curve->summaryAddressY();
-                    RimDataSourceSteppingTools::updateAddressIfMatching( oldValue, newValue, summaryCategoryToModify, &adr );
+                    RimDataSourceSteppingTools::updateAddressIfMatching( oldValue, newValue, summaryCategoryToModify, adr );
                     curve->setSummaryAddressY( adr );
                 }
 
                 if ( isXAxisStepping() )
                 {
                     RifEclipseSummaryAddress adr = curve->summaryAddressX();
-                    RimDataSourceSteppingTools::updateAddressIfMatching( oldValue, newValue, summaryCategoryToModify, &adr );
+                    RimDataSourceSteppingTools::updateAddressIfMatching( oldValue, newValue, summaryCategoryToModify, adr );
                     curve->setSummaryAddressX( adr );
                 }
             }
 
             if ( dataSourceSteppingObject() )
             {
-                for ( auto curveSet : dataSourceSteppingObject()->curveSets() )
-                {
-                    auto adr = curveSet->summaryAddress();
-                    RimDataSourceSteppingTools::updateAddressIfMatching( oldValue, newValue, summaryCategoryToModify, &adr );
-                    curveSet->setSummaryAddress( adr );
-                }
+                updateEnsembleAddresses( dataSourceSteppingObject()->curveSets() );
             }
 
             triggerLoadDataAndUpdate = true;
@@ -522,7 +533,15 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
             summaryMultiPlot->updatePlots();
             summaryMultiPlot->updatePlotTitles();
 
-            if ( isAutoZoomAllowed ) summaryMultiPlot->zoomAllYAxes();
+            if ( doZoomAll )
+            {
+                summaryMultiPlot->zoomAll();
+            }
+            else if ( isAutoZoomAllowed )
+            {
+                // The time axis can be zoomed and will be used for all plots. Do not zoom time axis in this case.
+                summaryMultiPlot->zoomAllYAxes();
+            }
 
             RiuPlotMainWindow* mainPlotWindow = RiaGuiApplication::instance()->mainPlotWindow();
             mainPlotWindow->updateMultiPlotToolBar();
@@ -1261,13 +1280,13 @@ void RimSummaryPlotSourceStepping::updateVectorNameInCurves( std::vector<RimSumm
         if ( isYAxisStepping() )
         {
             auto adr = curve->summaryAddressY();
-            if ( RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, &adr ) ) curve->setSummaryAddressY( adr );
+            if ( RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, adr ) ) curve->setSummaryAddressY( adr );
         }
 
         if ( isXAxisStepping() )
         {
             auto adr = curve->summaryAddressX();
-            if ( RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, &adr ) ) curve->setSummaryAddressX( adr );
+            if ( RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, adr ) ) curve->setSummaryAddressX( adr );
         }
 
         if ( m_autoUpdateAppearance )

ApplicationLibCode/Application/RiaSummaryCurveDefinition.cpp
@@ -41,11 +41,11 @@ RiaSummaryCurveDefinition::RiaSummaryCurveDefinition()
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-RiaSummaryCurveDefinition::RiaSummaryCurveDefinition( RimSummaryCase*                 summaryCase,
-                                                      const RifEclipseSummaryAddress& summaryAddress,
+RiaSummaryCurveDefinition::RiaSummaryCurveDefinition( RimSummaryCase*                 summaryCaseY,
+                                                      const RifEclipseSummaryAddress& summaryAddressY,
                                                       bool                            isEnsembleCurve )
-    : m_summaryCaseY( summaryCase )
-    , m_summaryAddressY( summaryAddress )
+    : m_summaryCaseY( summaryCaseY )
+    , m_summaryAddressY( summaryAddressY )
     , m_summaryCaseX( nullptr )
     , m_summaryAddressX( RifEclipseSummaryAddress::timeAddress() )
     , m_ensemble( nullptr )
@@ -56,9 +56,9 @@ RiaSummaryCurveDefinition::RiaSummaryCurveDefinition( RimSummaryCase*           
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-RiaSummaryCurveDefinition::RiaSummaryCurveDefinition( RimSummaryCaseCollection* ensemble, const RifEclipseSummaryAddress& summaryAddress )
+RiaSummaryCurveDefinition::RiaSummaryCurveDefinition( RimSummaryCaseCollection* ensemble, const RifEclipseSummaryAddress& summaryAddressY )
     : m_summaryCaseY( nullptr )
-    , m_summaryAddressY( summaryAddress )
+    , m_summaryAddressY( summaryAddressY )
     , m_summaryCaseX( nullptr )
     , m_summaryAddressX( RifEclipseSummaryAddress::timeAddress() )
     , m_ensemble( ensemble )

ApplicationLibCode/Application/RiaSummaryCurveDefinition.h
@@ -38,8 +38,8 @@ class RiaSummaryCurveDefinition
 {
 public:
     RiaSummaryCurveDefinition();
-    explicit RiaSummaryCurveDefinition( RimSummaryCase* summaryCase, const RifEclipseSummaryAddress& summaryAddress, bool isEnsembleCurve );
-    explicit RiaSummaryCurveDefinition( RimSummaryCaseCollection* ensemble, const RifEclipseSummaryAddress& summaryAddress );
+    explicit RiaSummaryCurveDefinition( RimSummaryCase* summaryCaseY, const RifEclipseSummaryAddress& summaryAddressY, bool isEnsembleCurve );
+    explicit RiaSummaryCurveDefinition( RimSummaryCaseCollection* ensemble, const RifEclipseSummaryAddress& summaryAddressY );
 
     // Y Axis
     RimSummaryCase*           summaryCaseY() const;

ApplicationLibCode/FileInterface/CMakeLists_files.cmake
@@ -45,7 +45,6 @@ set(SOURCE_GROUP_HEADER_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RifSummaryCaseRestartSelector.h
     ${CMAKE_CURRENT_LIST_DIR}/RifCaseRealizationParametersReader.h
     ${CMAKE_CURRENT_LIST_DIR}/RifFileParseTools.h
-    ${CMAKE_CURRENT_LIST_DIR}/RifEnsembleStatisticsReader.h
     ${CMAKE_CURRENT_LIST_DIR}/RifReaderEnsembleStatisticsRft.h
     ${CMAKE_CURRENT_LIST_DIR}/RifDerivedEnsembleReader.h
     ${CMAKE_CURRENT_LIST_DIR}/RifActiveCellsReader.h
@@ -136,7 +135,6 @@ set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RifSummaryCaseRestartSelector.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RifCaseRealizationParametersReader.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RifFileParseTools.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/RifEnsembleStatisticsReader.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RifDerivedEnsembleReader.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RifActiveCellsReader.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RifCsvDataTableFormatter.cpp

ApplicationLibCode/FileInterface/RifEnsembleStatisticsReader.cpp
@@ -1,116 +1,0 @@
-/////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (C) 2017- Statoil ASA
-//
-//  ResInsight is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
-//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//  FITNESS FOR A PARTICULAR PURPOSE.
-//
-//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
-//  for more details.
-//
-/////////////////////////////////////////////////////////////////////////////////
-
-#include "RifEnsembleStatisticsReader.h"
-
-#include "RimEnsembleCurveSet.h"
-#include "RimEnsembleStatisticsCase.h"
-#include "RimSummaryCaseCollection.h"
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-RifEnsembleStatisticsReader::RifEnsembleStatisticsReader( RimEnsembleStatisticsCase* ensStatCase )
-{
-    CVF_ASSERT( ensStatCase );
-
-    m_ensembleStatCase = ensStatCase;
-
-    m_allResultAddresses =
-        std::set<RifEclipseSummaryAddress>( { RifEclipseSummaryAddress::ensembleStatisticsAddress( ENSEMBLE_STAT_P10_QUANTITY_NAME, "" ),
-                                              RifEclipseSummaryAddress::ensembleStatisticsAddress( ENSEMBLE_STAT_P50_QUANTITY_NAME, "" ),
-                                              RifEclipseSummaryAddress::ensembleStatisticsAddress( ENSEMBLE_STAT_P90_QUANTITY_NAME, "" ),
-                                              RifEclipseSummaryAddress::ensembleStatisticsAddress( ENSEMBLE_STAT_MEAN_QUANTITY_NAME, "" ) } );
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-std::vector<time_t> RifEnsembleStatisticsReader::timeSteps( const RifEclipseSummaryAddress& resultAddress ) const
-{
-    if ( !validateAddress( resultAddress ) ) return {};
-    return m_ensembleStatCase->timeSteps();
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-bool RifEnsembleStatisticsReader::values( const RifEclipseSummaryAddress& resultAddress, std::vector<double>* values ) const
-{
-    if ( !validateAddress( resultAddress ) ) return false;
-
-    const std::vector<double>* sourceData   = nullptr;
-    auto                       quantityName = resultAddress.ensembleStatisticsVectorName();
-
-    if ( quantityName == ENSEMBLE_STAT_P10_QUANTITY_NAME )
-        sourceData = &m_ensembleStatCase->p10();
-    else if ( quantityName == ENSEMBLE_STAT_P50_QUANTITY_NAME )
-        sourceData = &m_ensembleStatCase->p50();
-    else if ( quantityName == ENSEMBLE_STAT_P90_QUANTITY_NAME )
-        sourceData = &m_ensembleStatCase->p90();
-    else if ( quantityName == ENSEMBLE_STAT_MEAN_QUANTITY_NAME )
-        sourceData = &m_ensembleStatCase->mean();
-
-    if ( !sourceData ) return false;
-
-    values->clear();
-    values->reserve( sourceData->size() );
-    for ( auto val : *sourceData )
-        values->push_back( val );
-    return true;
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-std::string RifEnsembleStatisticsReader::unitName( const RifEclipseSummaryAddress& resultAddress ) const
-{
-    std::string retval;
-
-    // The stat case does not have a unit set, so pick up the unit from one of the input cases, if possible
-    auto cases = m_ensembleStatCase->curveSet()->summaryCaseCollection()->allSummaryCases();
-    if ( cases.size() > 0 )
-    {
-        QString     qName    = QString::fromStdString( resultAddress.vectorName() );
-        std::string orgQName = qName.split( ":" )[1].toStdString();
-
-        RifEclipseSummaryAddress address = RifEclipseSummaryAddress( resultAddress );
-        address.setVectorName( orgQName );
-
-        retval = cases[0]->summaryReader()->unitName( address );
-    }
-
-    return retval;
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-RiaDefines::EclipseUnitSystem RifEnsembleStatisticsReader::unitSystem() const
-{
-    return m_ensembleStatCase->unitSystem();
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-bool RifEnsembleStatisticsReader::validateAddress( const RifEclipseSummaryAddress& address ) const
-{
-    return address.category() == RifEclipseSummaryAddressDefines::SummaryCategory::SUMMARY_ENSEMBLE_STATISTICS &&
-           !address.vectorName().empty();
-}

ApplicationLibCode/FileInterface/RifEnsembleStatisticsReader.h
@@ -1,45 +1,0 @@
-/////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (C) 2017- Statoil ASA
-//
-//  ResInsight is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
-//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//  FITNESS FOR A PARTICULAR PURPOSE.
-//
-//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
-//  for more details.
-//
-/////////////////////////////////////////////////////////////////////////////////
-
-#pragma once
-
-#include "RifSummaryReaderInterface.h"
-
-class RimEnsembleStatisticsCase;
-class RifEclipseSummaryAddress;
-
-//==================================================================================================
-///
-//==================================================================================================
-class RifEnsembleStatisticsReader : public RifSummaryReaderInterface
-{
-public:
-    RifEnsembleStatisticsReader( RimEnsembleStatisticsCase* ensStatCase );
-
-    std::vector<time_t> timeSteps( const RifEclipseSummaryAddress& resultAddress ) const override;
-    bool                values( const RifEclipseSummaryAddress& resultAddress, std::vector<double>* values ) const override;
-    std::string         unitName( const RifEclipseSummaryAddress& resultAddress ) const override;
-
-    RiaDefines::EclipseUnitSystem unitSystem() const override;
-
-private:
-    bool validateAddress( const RifEclipseSummaryAddress& address ) const;
-
-private:
-    RimEnsembleStatisticsCase* m_ensembleStatCase;
-};

ApplicationLibCode/ProjectDataModel/RimDataSourceSteppingTools.cpp
@@ -77,18 +77,16 @@ void RimDataSourceSteppingTools::modifyCurrentIndex( caf::PdmValueField*        
 bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&                                  oldValue,
                                                           const QVariant&                                  newValue,
                                                           RifEclipseSummaryAddressDefines::SummaryCategory category,
-                                                          RifEclipseSummaryAddress*                        adr )
+                                                          RifEclipseSummaryAddress&                        adr )
 {
-    if ( !adr ) return false;
-
     if ( category == RifEclipseSummaryAddressDefines::SummaryCategory::SUMMARY_REGION )
     {
         int oldInt = oldValue.toInt();
         int newInt = newValue.toInt();
 
-        if ( adr->regionNumber() == oldInt )
+        if ( adr.regionNumber() == oldInt )
         {
-            adr->setRegion( newInt );
+            adr.setRegion( newInt );
 
             return true;
         }
@@ -98,9 +96,9 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
         int oldInt = oldValue.toInt();
         int newInt = newValue.toInt();
 
-        if ( adr->aquiferNumber() == oldInt )
+        if ( adr.aquiferNumber() == oldInt )
         {
-            adr->setAquiferNumber( newInt );
+            adr.setAquiferNumber( newInt );
 
             return true;
         }
@@ -110,9 +108,9 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
         std::string oldString = oldValue.toString().toStdString();
         std::string newString = newValue.toString().toStdString();
 
-        if ( adr->groupName() == oldString )
+        if ( adr.groupName() == oldString )
         {
-            adr->setGroupName( newString );
+            adr.setGroupName( newString );
 
             return true;
         }
@@ -122,9 +120,9 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
         std::string oldString = oldValue.toString().toStdString();
         std::string newString = newValue.toString().toStdString();
 
-        if ( adr->networkName() == oldString )
+        if ( adr.networkName() == oldString )
         {
-            adr->setNetworkName( newString );
+            adr.setNetworkName( newString );
 
             return true;
         }
@@ -134,9 +132,9 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
         std::string oldString = oldValue.toString().toStdString();
         std::string newString = newValue.toString().toStdString();
 
-        if ( adr->wellName() == oldString )
+        if ( adr.wellName() == oldString )
         {
-            adr->setWellName( newString );
+            adr.setWellName( newString );
 
             return true;
         }
@@ -146,9 +144,9 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
     {
         std::string oldString = oldValue.toString().toStdString();
         std::string newString = newValue.toString().toStdString();
-        if ( adr->blockAsString() == oldString )
+        if ( adr.blockAsString() == oldString )
         {
-            adr->setCellIjk( newString );
+            adr.setCellIjk( newString );
 
             return true;
         }
@@ -157,11 +155,11 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
     {
         std::string oldString = oldValue.toString().toStdString();
         std::string newString = newValue.toString().toStdString();
-        if ( adr->formatUiTextRegionToRegion() == oldString )
+        if ( adr.formatUiTextRegionToRegion() == oldString )
         {
             auto [region1, region2] = RifEclipseSummaryAddress::regionToRegionPairFromUiText( newString );
-            adr->setRegion( region1 );
-            adr->setRegion2( region2 );
+            adr.setRegion( region1 );
+            adr.setRegion2( region2 );
 
             return true;
         }
@@ -170,9 +168,9 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
     {
         int oldInt = oldValue.toInt();
         int newInt = newValue.toInt();
-        if ( adr->wellSegmentNumber() == oldInt )
+        if ( adr.wellSegmentNumber() == oldInt )
         {
-            adr->setWellSegmentNumber( newInt );
+            adr.setWellSegmentNumber( newInt );
 
             return true;
         }
@@ -184,48 +182,14 @@ bool RimDataSourceSteppingTools::updateAddressIfMatching( const QVariant&       
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-bool RimDataSourceSteppingTools::updateHistoryAndSummaryQuantityIfMatching( const QVariant&           oldValue,
-                                                                            const QVariant&           newValue,
-                                                                            RifEclipseSummaryAddress* adr )
+bool RimDataSourceSteppingTools::updateQuantityIfMatching( const QVariant& oldValue, const QVariant& newValue, RifEclipseSummaryAddress& adr )
 {
-    if ( !adr ) return false;
-
     std::string oldString = oldValue.toString().toStdString();
     std::string newString = newValue.toString().toStdString();
 
-    if ( adr->vectorName() == oldString )
+    if ( adr.vectorName() == oldString )
     {
-        adr->setVectorName( newString );
-
-        return true;
-    }
-
-    std::string correspondingOldString = RiaSummaryAddressAnalyzer::correspondingHistorySummaryCurveName( oldString );
-    std::string correspondingNewString = RiaSummaryAddressAnalyzer::correspondingHistorySummaryCurveName( newString );
-
-    if ( adr->vectorName() == correspondingOldString )
-    {
-        adr->setVectorName( correspondingNewString );
-
-        return true;
-    }
-
-    return false;
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
-bool RimDataSourceSteppingTools::updateQuantityIfMatching( const QVariant& oldValue, const QVariant& newValue, RifEclipseSummaryAddress* adr )
-{
-    if ( !adr ) return false;
-
-    std::string oldString = oldValue.toString().toStdString();
-    std::string newString = newValue.toString().toStdString();
-
-    if ( adr->vectorName() == oldString )
-    {
-        adr->setVectorName( newString );
+        adr.setVectorName( newString );
 
         return true;
     }

ApplicationLibCode/ProjectDataModel/RimDataSourceSteppingTools.h
@@ -39,9 +39,7 @@ public:
     static bool updateAddressIfMatching( const QVariant&                                  oldValue,
                                          const QVariant&                                  newValue,
                                          RifEclipseSummaryAddressDefines::SummaryCategory category,
-                                         RifEclipseSummaryAddress*                        adr );
+                                         RifEclipseSummaryAddress&                        adr );
 
-    static bool updateHistoryAndSummaryQuantityIfMatching( const QVariant& oldValue, const QVariant& newValue, RifEclipseSummaryAddress* adr );
-
-    static bool updateQuantityIfMatching( const QVariant& oldValue, const QVariant& newValue, RifEclipseSummaryAddress* adr );
+    static bool updateQuantityIfMatching( const QVariant& oldValue, const QVariant& newValue, RifEclipseSummaryAddress& adr );
 };

commit 042358a0267df77bae586c7e1b39dd7d57e1da05

Fix regression introduced in fb288f602ded223c46bf666094748f3e654f4433

ApplicationLibCode/ProjectDataModel/Summary/RimSummaryPlotSourceStepping.cpp
@@ -362,17 +362,26 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
 
     bool triggerLoadDataAndUpdate = false;
 
-    auto updateEnsembleAddresses = [&doZoomAll, &oldValue, &newValue]( const std::vector<RimEnsembleCurveSet*>& curveSets )
+    auto updateEnsembleAddresses =
+        [&doZoomAll, &oldValue, &newValue]( const std::vector<RimEnsembleCurveSet*>& curveSets, SummaryCategory categoryToUpdate )
     {
         for ( auto curveSet : curveSets )
         {
             auto curveAdr = curveSet->curveAddress();
 
             auto yAddressToModify = curveAdr.summaryAddressY();
-            RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, yAddressToModify );
-
             auto xAddressToModify = curveAdr.summaryAddressX();
-            RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, xAddressToModify );
+
+            if ( categoryToUpdate != SummaryCategory::SUMMARY_INVALID )
+            {
+                RimDataSourceSteppingTools::updateAddressIfMatching( oldValue, newValue, categoryToUpdate, yAddressToModify );
+                RimDataSourceSteppingTools::updateAddressIfMatching( oldValue, newValue, categoryToUpdate, xAddressToModify );
+            }
+            else
+            {
+                RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, yAddressToModify );
+                RimDataSourceSteppingTools::updateQuantityIfMatching( oldValue, newValue, xAddressToModify );
+            }
 
             curveSet->setCurveAddress( RiaSummaryCurveAddress( xAddressToModify, yAddressToModify ) );
             curveSet->updateConnectedEditors();
@@ -451,7 +460,7 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
 
         if ( dataSourceSteppingObject() )
         {
-            updateEnsembleAddresses( dataSourceSteppingObject()->curveSets() );
+            updateEnsembleAddresses( dataSourceSteppingObject()->curveSets(), SummaryCategory::SUMMARY_INVALID );
         }
 
         m_vectorName.uiCapability()->updateConnectedEditors();
@@ -516,7 +525,7 @@ void RimSummaryPlotSourceStepping::fieldChangedByUi( const caf::PdmFieldHandle* 
 
             if ( dataSourceSteppingObject() )
             {
-                updateEnsembleAddresses( dataSourceSteppingObject()->curveSets() );
+                updateEnsembleAddresses( dataSourceSteppingObject()->curveSets(), summaryCategoryToModify );
             }
 
             triggerLoadDataAndUpdate = true;