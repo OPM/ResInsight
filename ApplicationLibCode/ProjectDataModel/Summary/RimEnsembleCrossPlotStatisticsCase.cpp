OPM/ResInsight

commit d17749184391b55885925b3914505b8944fd861d

#10648 Refactor summary reader: return std::vector<double> instead of taking ptr.

ApplicationLibCode/ProjectDataModel/Summary/RimEnsembleCrossPlotStatisticsCase.cpp
@@ -36,7 +36,7 @@
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-bool RimEnsembleCrossPlotStatisticsCase::values( const RifEclipseSummaryAddress& resultAddress, std::vector<double>* values ) const
+std::pair<bool, std::vector<double>> RimEnsembleCrossPlotStatisticsCase::values( const RifEclipseSummaryAddress& resultAddress ) const
 {
     if ( m_adrX.isValid() )
     {
@@ -44,23 +44,22 @@ bool RimEnsembleCrossPlotStatisticsCase::values( const RifEclipseSummaryAddress&
         auto it           = stringToTest.find( m_adrX.vectorName() );
         if ( it != std::string::npos )
         {
-            *values = m_binnedXValues;
-            return true;
+            return { true, m_binnedXValues };
         }
     }
 
     auto quantityName = resultAddress.ensembleStatisticsVectorName();
 
     if ( quantityName == RifEclipseSummaryAddressDefines::statisticsNameP10() )
-        *values = m_p10Data;
+        return { true, m_p10Data };
     else if ( quantityName == RifEclipseSummaryAddressDefines::statisticsNameP50() )
-        *values = m_p50Data;
+        return { true, m_p50Data };
     else if ( quantityName == RifEclipseSummaryAddressDefines::statisticsNameP90() )
-        *values = m_p90Data;
+        return { true, m_p90Data };
     else if ( quantityName == RifEclipseSummaryAddressDefines::statisticsNameMean() )
-        *values = m_meanData;
+        return { true, m_meanData };
 
-    return true;
+    return { true, {} };
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -142,12 +141,10 @@ void RimEnsembleCrossPlotStatisticsCase::calculate( const std::vector<RimSummary
         {
             const std::vector<time_t>& timeSteps = reader->timeSteps( inputAddressX );
 
-            std::vector<double> valuesX;
-            reader->values( inputAddressX, &valuesX );
+            auto [isXOk, valuesX] = values( inputAddressX );
             if ( valuesX.empty() ) continue;
 
-            std::vector<double> valuesY;
-            reader->values( inputAddressY, &valuesY );
+            auto [isYOk, valuesY] = reader->values( inputAddressY );
             if ( valuesY.empty() ) continue;
 
             if ( !includeIncompleteCurves && timeSteps.size() != valuesX.size() ) continue;

ApplicationLibCode/Application/RiaSummaryCurveDefinition.cpp
@@ -204,15 +204,16 @@ void RiaSummaryCurveDefinition::setIdentifierText( SummaryCategory category, con
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-void RiaSummaryCurveDefinition::resultValues( const RiaSummaryCurveDefinition& curveDefinition, gsl::not_null<std::vector<double>*> values )
+std::vector<double> RiaSummaryCurveDefinition::resultValues( const RiaSummaryCurveDefinition& curveDefinition )
 {
-    if ( !curveDefinition.summaryAddressY().isValid() ) return;
-    if ( !curveDefinition.summaryCaseY() ) return;
+    if ( !curveDefinition.summaryAddressY().isValid() ) return {};
+    if ( !curveDefinition.summaryCaseY() ) return {};
 
     RifSummaryReaderInterface* reader = curveDefinition.summaryCaseY()->summaryReader();
-    if ( !reader ) return;
+    if ( !reader ) return {};
 
-    reader->values( curveDefinition.summaryAddressY(), values );
+    auto [isOk, values] = reader->values( curveDefinition.summaryAddressY() );
+    return values;
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationLibCode/Application/RiaSummaryCurveDefinition.h
@@ -66,7 +66,7 @@ public:
     bool operator<( const RiaSummaryCurveDefinition& other ) const;
 
     // TODO: Consider moving to a separate tools class
-    static void                resultValues( const RiaSummaryCurveDefinition& curveDefinition, gsl::not_null<std::vector<double>*> values );
+    static std::vector<double> resultValues( const RiaSummaryCurveDefinition& curveDefinition );
     static std::vector<time_t> timeSteps( const RiaSummaryCurveDefinition& curveDefinition );
 
     QString curveDefinitionText() const;

ApplicationLibCode/Commands/CompletionExportCommands/RicExportFractureCompletionsImpl.cpp
@@ -454,8 +454,8 @@ void RicExportFractureCompletionsImpl::getWellPressuresAndInitialProductionTimeS
 
             if ( summaryCase && summaryCase->summaryReader() )
             {
-                std::vector<double> values;
-                if ( summaryCase->summaryReader()->values( wbhpPressureAddress, &values ) )
+                auto [isOk, values] = summaryCase->summaryReader()->values( wbhpPressureAddress );
+                if ( isOk )
                 {
                     std::vector<time_t> summaryTimeSteps = summaryCase->summaryReader()->timeSteps( wbhpPressureAddress );
                     CVF_ASSERT( values.size() == summaryTimeSteps.size() );

ApplicationLibCode/FileInterface/RifColumnBasedUserData.cpp
@@ -72,28 +72,31 @@ bool RifColumnBasedUserData::parse( const QString& data, QString* errorText )
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-bool RifColumnBasedUserData::values( const RifEclipseSummaryAddress& resultAddress, std::vector<double>* values ) const
+std::pair<bool, std::vector<double>> RifColumnBasedUserData::values( const RifEclipseSummaryAddress& resultAddress ) const
 {
+    std::vector<double> values;
+
     auto search = m_mapFromAddressToResultIndex.find( resultAddress );
+
     if ( search != m_mapFromAddressToResultIndex.end() )
     {
         std::pair<size_t, size_t> tableColIndices = search->second;
 
         const Column* ci = m_parser->columnInfo( tableColIndices.first, tableColIndices.second );
-        if ( !ci ) return false;
+        if ( !ci ) return { false, {} };
 
         if ( !ci->values.empty() )
         {
-            values->reserve( ci->values.size() );
+            values.reserve( ci->values.size() );
 
             for ( const auto& v : ci->values )
             {
-                values->push_back( v );
+                values.push_back( v );
             }
         }
     }
 
-    return true;
+    return { true, values };
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationLibCode/FileInterface/RifColumnBasedUserData.h
@@ -44,7 +44,7 @@ public:
 
     std::vector<time_t> timeSteps( const RifEclipseSummaryAddress& resultAddress ) const override;
 
-    bool values( const RifEclipseSummaryAddress& resultAddress, std::vector<double>* values ) const override;
+    std::pair<bool, std::vector<double>> values( const RifEclipseSummaryAddress& resultAddress ) const override;
 
     std::string                   unitName( const RifEclipseSummaryAddress& resultAddress ) const override;
     RiaDefines::EclipseUnitSystem unitSystem() const override;

ApplicationLibCode/FileInterface/RifCsvSummaryReader.cpp
@@ -49,25 +49,26 @@ RifCsvSummaryReader::~RifCsvSummaryReader()
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-bool RifCsvSummaryReader::values( const RifEclipseSummaryAddress& resultAddress, std::vector<double>* values ) const
+std::pair<bool, std::vector<double>> RifCsvSummaryReader::values( const RifEclipseSummaryAddress& resultAddress ) const
 {
+    std::vector<double> values;
+
     auto search = m_mapFromAddressToResultIndex.find( resultAddress );
     if ( search != m_mapFromAddressToResultIndex.end() )
     {
         size_t columnIndex = search->second;
 
         const Column* ci = m_parser->columnInfo( columnIndex );
-        if ( !ci ) return false;
+        if ( !ci ) return { false, {} };
 
-        values->clear();
-        values->reserve( ci->values.size() );
+        values.reserve( ci->values.size() );
         for ( double val : ci->values )
         {
-            values->push_back( val );
+            values.push_back( val );
         }
     }
 
-    return true;
+    return { true, values };
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationLibCode/FileInterface/RifCsvSummaryReader.h
@@ -41,7 +41,7 @@ public:
 
     std::vector<time_t> timeSteps( const RifEclipseSummaryAddress& resultAddress ) const override;
 
-    bool values( const RifEclipseSummaryAddress& resultAddress, std::vector<double>* values ) const override;
+    std::pair<bool, std::vector<double>> values( const RifEclipseSummaryAddress& resultAddress ) const override;
 
     std::string unitName( const RifEclipseSummaryAddress& resultAddress ) const override;
 

commit 7051a9042b4a774d6ee3bfd37f9653c5ab0edd2c

Fix regression introduced in https://github.com/OPM/ResInsight/pull/10680

ApplicationLibCode/ProjectDataModel/Summary/RimEnsembleCrossPlotStatisticsCase.cpp
@@ -141,7 +141,7 @@ void RimEnsembleCrossPlotStatisticsCase::calculate( const std::vector<RimSummary
         {
             const std::vector<time_t>& timeSteps = reader->timeSteps( inputAddressX );
 
-            auto [isXOk, valuesX] = values( inputAddressX );
+            auto [isXOk, valuesX] = reader->values( inputAddressX );
             if ( valuesX.empty() ) continue;
 
             auto [isYOk, valuesY] = reader->values( inputAddressY );