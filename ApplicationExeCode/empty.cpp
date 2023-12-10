7c09e4982b4a792c322944b0b2e9a8d4546625f4
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-12-10 13:36:06 +0100

    Refactor into a single target with multiple cmake commands

--- ApplicationExeCode/empty.cpp
+++ ApplicationExeCode/empty.cpp
@@ -1,10 +1,10 @@
 //
-// This empty file is used by ResInsightFileCopyTargets
+// This empty file is used by ResInsightDummyTarget
 //
 // Creating a dummy target with an empty file is used to have a different target than ResInsight.
 // This target is used to find the full path to the build folder.
 //
-// $<TARGET_FILE_DIR:ResInsightFileCopyTargets>
+// $<TARGET_FILE_DIR:ResInsightDummyTarget>
 //
 // If we use the ResInsight target ($<TARGET_FILE_DIR:ResInsight>), we get a circular dependency.
 //
