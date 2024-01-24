99dc266d979b32e4e67c2e8631fcff5f4d8cbb74
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-01-24 08:16:17 +0100

    Disconnect comparison view in destructor of RimEclipseResultCase
    In debug build on Windows, a crash in the destructor of Rim3dView happens when the comparison view is set to null.

--- ApplicationLibCode/ProjectDataModel/RimEclipseResultCase.cpp
+++ ApplicationLibCode/ProjectDataModel/RimEclipseResultCase.cpp
@@ -498,6 +498,13 @@ cvf::ref<RifReaderInterface> RimEclipseResultCase::createMockModel( QString mode
 //--------------------------------------------------------------------------------------------------
 RimEclipseResultCase::~RimEclipseResultCase()
 {
+    // Disconnect all comparison views. In debug build on Windows, a crash occurs. The comparison view is also set to zero in the destructor
+    // of Rim3dView()
+    for ( auto v : reservoirViews )
+    {
+        if ( v ) v->setComparisonView( nullptr );
+    }
+
     reservoirViews.deleteChildren();
     m_flowDiagSolutions.deleteChildren();
 }
