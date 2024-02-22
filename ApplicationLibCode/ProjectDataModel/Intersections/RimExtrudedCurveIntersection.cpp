6aa77dcdb1fa6dee2fd93bd60b4ad15206127aee
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-02-22 08:39:44 +0100

    Add support for returning complete closed polygon

--- ApplicationLibCode/ProjectDataModel/Intersections/RimExtrudedCurveIntersection.cpp
+++ ApplicationLibCode/ProjectDataModel/Intersections/RimExtrudedCurveIntersection.cpp
@@ -828,7 +828,7 @@ std::vector<std::vector<cvf::Vec3d>> RimExtrudedCurveIntersection::polyLines( cv
     {
         if ( m_projectPolygon )
         {
-            lines = m_projectPolygon->polyLinesData()->polyLines();
+            lines = m_projectPolygon->polyLinesData()->completePolyLines();
         }
     }
     else if ( type() == CrossSectionEnum::CS_AZIMUTHLINE )
