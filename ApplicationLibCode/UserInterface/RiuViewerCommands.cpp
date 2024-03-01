99bf43858a609933a043140eeb081adfc80a6cdb
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-03-01 12:26:05 +0100

    Use Polygon as text in 3D context menu

--- ApplicationLibCode/UserInterface/RiuViewerCommands.cpp
+++ ApplicationLibCode/UserInterface/RiuViewerCommands.cpp
@@ -393,6 +393,7 @@ void RiuViewerCommands::displayContextMenu( QMouseEvent* event )
                 }
 
                 menuBuilder << "RicNewPolygonFilter3dviewFeature";
+                menuBuilder.addCmdFeature( "RicNewPolygonFeature", "Polygon" );
 
                 menuBuilder << "RicEclipsePropertyFilterNewInViewFeature";
                 menuBuilder << "RicGeoMechPropertyFilterNewInViewFeature";
