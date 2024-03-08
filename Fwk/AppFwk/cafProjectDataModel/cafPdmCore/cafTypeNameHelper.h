3e734737aeae0f01ecc02d42c4b96b5a82333680
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-03-08 15:13:12 +0100

    Add type name helper

---
+++ Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafTypeNameHelper.h
@@ -0,0 +1,67 @@
+//##################################################################################################
+//
+//   Custom Visualization Core library
+//   Copyright (C) Ceetron Solutions AS
+//
+//   This library may be used under the terms of either the GNU General Public License or
+//   the GNU Lesser General Public License as follows:
+//
+//   GNU General Public License Usage
+//   This library is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU General Public License at <<http://www.gnu.org/licenses/gpl.html>>
+//   for more details.
+//
+//   GNU Lesser General Public License Usage
+//   This library is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation; either version 2.1 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU Lesser General Public License at <<http://www.gnu.org/licenses/lgpl-2.1.html>>
+//   for more details.
+//
+//##################################################################################################
+
+#pragma once
+
+#include <typeinfo>
+
+namespace caf
+{
+template <typename TypeName>
+std::string cafTypeName()
+{
+    return typeid( TypeName ).name();
+}
+
+template <typename TypeName>
+std::string cafTypeId()
+{
+    return typeid( TypeName );
+}
+
+template <typename TypeName>
+int convertToInteger( TypeName enumValue )
+{
+    return static_cast<std::underlying_type<TypeName>::type>( enumValue );
+}
+
+template <typename TypeName>
+TypeName convertToEnum( int enumIntegerValue )
+{
+    return static_cast<TypeName>( enumIntegerValue );
+}
+
+} //namespace caf
