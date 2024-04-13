c73be893d8fd184cc7cd2c2d7739fd706fc13af1
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-04-13 16:47:27 +0200

    Early return if data already is available
    If not checking if data is available, data is read from file and appended to result vector.

--- ApplicationLibCode/ReservoirDataModel/RigCaseCellResultsData.cpp
+++ ApplicationLibCode/ReservoirDataModel/RigCaseCellResultsData.cpp
@@ -1587,6 +1587,13 @@ size_t RigCaseCellResultsData::findOrLoadKnownScalarResultByResultTypeOrder( con
 //--------------------------------------------------------------------------------------------------
 size_t RigCaseCellResultsData::findOrLoadKnownScalarResultForTimeStep( const RigEclipseResultAddress& resVarAddr, size_t timeStepIndex )
 {
+    size_t scalarResultIndex = findScalarResultIndexFromAddress( resVarAddr );
+    if ( scalarResultIndex == cvf::UNDEFINED_SIZE_T )
+    {
+        return cvf::UNDEFINED_SIZE_T;
+    }
+    if ( isDataPresent( scalarResultIndex ) ) return scalarResultIndex;
+
     RiaDefines::ResultCatType type       = resVarAddr.resultCatType();
     QString                   resultName = resVarAddr.resultName();
 
@@ -1615,7 +1622,6 @@ size_t RigCaseCellResultsData::findOrLoadKnownScalarResultForTimeStep( const Rig
         return completionTypeScalarResultIndex;
     }
 
-    size_t scalarResultIndex = findScalarResultIndexFromAddress( resVarAddr );
     if ( scalarResultIndex == cvf::UNDEFINED_SIZE_T ) return cvf::UNDEFINED_SIZE_T;
 
     if ( type == RiaDefines::ResultCatType::GENERATED )
