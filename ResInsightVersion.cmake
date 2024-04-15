16f3518b5de5240bd3c5fbb6224ab26eaa0d7d5e
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-04-15 10:30:39 +0200

    Bump version to 2024.03.1-RC_01

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,11 +1,11 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2024)
 set(RESINSIGHT_MINOR_VERSION 03)
-set(RESINSIGHT_PATCH_VERSION 0)
+set(RESINSIGHT_PATCH_VERSION 1)
 
 # Opional text with no restrictions
 #set(RESINSIGHT_VERSION_TEXT "-dev")
-#set(RESINSIGHT_VERSION_TEXT "-RC_05")
+set(RESINSIGHT_VERSION_TEXT "-RC_01")
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
