f777dfa4ba80d478c11cf91af1fd457132afa63b
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2024-08-21 08:37:33 +0200

    Sumo: Increase request timeout.

--- ApplicationLibCode/Application/Tools/Cloud/RiaSumoDefines.cpp
+++ ApplicationLibCode/Application/Tools/Cloud/RiaSumoDefines.cpp
@@ -34,5 +34,5 @@ QString RiaSumoDefines::tokenPath()
 //--------------------------------------------------------------------------------------------------
 int RiaSumoDefines::requestTimeoutMillis()
 {
-    return 1 * 1000;
+    return 10 * 1000;
 }
