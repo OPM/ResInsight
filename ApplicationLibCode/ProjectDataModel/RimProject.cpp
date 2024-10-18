f468e3f62a3f24b113338cc2774391be54b8fafa
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-10-18 14:25:04 +0200

    Add quick access data model objects

--- ApplicationLibCode/ProjectDataModel/RimProject.cpp
+++ ApplicationLibCode/ProjectDataModel/RimProject.cpp
@@ -37,6 +37,7 @@
 #include "Cloud/RimCloudDataSourceCollection.h"
 #include "PlotTemplates/RimPlotTemplateFolderItem.h"
 #include "Polygons/RimPolygonCollection.h"
+#include "QuickAccess/RimQuickAccessCollection.h"
 #include "RimAdvancedSnapshotExportDefinition.h"
 #include "RimAnalysisPlotCollection.h"
 #include "RimAnnotationCollection.h"
@@ -161,6 +162,7 @@ RimProject::RimProject()
     scriptCollection.xmlCapability()->disableIO();
 
     CAF_PDM_InitFieldNoDefault( &m_mainPlotCollection, "MainPlotCollection", "Plots" );
+    CAF_PDM_InitFieldNoDefault( &m_pinnedFieldCollection, "PinnedFieldCollection", "PinnedFieldCollection" );
 
     CAF_PDM_InitFieldNoDefault( &viewLinkerCollection, "LinkedViews", "Linked Views", ":/LinkView.svg" );
     viewLinkerCollection = new RimViewLinkerCollection;
@@ -221,7 +223,8 @@ RimProject::RimProject()
     scriptCollection->uiCapability()->setUiName( "Scripts" );
     scriptCollection->uiCapability()->setUiIconFromResourceString( ":/octave.png" );
 
-    m_mainPlotCollection = new RimMainPlotCollection();
+    m_mainPlotCollection    = new RimMainPlotCollection();
+    m_pinnedFieldCollection = new RimQuickAccessCollection();
 
     CAF_PDM_InitFieldNoDefault( &m_plotTemplateTopFolder, "PlotTemplateCollection", "Plot Templates" );
     m_plotTemplateTopFolder = new RimPlotTemplateFolderItem();
@@ -350,6 +353,14 @@ void RimProject::updatesAfterProjectFileIsRead()
 
     if ( m_subWindowsTiled3DWindow_OBSOLETE ) m_subWindowsTileMode3DWindow = RiaDefines::WindowTileMode::DEFAULT;
     if ( m_subWindowsTiledPlotWindow_OBSOLETE ) m_subWindowsTileModePlotWindow = RiaDefines::WindowTileMode::DEFAULT;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimQuickAccessCollection* RimProject::pinnedFieldCollection() const
+{
+    return m_pinnedFieldCollection();
 }
 
 //--------------------------------------------------------------------------------------------------
