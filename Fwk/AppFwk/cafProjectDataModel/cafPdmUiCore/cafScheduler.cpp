c5ef7c595c9bde7070f8cd32f9ddf86ca27de213
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-01-04 09:12:13 +0100

    Add scheduler to AppFwk
    Add scheduler to AppFwk
    Make it possible to do a delayed call to updateConnectedEditors

--- Fwk/AppFwk/cafProjectDataModel/cafPdmUiCore/cafScheduler.cpp
+++ Fwk/AppFwk/cafProjectDataModel/cafPdmUiCore/cafScheduler.cpp
@@ -16,16 +16,15 @@
 //
 /////////////////////////////////////////////////////////////////////////////////
 
-#include "RiaScheduler.h"
+#include "cafScheduler.h"
 
-#include "cafProgressState.h"
-
-#include <QCoreApplication>
+namespace caf
+{
 
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-RiaScheduler::RiaScheduler()
+Scheduler::Scheduler()
     : m_blockUpdate( false )
 {
 }
@@ -33,14 +32,14 @@ RiaScheduler::RiaScheduler()
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-RiaScheduler::~RiaScheduler()
+Scheduler::~Scheduler()
 {
 }
 
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-void RiaScheduler::blockUpdate( bool blockUpdate )
+void Scheduler::blockUpdate( bool blockUpdate )
 {
     m_blockUpdate = blockUpdate;
     if ( !m_blockUpdate )
@@ -52,9 +51,9 @@ void RiaScheduler::blockUpdate( bool blockUpdate )
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-void RiaScheduler::slotUpdateScheduledItemsWhenReady()
+void Scheduler::slotUpdateScheduledItemsWhenReady()
 {
-    if ( caf::ProgressState::isActive() )
+    if ( SchedulerCallable::instance()->isScheduledTaskBlocked() )
     {
         startTimer( 100 );
         return;
@@ -66,7 +65,7 @@ void RiaScheduler::slotUpdateScheduledItemsWhenReady()
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-void RiaScheduler::startTimer( int msecs )
+void Scheduler::startTimer( int msecs )
 {
     if ( !m_updateTimer )
     {
@@ -80,3 +79,4 @@ void RiaScheduler::startTimer( int msecs )
         m_updateTimer->start( msecs );
     }
 }
+} // namespace caf
