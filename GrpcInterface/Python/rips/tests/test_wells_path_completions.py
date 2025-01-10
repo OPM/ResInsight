d172b7586a96b4017df390679161e5feb5d9fff4
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-01-10 13:13:53 +0100

    Python: Add more test for completion settings

--- GrpcInterface/Python/rips/tests/test_wells_path_completions.py
+++ GrpcInterface/Python/rips/tests/test_wells_path_completions.py
@@ -34,19 +34,35 @@ def test_add_well_path_completions(rips_instance, initialize_test):
 
     # Update the completion settings
     completions_settings = well_path.completion_settings()
-    completions_settings.msw_roughness = 12.34
-    completions_settings.msw_liner_diameter = 0.2123
-    completions_settings.well_name_for_export = "file name"
-    completions_settings.group_name_for_export = "msj"
-    completions_settings.well_type_for_export = "GAS"
+    completions_settings.allow_well_cross_flow = True
+    completions_settings.auto_well_shut_in = "STOP"
+    completions_settings.drainage_radius_for_pi = 1.56
+    completions_settings.fluid_in_place_region = 99
+    completions_settings.gas_inflow_eq = "R-G"
+    completions_settings.group_name_for_export = "TestGroup"
+    completions_settings.hydrostatic_density = "AVG"
+    completions_settings.msw_liner_diameter = 0.12
+    completions_settings.msw_roughness = 4.66
+    completions_settings.reference_depth_for_export = 1234
+    completions_settings.well_bore_fluid_pvt_table = 33
+    completions_settings.well_name_for_export = "TestWellName"
+    completions_settings.well_type_for_export = "LIQUID"
     completions_settings.update()  # Commit updates back to ResInsight
 
     completions_settings_updated = well_path.completion_settings()
-    assert completions_settings_updated.msw_roughness == 12.34
-    assert completions_settings_updated.msw_liner_diameter == 0.2123
-    assert completions_settings_updated.well_name_for_export == "file name"
-    assert completions_settings_updated.group_name_for_export == "msj"
-    assert completions_settings_updated.well_type_for_export == "GAS"
+    assert completions_settings_updated.allow_well_cross_flow == True
+    assert completions_settings_updated.auto_well_shut_in == "STOP"
+    assert completions_settings_updated.drainage_radius_for_pi == "1.56"
+    assert completions_settings_updated.fluid_in_place_region == 99
+    assert completions_settings_updated.gas_inflow_eq == "R-G"
+    assert completions_settings_updated.group_name_for_export == "TestGroup"
+    assert completions_settings_updated.hydrostatic_density == "AVG"
+    assert completions_settings_updated.msw_liner_diameter == 0.12
+    assert completions_settings_updated.msw_roughness == 4.66
+    assert completions_settings_updated.reference_depth_for_export == "1234"
+    assert completions_settings_updated.well_bore_fluid_pvt_table == 33
+    assert completions_settings_updated.well_name_for_export == "TestWellName"
+    assert completions_settings_updated.well_type_for_export == "LIQUID"
 
     msw_settings = well_path.msw_settings()
     msw_settings.custom_values_for_lateral = True
