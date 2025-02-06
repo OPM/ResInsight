87842a92eac623c3054dcd7072b201f6dd59f8f4
magnesj <1793152+magnesj@users.noreply.github.com>
2025-02-06 14:04:33 +0000

    Fixes by cmake-format

--- ApplicationLibCode/ProjectDataModel/Tools/CMakeLists_files.cmake
+++ ApplicationLibCode/ProjectDataModel/Tools/CMakeLists_files.cmake
@@ -1,11 +1,13 @@
-set(SOURCE_GROUP_HEADER_FILES ${CMAKE_CURRENT_LIST_DIR}/RiaVariableMapper.h
-                              ${CMAKE_CURRENT_LIST_DIR}/RimPlotAxisTools.h
-                              ${CMAKE_CURRENT_LIST_DIR}/RimAutomationSettings.h
+set(SOURCE_GROUP_HEADER_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RiaVariableMapper.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimPlotAxisTools.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimAutomationSettings.h
 )
 
-set(SOURCE_GROUP_SOURCE_FILES ${CMAKE_CURRENT_LIST_DIR}/RiaVariableMapper.cpp
-                              ${CMAKE_CURRENT_LIST_DIR}/RimPlotAxisTools.cpp
-                              ${CMAKE_CURRENT_LIST_DIR}/RimAutomationSettings.cpp
+set(SOURCE_GROUP_SOURCE_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RiaVariableMapper.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimPlotAxisTools.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimAutomationSettings.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
