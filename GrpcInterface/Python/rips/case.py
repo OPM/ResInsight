207dd29871046c370fdc96b7db4719bd9751c902
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-02-15 11:47:08 +0100

    Remove debug output in the views function

--- GrpcInterface/Python/rips/case.py
+++ GrpcInterface/Python/rips/case.py
@@ -344,7 +344,6 @@ def views(self):
     views = project.views()
     views_for_case = []
     for view_object in views:
-        view_object.print_object_info()
         if view_object.id == self.id:
             views_for_case.append(view_object)
     return views_for_case
