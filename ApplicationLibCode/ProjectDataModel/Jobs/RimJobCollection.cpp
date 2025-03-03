d23ab5a7a3afe0f6e1962b5d7edbed628ca44eb5
jonjenssen <jon@soundsoft.no>
2025-03-03 01:04:16 +0100

    Work in progress

---
+++ ApplicationLibCode/ProjectDataModel/Jobs/RimJobCollection.cpp
@@ -0,0 +1,90 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2025     Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RimJobCollection.h"
+
+#include "RiaLogging.h"
+
+#include "Rim3dView.h"
+#include "RimGenericJob.h"
+#include "RimGridView.h"
+#include "RimProject.h"
+
+#include "cafCmdFeatureMenuBuilder.h"
+
+CAF_PDM_SOURCE_INIT( RimJobCollection, "JobCollection" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimJobCollection::RimJobCollection()
+{
+    CAF_PDM_InitObject( "Jobs", ":/gear.svg" );
+
+    CAF_PDM_InitFieldNoDefault( &m_jobs, "Jobs", "Jobs" );
+
+    setCustomContextMenuEnabled( true );
+    setDeletable( false );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimJobCollection::~RimJobCollection()
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+std::vector<RimGenericJob*> RimJobCollection::jobs() const
+{
+    return m_jobs.childrenByType();
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+bool RimJobCollection::isEmpty()
+{
+    return !m_jobs.hasChildren();
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimJobCollection::addNewJob( RimGenericJob* newJob )
+{
+    m_jobs.push_back( newJob );
+    updateAllRequiredEditors();
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimJobCollection::onChildDeleted( caf::PdmChildArrayFieldHandle* childArray, std::vector<caf::PdmObjectHandle*>& referringObjects )
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimJobCollection::appendMenuItems( caf::CmdFeatureMenuBuilder& menuBuilder ) const
+{
+    menuBuilder << "RicNewOpmFlowJobFeature";
+}
---
+++ ApplicationLibCode/ProjectDataModel/Jobs/RimOpmFlowJob.cpp
@@ -0,0 +1,88 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2025 Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RimOpmFlowJob.h"
+#include "RimEclipseCase.h"
+#include "RimTools.h"
+
+#include "cafPdmUiFilePathEditor.h"
+
+CAF_PDM_SOURCE_INIT( RimOpmFlowJob, "OpmFlowJob" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimOpmFlowJob::RimOpmFlowJob()
+{
+    CAF_PDM_InitObject( "Opm Flow Job", ":/gear.svg" );
+
+    CAF_PDM_InitFieldNoDefault( &m_eclipseCase, "EclipseCase", "Eclipse Case" );
+    CAF_PDM_InitFieldNoDefault( &m_workDir, "WorkDirectory", "Working Folder" );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimOpmFlowJob::~RimOpmFlowJob()
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+QList<caf::PdmOptionItemInfo> RimOpmFlowJob::calculateValueOptions( const caf::PdmFieldHandle* fieldNeedingOptions )
+{
+    QList<caf::PdmOptionItemInfo> options;
+
+    if ( fieldNeedingOptions == &m_eclipseCase )
+    {
+        RimTools::eclipseCaseOptionItems( &options );
+        if ( options.isEmpty() )
+        {
+            options.push_back( caf::PdmOptionItemInfo( "None", nullptr ) );
+        }
+    }
+
+    return options;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimOpmFlowJob::defineEditorAttribute( const caf::PdmFieldHandle* field, QString uiConfigName, caf::PdmUiEditorAttribute* attribute )
+{
+    if ( field == &m_workDir )
+    {
+        if ( auto myAttr = dynamic_cast<caf::PdmUiFilePathEditorAttribute*>( attribute ) )
+        {
+            myAttr->m_selectDirectory = true;
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimOpmFlowJob::defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering )
+{
+    uiOrdering.add( nameField() );
+    uiOrdering.add( &m_eclipseCase );
+    uiOrdering.add( &m_workDir );
+
+    uiOrdering.skipRemainingFields();
+}
