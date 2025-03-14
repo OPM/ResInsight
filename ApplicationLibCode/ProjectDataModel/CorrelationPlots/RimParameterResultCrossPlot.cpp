4581816c04a8f6506b8eb1b90b0a49f680776712
magnesj <1793152+magnesj@users.noreply.github.com>
2025-03-14 10:51:21 +0000

    Fixes by clang-format

--- ApplicationLibCode/ProjectDataModel/CorrelationPlots/RimParameterResultCrossPlot.cpp
+++ ApplicationLibCode/ProjectDataModel/CorrelationPlots/RimParameterResultCrossPlot.cpp
@@ -43,8 +43,8 @@
 #include "qwt_picker_machine.h"
 #include "qwt_plot.h"
 #include "qwt_plot_curve.h"
-#include "qwt_plot_picker.h"
 #include "qwt_plot_marker.h"
+#include "qwt_plot_picker.h"
 #include "qwt_text.h"
 
 #include <QColor>
