549c07b22d5355ab864ff587366abd0c8a8198ee
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-03-17 08:29:36 +0100

    Bump to 2025.03.0-rc_1

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,17 +1,17 @@
 
-set(RESINSIGHT_MAJOR_VERSION 2024)
-set(RESINSIGHT_MINOR_VERSION 12)
-set(RESINSIGHT_PATCH_VERSION 3)
+set(RESINSIGHT_MAJOR_VERSION 2025)
+set(RESINSIGHT_MINOR_VERSION 03)
+set(RESINSIGHT_PATCH_VERSION 0)
 
 # Opional text with no restrictions
-set(RESINSIGHT_VERSION_TEXT "-dev")
-#set(RESINSIGHT_VERSION_TEXT "-RC_2")
+#set(RESINSIGHT_VERSION_TEXT "-dev")
+set(RESINSIGHT_VERSION_TEXT "-RC_1")
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
 # The uniqueness of this text is independent of RESINSIGHT_VERSION_TEXT 
 # Format of text must be ".xx"
-set(RESINSIGHT_DEV_VERSION ".05")
+#set(RESINSIGHT_DEV_VERSION ".05")
 
 # https://github.com/CRAVA/crava/tree/master/libs/nrlib
 set(NRLIB_GITHUB_SHA "ba35d4359882f1c6f5e9dc30eb95fe52af50fd6f") 
