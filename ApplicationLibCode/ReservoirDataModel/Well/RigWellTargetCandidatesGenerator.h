3c54b0582228a120ff145304bd79d6bb9afb49ae
jonjenssen <jon@soundsoft.no>
2025-03-18 00:29:12 +0100

    Add support for NNC when growing clusters

--- ApplicationLibCode/ReservoirDataModel/Well/RigWellTargetCandidatesGenerator.h
+++ ApplicationLibCode/ReservoirDataModel/Well/RigWellTargetCandidatesGenerator.h
@@ -25,11 +25,14 @@
 
 #include "RigEclipseResultAddress.h"
 
+#include <list>
 #include <map>
 #include <optional>
+#include <utility>
 
 class RigActiveCellInfo;
 class RigCaseCellResultsData;
+class RigMainGrid;
 class RimEclipseCase;
 class RimEclipseCaseEnsemble;
 class RimRegularGridCase;
@@ -41,6 +44,8 @@ class RimRegularGridCase;
 class RigWellTargetCandidatesGenerator
 {
 public:
+    using CellFaceType = cvf::StructGridInterface::FaceType;
+
     enum class VolumeType
     {
         OIL,
@@ -70,6 +75,20 @@ public:
         int                     maxClusters;
         int                     maxIterations;
         RigEclipseResultAddress filterAddress;
+    };
+
+    struct DataContainer
+    {
+        std::vector<double>        volume;
+        std::vector<double>        pressure;
+        std::vector<double>        permeabilityX;
+        std::vector<double>        permeabilityY;
+        std::vector<double>        permeabilityZ;
+        std::vector<double>        permeabilityNNC;
+        std::vector<double>        transmissibilityX;
+        std::vector<double>        transmissibilityY;
+        std::vector<double>        transmissibilityZ;
+        const std::vector<double>* transmissibilityNNC;
     };
 
     static void generateCandidates( RimEclipseCase*         eclipseCase,
@@ -124,45 +143,24 @@ private:
     static std::optional<caf::VecIjk> findStartCell( RimEclipseCase*            eclipseCase,
                                                      size_t                     timeStepIdx,
                                                      const ClusteringLimits&    limits,
-                                                     const std::vector<double>& volume,
-                                                     const std::vector<double>& pressure,
-                                                     const std::vector<double>& permeabilityX,
-                                                     const std::vector<double>& permeabilityY,
-                                                     const std::vector<double>& permeabilityZ,
-                                                     const std::vector<double>& transmissibilityX,
-                                                     const std::vector<double>& transmissibilityY,
-                                                     const std::vector<double>& transmissibilityZ,
+                                                     const DataContainer&       data,
                                                      const std::vector<double>& filterVector,
                                                      const std::vector<int>&    clusters );
 
     static void growCluster( RimEclipseCase*            eclipseCase,
                              const caf::VecIjk&         startCell,
                              const ClusteringLimits&    limits,
-                             const std::vector<double>& volume,
-                             const std::vector<double>& pressure,
-                             const std::vector<double>& permeabilityX,
-                             const std::vector<double>& permeabilityY,
-                             const std::vector<double>& permeabilityZ,
-                             const std::vector<double>& transmissibilityX,
-                             const std::vector<double>& transmissibilityY,
-                             const std::vector<double>& transmissibilityZ,
+                             const DataContainer&       data,
                              const std::vector<double>& filterVector,
                              std::vector<int>&          clusters,
                              int                        clusterId,
                              size_t                     timeStepIdx,
                              int                        maxIterations );
 
-    static std::vector<size_t> findCandidates( const RimEclipseCase&      eclipseCase,
+    static std::vector<size_t> findCandidates( RimEclipseCase*            eclipseCase,
                                                const std::vector<size_t>& previousCells,
                                                const ClusteringLimits&    limits,
-                                               const std::vector<double>& volume,
-                                               const std::vector<double>& pressure,
-                                               const std::vector<double>& permeabilityX,
-                                               const std::vector<double>& permeabilityY,
-                                               const std::vector<double>& permeabilityZ,
-                                               const std::vector<double>& transmissibilityX,
-                                               const std::vector<double>& transmissibilityY,
-                                               const std::vector<double>& transmissibilityZ,
+                                               const DataContainer&       data,
                                                const std::vector<double>& filterVector,
                                                std::vector<int>&          clusters );
 
@@ -179,18 +177,18 @@ private:
 
     static void createResultVector( RimEclipseCase& eclipseCase, const QString& resultName, const std::vector<int>& clusterIds, double value );
 
-    static double getValueForFace( const std::vector<double>&         x,
-                                   const std::vector<double>&         y,
-                                   const std::vector<double>&         z,
-                                   cvf::StructGridInterface::FaceType face,
-                                   size_t                             resultIndex );
+    static double getValueForFace( const std::vector<double>& x,
+                                   const std::vector<double>& y,
+                                   const std::vector<double>& z,
+                                   CellFaceType               face,
+                                   size_t                     resultIndex );
 
-    static double getTransmissibilityValueForFace( const std::vector<double>&         x,
-                                                   const std::vector<double>&         y,
-                                                   const std::vector<double>&         z,
-                                                   cvf::StructGridInterface::FaceType face,
-                                                   size_t                             resultIndex,
-                                                   size_t                             neighborResultIndex );
+    static double getTransmissibilityValueForFace( const std::vector<double>& x,
+                                                   const std::vector<double>& y,
+                                                   const std::vector<double>& z,
+                                                   CellFaceType               face,
+                                                   size_t                     resultIndex,
+                                                   size_t                     neighborResultIndex );
 
     static std::vector<RigWellTargetCandidatesGenerator::ClusterStatistics> generateStatistics( RimEclipseCase*            eclipseCase,
                                                                                                 const std::vector<double>& pressure,
@@ -211,4 +209,6 @@ private:
                                                 std::vector<int>&                                    occupancy );
 
     static cvf::BoundingBox computeBoundingBoxForResult( RimEclipseCase& eclipseCase, const QString& resultName, size_t timeStepIndex );
+
+    static std::list<std::pair<std::pair<size_t, CellFaceType>, size_t>> nncConnectionCellAndResult( size_t cellIdx, RigMainGrid* mainGrid );
 };
