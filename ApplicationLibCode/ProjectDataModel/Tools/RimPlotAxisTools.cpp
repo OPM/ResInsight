OPM/ResInsight

commit 0604eea74b2430484d49b0ec8648e88cbaa584f8

#12157 Always show vector name when underscore is present

ApplicationLibCode/ProjectDataModel/Tools/RimPlotAxisTools.cpp
@@ -18,6 +18,8 @@
 
 #include "RimPlotAxisTools.h"
 
+#include "RifEclipseSummaryAddress.h"
+
 #include "RimPlotAxisLogRangeCalculator.h"
 #include "RimPlotAxisProperties.h"
 #include "RimPlotCurve.h"
@@ -223,6 +225,42 @@ QString scaleFactorText( const RimPlotAxisProperties* const axisProperties )
     }
 
     return {};
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+QString axisTextForAddress( RifEclipseSummaryAddress address )
+{
+    if ( address.isCalculated() )
+    {
+        auto shortName = shortCalculationName( address.uiText() );
+        return QString::fromStdString( shortName );
+    }
+
+    auto candidate = QString::fromStdString( address.uiText() );
+
+    // Strip text starting with _ to avoid duplicate axis names
+    auto index = candidate.indexOf( "_" );
+    if ( index != -1 ) candidate = candidate.left( index );
+
+    return candidate;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+std::string shortCalculationName( const std::string& calculationName )
+{
+    QString calculationShortName = QString::fromStdString( calculationName );
+
+    int indexOfFirstSpace = calculationShortName.indexOf( ' ' );
+    if ( indexOfFirstSpace > -1 && indexOfFirstSpace < calculationShortName.size() )
+    {
+        calculationShortName = calculationShortName.left( indexOfFirstSpace );
+    }
+
+    return calculationShortName.toStdString();
 }
 
 } // namespace RimPlotAxisTools

ApplicationLibCode/Application/Tools/Summary/RiaSummaryPlotTools.cpp
@@ -36,6 +36,7 @@
 #include "RimSummaryCase.h"
 #include "RimSummaryCurve.h"
 #include "RimSummaryCurveAppearanceCalculator.h"
+#include "RimSummaryCurveAutoName.h"
 #include "RimSummaryEnsemble.h"
 #include "RimSummaryMultiPlot.h"
 #include "RimSummaryMultiPlotCollection.h"
@@ -92,6 +93,16 @@ RimSummaryCurve* createCurve( RimSummaryCase* summaryCase, const RifEclipseSumma
 
     curve->setSummaryCaseY( summaryCase );
     curve->setSummaryAddressY( addr );
+
+    size_t pos = addr.vectorName().find( '_' );
+    if ( pos != std::string::npos )
+    {
+        // https://github.com/OPM/ResInsight/issues/12157
+
+        RimSummaryCurveAutoName settings;
+        settings.enableVectorName( true );
+        curve->applyCurveAutoNameSettings( settings );
+    }
 
     return curve;
 }
@@ -540,10 +551,7 @@ RimEnsembleCurveSet* addNewEnsembleCurve( RimSummaryPlot* summaryPlot, const Ria
 //--------------------------------------------------------------------------------------------------
 RimSummaryCurve* addNewSummaryCurve( RimSummaryPlot* summaryPlot, const RiaSummaryCurveAddress& curveAddress, RimSummaryCase* summaryCase )
 {
-    auto curve = new RimSummaryCurve();
-
-    curve->setSummaryCaseY( summaryCase );
-    curve->setSummaryAddressY( curveAddress.summaryAddressY() );
+    auto curve = createCurve( summaryCase, curveAddress.summaryAddressY() );
 
     curve->setSummaryCaseX( summaryCase );
     curve->setSummaryAddressX( curveAddress.summaryAddressX() );

ApplicationLibCode/Commands/FlowCommands/RicPlotProductionRateFeature.cpp
@@ -255,11 +255,8 @@ RimSummaryCurve* RicPlotProductionRateFeature::addSummaryCurve( RimSummaryPlot* 
         return nullptr;
     }
 
-    RimSummaryCurve* newCurve = new RimSummaryCurve();
+    auto newCurve = RiaSummaryPlotTools::createCurve( summaryCase, addr );
     plot->addCurveAndUpdate( newCurve );
-
-    newCurve->setSummaryCaseY( summaryCase );
-    newCurve->setSummaryAddressY( addr );
     newCurve->setColor( color );
     newCurve->setLeftOrRightAxisY( RiuPlotAxis( plotAxis ) );
     newCurve->loadDataAndUpdate( true );

ApplicationLibCode/Commands/SummaryPlotCommands/RicSummaryPlotFeatureImpl.cpp
@@ -87,7 +87,7 @@ RimSummaryCurve* RicSummaryPlotFeatureImpl::createHistoryCurve( const RifEclipse
     historyAddr.setVectorName( historyAddr.vectorName() + "H" );
     if ( summaryCasesToUse->summaryReader()->allResultAddresses().count( historyAddr ) )
     {
-        return createCurve( summaryCasesToUse, historyAddr );
+        return RiaSummaryPlotTools::createCurve( summaryCasesToUse, historyAddr );
     }
 
     return nullptr;
@@ -621,7 +621,7 @@ std::vector<RimSummaryPlot*> RicSummaryPlotFeatureImpl::createMultipleSummaryPlo
                 const std::set<RifEclipseSummaryAddress>& allAddrsInCase = sumCase->summaryReader()->allResultAddresses();
                 if ( allAddrsInCase.count( addr ) )
                 {
-                    auto* newCurve = createCurve( sumCase, addr );
+                    auto newCurve = RiaSummaryPlotTools::createCurve( sumCase, addr );
                     createdCurves.push_back( newCurve );
                 }
             }
@@ -685,18 +685,6 @@ std::set<RifEclipseSummaryAddress>
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
-RimSummaryCurve* RicSummaryPlotFeatureImpl::createCurve( RimSummaryCase* summaryCase, const RifEclipseSummaryAddress& address )
-{
-    auto curve = new RimSummaryCurve();
-    curve->setSummaryCaseY( summaryCase );
-    curve->setSummaryAddressY( address );
-
-    return curve;
-}
-
-//--------------------------------------------------------------------------------------------------
-///
-//--------------------------------------------------------------------------------------------------
 std::vector<RimSummaryCurve*> RicSummaryPlotFeatureImpl::addCurvesFromAddressFiltersToPlot( const QStringList& curveFilters,
                                                                                             RimSummaryPlot*    plot,
                                                                                             RimSummaryCase*    summaryCase,
@@ -746,8 +734,7 @@ std::vector<RimSummaryCurve*> RicSummaryPlotFeatureImpl::addCurvesFromAddressFil
 
     for ( const auto& addr : curveAddressesToUse )
     {
-        auto* newCurve = createCurve( summaryCase, addr );
-
+        auto newCurve = RiaSummaryPlotTools::createCurve( summaryCase, addr );
         createdCurves.push_back( newCurve );
         plot->addCurveNoUpdate( newCurve );
     }

ApplicationLibCode/Commands/SummaryPlotCommands/RicSummaryPlotFeatureImpl.h
@@ -83,8 +83,6 @@ private:
                                                 EnsembleColoringType            ensembleColoringStyle,
                                                 QString                         ensembleColoringParameter );
 
-    static RimSummaryCurve* createCurve( RimSummaryCase* summaryCase, const RifEclipseSummaryAddress& address );
-
     static RimSummaryCurve* createHistoryCurve( const RifEclipseSummaryAddress& addr, RimSummaryCase* summaryCasesToUse );
 
     static std::vector<RimSummaryCurve*> addCurvesFromAddressFiltersToPlot( const QStringList& curveFilters,

ApplicationLibCode/ProjectDataModel/Summary/RimEnsembleCurveSet.cpp
@@ -27,6 +27,7 @@
 #include "RiaTimeTTools.h"
 #include "Summary/RiaSummaryAddressAnalyzer.h"
 #include "Summary/RiaSummaryCurveDefinition.h"
+#include "Summary/RiaSummaryPlotTools.h"
 
 #include "RimSummaryCalculationCollection.h"
 #include "SummaryPlotCommands/RicSummaryPlotEditorUi.h"
@@ -2052,9 +2053,7 @@ void RimEnsembleCurveSet::updateEnsembleCurves( const std::vector<RimSummaryCase
 
             for ( auto& sumCase : sumCases )
             {
-                RimSummaryCurve* curve = new RimSummaryCurve();
-                curve->setSummaryCaseY( sumCase );
-                curve->setSummaryAddressY( addr->address() );
+                auto curve = RiaSummaryPlotTools::createCurve( sumCase, addr->address() );
                 curve->setResampling( m_resampling() );
 
                 int lineThickness = 1;
@@ -2228,7 +2227,7 @@ void RimEnsembleCurveSet::updateStatisticsCurves( const std::vector<RimSummaryCa
 
         for ( const auto& address : addresses )
         {
-            auto curve = new RimSummaryCurve();
+            auto curve = RiaSummaryPlotTools::createCurve( summaryCase, address.summaryAddressY() );
             curve->setParentPlotNoReplot( plot->plotWidget() );
             m_curves.push_back( curve );
             curve->setColor( m_statistics->color() );
@@ -2256,8 +2255,6 @@ void RimEnsembleCurveSet::updateStatisticsCurves( const std::vector<RimSummaryCa
                 curve->setSymbolSize( m_statisticsSymbolSize() );
             }
 
-            curve->setSummaryCaseY( summaryCase );
-            curve->setSummaryAddressY( address.summaryAddressY() );
             curve->setLeftOrRightAxisY( axisY() );
 
             if ( isXAxisSummaryVector() )

ApplicationLibCode/ProjectDataModel/Summary/RimSummaryCurveAutoName.cpp
@@ -189,6 +189,14 @@ void RimSummaryCurveAutoName::applySettings( const RimSummaryCurveAutoName& othe
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
+void RimSummaryCurveAutoName::enableVectorName( bool enable )
+{
+    m_vectorName = enable;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 void RimSummaryCurveAutoName::appendWellName( std::string&                    text,
                                               const RifEclipseSummaryAddress& summaryAddress,
                                               const RimSummaryNameHelper*     nameHelper ) const

ApplicationLibCode/ProjectDataModel/Summary/RimSummaryCurveAutoName.h
@@ -37,6 +37,7 @@ public:
                        const RimSummaryNameHelper*   plotNameHelper ) const;
 
     void applySettings( const RimSummaryCurveAutoName& other );
+    void enableVectorName( bool enable );
 
 private:
     QString curveNameY( const RifEclipseSummaryAddress& summaryAddress,

commit c1cef43c708dc8fe421aaea36ecf47d6ff79dc18

Use vector name as axis object text, avoid identifier text
This bug was introduced in 0604eea

ApplicationLibCode/ProjectDataModel/Tools/RimPlotAxisTools.cpp
@@ -238,7 +238,7 @@ QString axisTextForAddress( RifEclipseSummaryAddress address )
         return QString::fromStdString( shortName );
     }
 
-    auto candidate = QString::fromStdString( address.uiText() );
+    auto candidate = QString::fromStdString( address.vectorName() );
 
     // Strip text starting with _ to avoid duplicate axis names
     auto index = candidate.indexOf( "_" );