3e512b8220cf0d247dd58d6270f56eea71263407
magnesj <1793152+magnesj@users.noreply.github.com>
2025-04-11 07:32:51 +0000

    Fixes by clang-format

--- ApplicationLibCode/ProjectDataModel/RimGridStatisticsPlot.cpp
+++ ApplicationLibCode/ProjectDataModel/RimGridStatisticsPlot.cpp
@@ -244,7 +244,7 @@ bool RimGridStatisticsPlot::hasStatisticsData() const
 RigHistogramData RimGridStatisticsPlot::createStatisticsData() const
 {
     std::unique_ptr<RimHistogramCalculator> histogramCalculator;
-    histogramCalculator = std::make_unique<RimHistogramCalculator>( );
+    histogramCalculator = std::make_unique<RimHistogramCalculator>();
     histogramCalculator->setNumBins( static_cast<size_t>( m_numHistogramBins() ) );
 
     RigHistogramData histogramData;
