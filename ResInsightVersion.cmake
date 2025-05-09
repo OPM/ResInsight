158421297f8ebe1704e2c744d64870914828fe7c
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-05-09 11:53:00 +0200

    Updates after patch release
    * Merge main after release
    * Bump to version 2025.04.2-dev.01

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,11 +1,12 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2025)
 set(RESINSIGHT_MINOR_VERSION 04)
-set(RESINSIGHT_PATCH_VERSION 1)
+set(RESINSIGHT_PATCH_VERSION 2)
 
 # Opional text with no restrictions
 set(RESINSIGHT_VERSION_TEXT "-dev")
 # set(RESINSIGHT_VERSION_TEXT "-RC_4")
+
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
