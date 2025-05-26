3253d6a559630a5ccd9b92fce185d07237d1d45d
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-05-26 12:30:57 +0200

    Add import of .irap and .gri surface files

--- ApplicationLibCode/ProjectDataModel/Surfaces/CMakeLists_files.cmake
+++ ApplicationLibCode/ProjectDataModel/Surfaces/CMakeLists_files.cmake
@@ -11,6 +11,7 @@ set(SOURCE_GROUP_HEADER_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RimDepthSurface.h
     ${CMAKE_CURRENT_LIST_DIR}/RimFractureSurface.h
     ${CMAKE_CURRENT_LIST_DIR}/RimRegularSurface.h
+    ${CMAKE_CURRENT_LIST_DIR}/RimRegularFileSurface.h
 )
 
 set(SOURCE_GROUP_SOURCE_FILES
@@ -26,6 +27,7 @@ set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RimDepthSurface.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RimFractureSurface.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RimRegularSurface.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RimRegularFileSurface.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
