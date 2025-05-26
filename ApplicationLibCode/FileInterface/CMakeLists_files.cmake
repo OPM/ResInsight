bad5a0bbc423509ca388b1afb0c1433956d61d5a
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-05-26 12:29:29 +0200

    Add surfio as submodule and create importer with tests

--- ApplicationLibCode/FileInterface/CMakeLists_files.cmake
+++ ApplicationLibCode/FileInterface/CMakeLists_files.cmake
@@ -105,6 +105,7 @@ set(SOURCE_GROUP_HEADER_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RifArrowTools.h
     ${CMAKE_CURRENT_LIST_DIR}/RifReaderRegularGridModel.h
     ${CMAKE_CURRENT_LIST_DIR}/RifVtkSurfaceImporter.h
+    ${CMAKE_CURRENT_LIST_DIR}/RifSurfio.h
 )
 
 set(SOURCE_GROUP_SOURCE_FILES
@@ -207,6 +208,7 @@ set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RifArrowTools.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RifReaderRegularGridModel.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RifVtkSurfaceImporter.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RifSurfio.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
