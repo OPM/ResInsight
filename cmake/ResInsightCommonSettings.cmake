49a41f2f3a9ca5af277777a4db5ba29f62e042a6
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-06-16 19:49:14 +0200

    Move more into common settings

--- cmake/ResInsightCommonSettings.cmake
+++ cmake/ResInsightCommonSettings.cmake
@@ -1,7 +1,9 @@
 # Create interface library for common settings
 add_library(ResInsightCommonSettings INTERFACE)
 
-# Compiler warnings as errors
+option(RESINSIGHT_TREAT_WARNINGS_AS_ERRORS
+       "Treat warnings as errors (stops build)" OFF
+)
 if(RESINSIGHT_TREAT_WARNINGS_AS_ERRORS)
     if(MSVC)
         target_compile_options(ResInsightCommonSettings INTERFACE /WX)
@@ -9,29 +11,3 @@ if(RESINSIGHT_TREAT_WARNINGS_AS_ERRORS)
         target_compile_options(ResInsightCommonSettings INTERFACE -Werror)
     endif()
 endif()
-
-if(MSVC)
-  # The following warnings are supposed to be used in ResInsight, but
-  # temporarily disabled to avoid too much noise warning C4245: 'return':
-  # conversion from 'int' to 'size_t', signed/unsigned mismatch warning C4005:
-  # Macro redefinition for math constants (M_PI, M_SQRT2 etc)
-
-  # If possible, the following command is supposed to be the final target
-  # set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "/W3 /wd4190
-  # /wd4100 /wd4127")
-
-  set(BUILD_FLAGS_FOR_MSVC "/W3 /wd4190 /wd4100 /wd4127")
-
-  message(STATUS "CMAKE_CXX_COMPILER_VERSION ${CMAKE_CXX_COMPILER_VERSION}")
-
-  if(CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 19.38)
-    # https://github.com/OPM/ResInsight/issues/10844
-    set(BUILD_FLAGS_FOR_MSVC "${BUILD_FLAGS_FOR_MSVC} /wd4996")
-  endif()
-
-  message(STATUS "BUILD_FLAGS_FOR_MSVC ${BUILD_FLAGS_FOR_MSVC}")
-  set_target_properties(
-    ResInsightCommonSettings PROPERTIES COMPILE_FLAGS ${BUILD_FLAGS_FOR_MSVC}
-  )
-endif()
-
