a99c98fc04342ca84dc5d97b80ae682139250a81
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2025-08-18 18:13:37 +0000

    #12773 Python: Add API for creating valve templates
    - Add RimcValveTemplateCollection GRPC command with add_template method
    - Support ICD, ICV, and AICD valve types with custom parameters
    - Add setter methods for orifice diameter and flow coefficient
    - Include Python tests and example demonstrating usage
    - API: valve_templates.add_template(completion_type='AICD', orifice_diameter=7.3, flow_coefficient=0.2)

---
+++ ApplicationLibCode/ProjectDataModelCommands/RimcValveTemplateCollection.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2025- Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "RiaDefines.h"
+#include "RimValveTemplateCollection.h"
+
+#include "cafAppEnum.h"
+#include "cafPdmField.h"
+#include "cafPdmObjectHandle.h"
+#include "cafPdmObjectMethod.h"
+
+#include <QString>
+
+//==================================================================================================
+///
+//==================================================================================================
+class RimcValveTemplateCollection_add_template : public caf::PdmObjectCreationMethod
+{
+    CAF_PDM_HEADER_INIT;
+
+public:
+    RimcValveTemplateCollection_add_template( caf::PdmObjectHandle* self );
+
+    std::expected<caf::PdmObjectHandle*, QString> execute() override;
+    QString                                       classKeywordReturnedType() const override;
+
+private:
+    caf::PdmField<caf::AppEnum<RiaDefines::WellPathComponentType>> m_completionType;
+    caf::PdmField<double>                                          m_orificeDiameter;
+    caf::PdmField<double>                                          m_flowCoefficient;
+    caf::PdmField<QString>                                         m_userLabel;
+};
