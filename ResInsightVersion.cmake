0d017034d35b9ac00444a06ca831fce5e066bfdd
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-08-28 15:39:11 +0200

    Bump to 2025.09.0-RC_1

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,36 +1,17 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2025)
-set(RESINSIGHT_MINOR_VERSION 04)
-set(RESINSIGHT_PATCH_VERSION 4)
+set(RESINSIGHT_MINOR_VERSION 09)
+set(RESINSIGHT_PATCH_VERSION 0)
 
 # Opional text with no restrictions
-set(RESINSIGHT_VERSION_TEXT "-dev")
-# set(RESINSIGHT_VERSION_TEXT "-RC_4")
-
+#set(RESINSIGHT_VERSION_TEXT "-dev")
+set(RESINSIGHT_VERSION_TEXT "-RC_1")
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
 # The uniqueness of this text is independent of RESINSIGHT_VERSION_TEXT 
 # Format of text must be ".xx"
-set(RESINSIGHT_DEV_VERSION ".10")
-
-# https://github.com/CRAVA/crava/tree/master/libs/nrlib
-set(NRLIB_GITHUB_SHA "ba35d4359882f1c6f5e9dc30eb95fe52af50fd6f") 
-
-# https://github.com/Statoil/libecl
-# Note:
-# Apply patches fix-synthetic-odb-cases.patch and install-ert.patch after update
-set(ECL_GITHUB_SHA "0e1e780fd6f18ce93119061e36a4fca9711bc020")
-
-# https://github.com/OPM/opm-flowdiagnostics
-set(OPM_FLOWDIAGNOSTICS_SHA "8bb60d6111063f2b7557502ecaa329a2d5c13b41")
-
-# https://github.com/OPM/opm-flowdiagnostics-applications
-set(OPM_FLOWDIAGNOSTICS_APPLICATIONS_SHA "f57942a8cdf57422fabf3a4423d02a3e46e0be4e")
-
-# https://github.com/OPM/opm-parser/blob/master/opm/parser/eclipse/Units/Units.hpp
-# This file was moved from opm-core to opm-parser october 2016
-# sha for Units.hpp 9a679071dd0066236154852c39a9e0b3c3ac4873
+#set(RESINSIGHT_DEV_VERSION ".10")
 
 set(STRPRODUCTVER ${RESINSIGHT_MAJOR_VERSION}.${RESINSIGHT_MINOR_VERSION}.${RESINSIGHT_PATCH_VERSION}${RESINSIGHT_VERSION_TEXT}${RESINSIGHT_DEV_VERSION})
 
