8e81d8d6f2701fcbcea6ac51b07704d05f9d5780
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-09-01 15:28:08 +0200

    Bump to dev version after release

--- ResInsightVersion.cmake
+++ ResInsightVersion.cmake
@@ -1,17 +1,17 @@
 
 set(RESINSIGHT_MAJOR_VERSION 2025)
 set(RESINSIGHT_MINOR_VERSION 09)
-set(RESINSIGHT_PATCH_VERSION 0)
+set(RESINSIGHT_PATCH_VERSION 1)
 
 # Opional text with no restrictions
-#set(RESINSIGHT_VERSION_TEXT "-dev")
+set(RESINSIGHT_VERSION_TEXT "-dev")
 #set(RESINSIGHT_VERSION_TEXT "-RC_2")
 
 # Optional text
 # Must be unique and increasing within one combination of major/minor/patch version 
 # The uniqueness of this text is independent of RESINSIGHT_VERSION_TEXT 
 # Format of text must be ".xx"
-#set(RESINSIGHT_DEV_VERSION ".10")
+set(RESINSIGHT_DEV_VERSION ".01")
 
 set(STRPRODUCTVER ${RESINSIGHT_MAJOR_VERSION}.${RESINSIGHT_MINOR_VERSION}.${RESINSIGHT_PATCH_VERSION}${RESINSIGHT_VERSION_TEXT}${RESINSIGHT_DEV_VERSION})
 
