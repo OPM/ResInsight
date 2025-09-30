59be291773e061403fd77cf1517d8ac8e2a942e0
Jon Jenssen <jon@soundsoft.no>
2025-09-30 17:59:36 +0200

    Work in progress

---
+++ ApplicationLibCode/ProjectDataModel/Jobs/RimKeywordWconprod.h
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2025 Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "cafPdmField.h"
+#include "cafPdmObject.h"
+
+#include <QString>
+
+#include <optional>
+
+//==================================================================================================
+///
+///
+//==================================================================================================
+class RimKeywordWconprod : public caf::PdmObject
+{
+    CAF_PDM_HEADER_INIT;
+
+public:
+    RimKeywordWconprod();
+    ~RimKeywordWconprod() override;
+
+    void setWellName( const QString& name );
+
+    QString recordAsString() const;
+
+    // TODO: Return an actual OPM record
+    // Opm::DeckRecord toRecord() const;
+
+protected:
+    void                          defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering ) override;
+    QList<caf::PdmOptionItemInfo> calculateValueOptions( const caf::PdmFieldHandle* fieldNeedingOptions ) override;
+
+private:
+    caf::PdmField<QString>               m_wellName;
+    caf::PdmField<QString>               m_status;
+    caf::PdmField<QString>               m_target;
+    caf::PdmField<std::optional<double>> m_orat;
+    caf::PdmField<std::optional<double>> m_wrat;
+    caf::PdmField<std::optional<double>> m_grat;
+    caf::PdmField<std::optional<double>> m_lrat;
+    caf::PdmField<std::optional<double>> m_resv;
+    caf::PdmField<std::optional<double>> m_bhp;
+    caf::PdmField<std::optional<double>> m_thp;
+    caf::PdmField<std::optional<int>>    m_vfptab;
+    caf::PdmField<std::optional<double>> m_alqWell;
+};
