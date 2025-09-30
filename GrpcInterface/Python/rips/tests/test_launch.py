805cf7e8ba71b8a7db083c30eaf510e56cd1b542
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-09-30 13:10:18 +0200

    Adds test for launching non-existing instance
    Adds a test case to verify the behavior when attempting to launch a non-existent instance.
    
    This test ensures that the `launch` method returns `None` when provided with an invalid instance name, enhancing the robustness of the `rips` module.

--- GrpcInterface/Python/rips/tests/test_launch.py
+++ GrpcInterface/Python/rips/tests/test_launch.py
@@ -7,6 +7,11 @@ sys.path.insert(1, os.path.join(sys.path[0], "../../"))
 import rips
 
 
+def test_launch_non_existing(rips_instance, initialize_test):
+    instance=rips.Instance.launch("non_existing", console=True)
+    assert instance is None
+
+    
 def launch_resinsight(sec=1):
     resinsight_executable_from_env = os.environ.get("RESINSIGHT_EXECUTABLE")
     if resinsight_executable_from_env is None:
