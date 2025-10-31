beda624e93e890e4e719571d5532e171a66d1e12
magnesj <1793152+magnesj@users.noreply.github.com>
2025-10-31 06:37:39 +0000

    Fixes by cmake-format

--- ApplicationLibCode/ReservoirDataModel/CompletionsMsw/CMakeLists_files.cmake
+++ ApplicationLibCode/ReservoirDataModel/CompletionsMsw/CMakeLists_files.cmake
@@ -1,12 +1,14 @@
-set(SOURCE_GROUP_HEADER_FILES ${CMAKE_CURRENT_LIST_DIR}/RigMswTableRows.h
-                              ${CMAKE_CURRENT_LIST_DIR}/RigMswTableData.h
-                              ${CMAKE_CURRENT_LIST_DIR}/RigMswDataFormatter.h
-                              ${CMAKE_CURRENT_LIST_DIR}/RigMswUnifiedData.h 
+set(SOURCE_GROUP_HEADER_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RigMswTableRows.h
+    ${CMAKE_CURRENT_LIST_DIR}/RigMswTableData.h
+    ${CMAKE_CURRENT_LIST_DIR}/RigMswDataFormatter.h
+    ${CMAKE_CURRENT_LIST_DIR}/RigMswUnifiedData.h
 )
 
-set(SOURCE_GROUP_SOURCE_FILES ${CMAKE_CURRENT_LIST_DIR}/RigMswTableData.cpp
-                              ${CMAKE_CURRENT_LIST_DIR}/RigMswDataFormatter.cpp
-                              ${CMAKE_CURRENT_LIST_DIR}/RigMswUnifiedData.cpp
+set(SOURCE_GROUP_SOURCE_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/RigMswTableData.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RigMswDataFormatter.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RigMswUnifiedData.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
