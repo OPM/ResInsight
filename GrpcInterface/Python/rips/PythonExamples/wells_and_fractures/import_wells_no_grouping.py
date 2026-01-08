0a951df7624520da146d930e5ff77eb8fc2f3bb5
magnesj <1793152+magnesj@users.noreply.github.com>
2026-01-08 15:20:04 +0000

    Python code linting changes detected by black

--- GrpcInterface/Python/rips/PythonExamples/wells_and_fractures/import_wells_no_grouping.py
+++ GrpcInterface/Python/rips/PythonExamples/wells_and_fractures/import_wells_no_grouping.py
@@ -7,8 +7,12 @@ resInsight = rips.Instance.find()
 well_path_coll = resInsight.project.well_path_collection()
 well_path_coll.set_msw_name_grouping("")
 
-well1 = well_path_coll.import_well_path("f:/scratch/2026-well-path-import/Well-1_Y1.dev")
-well2 = well_path_coll.import_well_path("f:/scratch/2026-well-path-import/Well-1_Y2.dev")
+well1 = well_path_coll.import_well_path(
+    "f:/scratch/2026-well-path-import/Well-1_Y1.dev"
+)
+well2 = well_path_coll.import_well_path(
+    "f:/scratch/2026-well-path-import/Well-1_Y2.dev"
+)
 
 # Create lateral well path from geometry of another well path
 well1.append_lateral_from_geometry(well2)
