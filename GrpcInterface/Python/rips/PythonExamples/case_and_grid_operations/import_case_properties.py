30adabe9af8959f144fe283d9eff3f0506b744ed
jonjenssen <jon@soundsoft.no>
2026-01-16 18:19:33 +0100

    Update python script to work on Windows

--- GrpcInterface/Python/rips/PythonExamples/case_and_grid_operations/import_case_properties.py
+++ GrpcInterface/Python/rips/PythonExamples/case_and_grid_operations/import_case_properties.py
@@ -10,6 +10,7 @@
 
 # Access to environment variables and path tools
 import os
+import pathlib
 
 # Load ResInsight Processing Server Client Library
 import rips
@@ -21,13 +22,14 @@ resinsight = rips.Instance.find()
 resinsight_exe_path = os.environ.get("RESINSIGHT_EXECUTABLE")
 
 # Get the TestModels path from the executable path
-resinsight_install_path = os.path.dirname(resinsight_exe_path)
-test_models_path = os.path.join(resinsight_install_path, "TestModels")
+resinsight_install_path = pathlib.PurePath(os.path.dirname(resinsight_exe_path)).as_posix()
+test_models_path = resinsight_install_path + "/TestModels/"
 
 # Get the .roff case
 roff_case_path = os.path.join(
     test_models_path, "reek/reek_box_grid_w_out_props.roffasc"
 )
+
 roff_case = resinsight.project.load_case(roff_case_path)
 
 # PORO and EQLNUM should not be among available properties yet
@@ -41,6 +43,7 @@ poro_property_path = os.path.join(
 eqlnum_property_path = os.path.join(
     test_models_path, "reek/reek_box_EQLNUM_property.roffasc"
 )
+
 roff_case.import_properties(file_names=[poro_property_path, eqlnum_property_path])
 
 # PORO and EQLNUM should now be among available properties
