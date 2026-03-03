10234c898cefec94684848a16840c81b3cdfc0ab
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2026-03-03 08:53:19 +0100

    wip

--- ApplicationLibCode/ProjectDataModel/ContourMap/RimStatisticsContourMap.cpp
+++ ApplicationLibCode/ProjectDataModel/ContourMap/RimStatisticsContourMap.cpp
@@ -160,6 +160,7 @@ void RimStatisticsContourMap::defineUiOrdering( QString uiConfigName, caf::PdmUi
     {
         auto* btn = uiOrdering.addNewButton( "Compute", [this]() { onComputeStatisticsClicked(); } );
         btn->setUiToolTip( computeOK ? "Start statistics computations." : "Please check your time step and/or formation filter selections." );
+        btn->setUiReadOnly( !computeOK );
     }
 
     auto genGrp = uiOrdering.addNewGroup( "General" );
