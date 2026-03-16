3b00f17d2d949c7679731b2cfbc60af166b92cb6
jonjenssen <jon@soundsoft.no>
2026-03-16 22:57:17 +0100

    Add job wait/queued state

--- ApplicationLibCode/ProjectDataModel/Jobs/RimGenericJob.h
+++ ApplicationLibCode/ProjectDataModel/Jobs/RimGenericJob.h
@@ -38,6 +38,17 @@ class RimGenericJob : public RimNamedObject
     CAF_PDM_HEADER_INIT;
 
 public:
+
+    enum JobState
+    {
+        Idle,      // default, nothing is happening
+        Queued,    // waiting for available resources to run
+        Running,   // job is running
+        Completed, // job completed without errors
+        Failed     // job completed with errors
+    };
+
+
     RimGenericJob();
     ~RimGenericJob() override;
 
@@ -46,6 +57,8 @@ public:
 
     bool isRunning() const;
     bool stop();
+
+    JobState state() const;
 
     double            percentageDone() const;
     const QStringList jobLog() const;
@@ -72,7 +85,6 @@ protected:
     int    m_errorsDetected;
 
 private:
-    bool                        m_lastRunFailed;
-    bool                        m_isRunning;
+    JobState                    m_jobState;
     caf::PdmPointer<RimProcess> m_process;
 };
