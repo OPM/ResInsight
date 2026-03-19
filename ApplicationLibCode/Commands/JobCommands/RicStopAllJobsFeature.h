2e29c1560b10f246d951a087a6e312a3cf629479
Jon Jenssen <jon@soundsoft.no>
2026-03-19 18:00:56 +0100

    Clean up

--- ApplicationLibCode/Commands/JobCommands/RicStopAllJobsFeature.h
+++ ApplicationLibCode/Commands/JobCommands/RicStopAllJobsFeature.h
@@ -20,7 +20,6 @@
 
 #include "cafCmdFeature.h"
 
-class RimGenericJob;
 class RimJobCollection;
 
 //==================================================================================================
