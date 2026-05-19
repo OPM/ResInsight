dcc2b765f9adb7ff6bdd9888a2402ab753545aea
jonjenssen <jon@soundsoft.no>
2026-05-19 02:26:59 +0200

    Work in progress

--- ApplicationLibCode/UserInterface/RiuViewer.h
+++ ApplicationLibCode/UserInterface/RiuViewer.h
@@ -147,6 +147,7 @@ protected:
     void mouseMoveEvent( QMouseEvent* e ) override;
     void enterEvent( QEnterEvent* e ) override;
     void leaveEvent( QEvent* ) override;
+    void mousePressEvent( QMouseEvent* e ) override;
 
 private:
     void updateLegendLayout();
@@ -161,7 +162,6 @@ private:
     void paintOverlayItems( QPainter* painter ) override;
 
     void mouseReleaseEvent( QMouseEvent* event ) override;
-    void mousePressEvent( QMouseEvent* event ) override;
     void mouseDoubleClickEvent( QMouseEvent* event ) override;
 
 private:
