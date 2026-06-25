d44a4c137d22db112f0013663b78af710677df21
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2026-06-25 10:51:34 +0200

    Allow sub-tasks to display progress

--- ApplicationLibCode/Application/RiaGuiApplication.cpp
+++ ApplicationLibCode/Application/RiaGuiApplication.cpp
@@ -1802,9 +1802,6 @@ bool RiaGuiApplication::notify( QObject* receiver, QEvent* event )
         return QApplication::notify( receiver, event );
     }
 
-    // Prevent re-entrant event pumping from progress updates while dispatching events.
-    caf::ProgressInfoEventProcessingBlocker progressEventBlocker;
-
     bool done = false;
     try
     {
