59694ec020a49bfb9672e9683fa74dfdea01a9cb
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2026-07-28 11:15:41 +0200

    Trigger a debug break in CAF_ASSERT before aborting

--- Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafAssert.h
+++ Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafAssert.h
@@ -4,12 +4,23 @@
 #include <cstdio>
 #include <cstdlib>
 
+// Break into an attached debugger at the assert site. std::abort() alone does not trigger a debug
+// break with the release CRT, so asserts in RelWithDebInfo builds would terminate without stopping
+// in the debugger. Without a debugger attached, the breakpoint exception terminates the process
+// with the assert site at the top of the crash stack.
+#if defined( _MSC_VER )
+#define CAF_DEBUG_BREAK() __debugbreak()
+#else
+#define CAF_DEBUG_BREAK() __builtin_trap()
+#endif
+
 #define CAF_ASSERT( expr )                                                                   \
     do                                                                                       \
     {                                                                                        \
         if ( !( expr ) ) /* NOLINT */                                                        \
         {                                                                                    \
             std::printf( "%s : %i : CAF_ASSERT( %s ) failed\n", __FILE__, __LINE__, #expr ); \
+            CAF_DEBUG_BREAK();                                                               \
             std::abort();                                                                    \
         }                                                                                    \
     } while ( false )
