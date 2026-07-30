e57b596f4d6b4eb7178cf65c2792c3d7403c1675
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2026-07-30 08:29:20 +0200

    #14404 Document why EXTRA_FINE and EXTRA_COARSE have no alias

--- ApplicationLibCode/ProjectDataModel/ContourMap/RimContourMapResolutionTools.cpp
+++ ApplicationLibCode/ProjectDataModel/ContourMap/RimContourMapResolutionTools.cpp
@@ -26,7 +26,8 @@ template <>
 void caf::AppEnum<RimContourMapResolutionTools::SamplingResolution>::setUp()
 {
     // The alias is the serialization text used by previous versions, as read back from XML, and maps
-    // old project files to the correct enum value
+    // old project files to the correct enum value. EXTRA_FINE and EXTRA_COARSE have no alias, as their
+    // legacy texts contained whitespace and were never read back correctly from XML
     addItem( RimContourMapResolutionTools::SamplingResolution::EXTRA_FINE, "EXTRA_FINE", "Extra Fine" );
     addItem( RimContourMapResolutionTools::SamplingResolution::FINE, "FINE", "Fine", { "Fine" } );
     addItem( RimContourMapResolutionTools::SamplingResolution::NORMAL, "NORMAL", "Normal", { "Normal" } );
