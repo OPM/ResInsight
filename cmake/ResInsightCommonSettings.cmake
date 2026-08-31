0d61f5cb5a746ea9a4cda2b8d74e6ad690e7aa82
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2026-08-31 10:00:16 +0200

    Do not treat GCC free-nonheap-object warning as error
    GCC's -Wfree-nonheap-object false-positives on inlined std::vector copies
    (GCC PR 104475), breaking -Werror builds with GCC 15 on Ubuntu 26.04. Keep
    the warning visible but do not let it fail the build.

--- cmake/ResInsightCommonSettings.cmake
+++ cmake/ResInsightCommonSettings.cmake
@@ -9,5 +9,13 @@ if(RESINSIGHT_TREAT_WARNINGS_AS_ERRORS)
         target_compile_options(ResInsightCommonSettings INTERFACE /WX)
     elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
         target_compile_options(ResInsightCommonSettings INTERFACE -Werror)
+        if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+            # GCC's -Wfree-nonheap-object false-positives on inlined
+            # std::vector copies (GCC PR 104475)
+            target_compile_options(
+                ResInsightCommonSettings INTERFACE
+                -Wno-error=free-nonheap-object
+            )
+        endif()
     endif()
 endif()
